de4160c16a0dba72533e6818b56661955be60947
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2014-06-10 10:27:33 +0300

    ScalaVersion = "2.11.1" & ScalatraVersion = "2.3.0.RC3"

--- project/build.scala
+++ project/build.scala
@@ -10,8 +10,8 @@ object OmatsivutBuild extends Build {
   val Organization = "fi.vm.sade"
   val Name = "omatsivut"
   val Version = "0.1.0-SNAPSHOT"
-  val ScalaVersion = "2.10.3"
-  val ScalatraVersion = "2.2.2"
+  val ScalaVersion = "2.11.1"
+  val ScalatraVersion = "2.3.0.RC3"
 
   lazy val project = Project (
     "omatsivut",
@@ -35,21 +35,10 @@ object OmatsivutBuild extends Build {
         "org.apache.tomcat.embed" % "tomcat-embed-logging-juli" % "7.0.22" % "container",
         "org.apache.tomcat.embed" % "tomcat-embed-jasper"       % "7.0.22" % "container",
         "org.mongodb" %% "casbah" % "2.7.2",
-        "org.json4s" %% "json4s-jackson" % "3.2.9"
+        "org.json4s" %% "json4s-jackson" % "3.2.9",
+        "com.typesafe" % "config" % "1.2.1"
       ),
-      testOptions in Test += Tests.Argument("junitxml"),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq(
-              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
-            Some("templates")
-          )
-        )
-      }
+      testOptions in Test += Tests.Argument("junitxml")
     )
   )
 }
