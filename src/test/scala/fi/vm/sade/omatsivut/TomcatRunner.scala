cea961f77f97f8a7a645dcf5124a74b48cd509cd
Juha Paananen <juha.paananen@gmail.com>
2014-06-12 12:12:58 +0300

    add TomcatRunner for running in IDE

---
+++ src/test/scala/fi/vm/sade/omatsivut/TomcatRunner.scala
@@ -0,0 +1,18 @@
+package fi.vm.sade.omatsivut
+
+import org.apache.catalina.startup.Tomcat
+import java.io.File
+
+/**
+ * Runs the application in an embedded Tomcat. Suitable for running in IDEA or Eclipse.
+ */
+object TomcatRunner extends App {
+  val tomcat = new Tomcat()
+  val webappDirLocation = "src/main/webapp/"
+  tomcat.setPort(8080)
+  tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+  System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+  tomcat.start();
+  tomcat.getServer().await();
+}
