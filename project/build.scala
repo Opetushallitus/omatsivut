060f43008052df4d1c2fe26079b76fad79e665a0
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2014-06-12 15:27:23 +0300

    Use 1.7 JVM target

--- project/build.scala
+++ project/build.scala
@@ -21,6 +21,8 @@ object OmatsivutBuild extends Build {
       name := Name,
       version := Version,
       scalaVersion := ScalaVersion,
+      javacOptions ++= Seq("-source", "1.7", "-target", "1.7"),
+      scalacOptions ++= Seq("-target:jvm-1.7", "-deprecation"),
       resolvers += Classpaths.typesafeReleases,
       sourceGenerators in Compile <+= buildInfo,
       buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion, sbtVersion),
