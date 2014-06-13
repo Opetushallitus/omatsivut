39a98751bb1091ffc06728709c3b3b507a8360ba
Juha Paananen <juha.paananen@gmail.com>
2014-06-13 11:45:37 +0300

    use MutableScalatraSpec

--- src/test/scala/fi/vm/sade/omatsivut/OHPServletSpec.scala
+++ src/test/scala/fi/vm/sade/omatsivut/OHPServletSpec.scala
@@ -2,17 +2,14 @@ package fi.vm.sade.omatsivut
 
 import org.scalatra.test.specs2._
 
-// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class OHPServletSpec extends ScalatraSpec { def is =
-  "GET / on OHPServlet"                     ^
-    "should return status 200"                  ! root200^
-                                                end
-
-  implicit val swagger = new OHPSwagger
-  
-  addServlet(new OHPServlet, "/*")
-
-  def root200 = get("/") {
-    status must_== 200
+class OHPServletSpec extends MutableScalatraSpec {
+  "GET / on OHPServlet" should {
+    "return status 200" in {
+      get("/") {
+        status must_== 200
+      }
+    }
   }
+
+  addServlet(new OHPServlet()(new OHPSwagger), "/*")
 }
