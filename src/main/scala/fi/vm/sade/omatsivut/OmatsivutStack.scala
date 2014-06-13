2d87ce79d20f8991648972eade5e35a4d7f368b0
Juha Paananen <juha.paananen@gmail.com>
2014-06-13 11:26:13 +0300

    catch exceptions, log and return generic error content

--- src/main/scala/fi/vm/sade/omatsivut/OmatsivutStack.scala
+++ src/main/scala/fi/vm/sade/omatsivut/OmatsivutStack.scala
@@ -3,13 +3,18 @@ package fi.vm.sade.omatsivut
 import org.scalatra._
 import org.slf4j.LoggerFactory
 
-trait OmatsivutStack extends ScalatraServlet {
-
-  val logger = LoggerFactory.getLogger(getClass)
-
+trait OmatsivutStack extends ScalatraServlet with Logging {
   notFound {
     // remove content type in case it was set through an action
     contentType = null
     serveStaticResource() getOrElse resourceNotFound()
+  }
+
+  error {
+    case e => {
+      logger.error(request.getMethod + " " + requestPath, e);
+      response.setStatus(500)
+      "500 Internal Server Error"
+    }
   }
 }
