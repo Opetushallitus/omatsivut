5a5d6f0f3b3efccb09c3496d2f7232c4f92ca309
Juha Paananen <juha.paananen@gmail.com>
2014-06-13 16:18:04 +0300

    add CacheControlFilter

---
+++ src/main/scala/fi/vm/sade/omatsivut/CacheControlFilter.scala
@@ -0,0 +1,23 @@
+package fi.vm.sade.omatsivut
+
+import javax.servlet._
+import javax.servlet.http.HttpServletResponse
+
+class CacheControlFilter extends Filter {
+  def doFilter(req: ServletRequest, res: ServletResponse, filterChain: FilterChain) {
+    doNotCache(res.asInstanceOf[HttpServletResponse])
+    filterChain.doFilter(req, res)
+  }
+
+  def doNotCache(response: HttpServletResponse) {
+    response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate, max-age=0, proxy-revalidate, no-transform")
+    response.setHeader("Pragma", "no-cache")
+    response.setHeader("Expires", "0")
+  }
+
+  def init(filterConfig: FilterConfig) {
+  }
+
+  def destroy {
+  }
+}
