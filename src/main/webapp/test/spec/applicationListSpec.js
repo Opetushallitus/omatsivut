c90722423e8f8ec528cf5f82a3336af62e38b5ae
Juha Paananen <juha.paananen@gmail.com>
2014-06-13 15:13:04 +0300

    make ssn explicit

--- src/main/webapp/test/spec/applicationListSpec.js
+++ src/main/webapp/test/spec/applicationListSpec.js
@@ -1,7 +1,7 @@
 describe('hakemuslistaus', function () {
     before(openPage('/?hetu=010101-123N', ApplicationListPage().visible))
 
-    it('hakemuslistassa on hakemus', function () {
+    it('hakemuslistassa on hakemus henkilölle 010101-123N', function () {
         expect(ApplicationListPage().applications()).to.deep.equal([{
             applicationSystemName: "MUUTettu TAAS Ammatillisen koulutuksen ja lukiokoulutuksen kevään 2014 yhteishaku"
         }])
