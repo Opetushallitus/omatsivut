45c0657491833324e82f301a3abc895fcbb224cf
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-06-18 09:42:58 +0300

    Add Gulp tasks and less compilation

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    less = require('gulp-less'),
+    jshint = require('gulp-jshint'),
+    livereload = require('gulp-livereload');
+
+var jsFiles = 'src/main/js/**/*.js';
+var lessFiles = 'src/main/less/**/*.less';
+
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
+
+gulp.task('lint', function() {
+    gulp.src(jsFiles)
+        .pipe(jshint({
+            globals: {
+                require: false,
+                angular: false
+            }
+        }))
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('less', function () {
+    gulp.src(lessFiles)
+        .pipe(less().on('error', handleError))
+        .pipe(gulp.dest('src/main/webapp/css'));
+});
+
+gulp.task('browserify', function() {
+    gulp.src(['src/main/js/listApp.js'])
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true
+        }).on('error', handleError))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('src/main/webapp'));
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch(['src/main/webapp/**/*.js', 'src/main/webapp/**/*.css', 'src/main/webapp/**/*.html'], livereload.changed);
+    gulp.watch([jsFiles],['lint', 'browserify']);
+    gulp.watch([lessFiles],['less']);
+
+});
+
+gulp.task('dev', ['lint', 'browserify', 'less', 'watch']);
--- src/main/js/listApp.js
+++ src/main/js/listApp.js
@@ -1,3 +1,6 @@
+require("angular/angular");
+require('angular-resource/angular-resource');
+
 var listApp = angular.module('listApp', ["ngResource"], function($locationProvider) {
     $locationProvider.html5Mode(true);
 });
