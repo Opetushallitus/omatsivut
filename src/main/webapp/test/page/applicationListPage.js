862206a475e3e5e99dc07675e74b760b8370917e
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-06-18 10:49:43 +0300

    Fix failing ui tests

--- src/main/webapp/test/page/applicationListPage.js
+++ src/main/webapp/test/page/applicationListPage.js
@@ -1,16 +1,13 @@
 function ApplicationListPage() {
     function visible() {
-        return api.applications().length > 0
+        return S("#hakemus-list").attr("ng-cloak") == null && api.applications().length > 0
     }
+
     var api = {
         openPage: openPage('/?hetu=010101-123N', visible),
 
-        visible: function() {
-            return api.applications().length > 0
-        },
         applications: function() {
             return S("#hakemus-list>li")
-
                 .filter(function() { return $(this).find("h2").length > 0 })
                 .map(function() { return { applicationSystemName: $(this).find("h2").text().trim() } }).toArray()
         }
