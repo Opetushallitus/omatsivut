e3348c6c3b50c061f80ecb5126ecc817a7652d7e
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-06-19 14:00:30 +0300

    Add stub for saving application to API

--- src/main/scala/fi/vm/sade/omatsivut/OHPServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/OHPServlet.scala
@@ -4,21 +4,25 @@ import fi.vm.sade.omatsivut.http.HttpClient
 import org.scalatra.json._
 import org.scalatra.swagger._
 import org.json4s.{DefaultFormats, Formats}
+import org.slf4j.LoggerFactory
 
 class OHPServlet(implicit val swagger: Swagger) extends OmatsivutStack with HttpClient with JacksonJsonSupport with OHPJsonFormats with SwaggerSupport {
   override def applicationName = Some("api")
+
   protected val applicationDescription = "Oppijan henkilökohtaisen palvelun REST API, jolla voi hakea ja muokata hakemuksia ja omia tietoja"
 
   before() {
     contentType = formats("json")
   }
-  
-  val getApplicationsSwagger =  (apiOperation[List[Hakemus]]("getApplications")
-      summary "Hae oppijan hakemukset"
-      parameters (
-        pathParam[String]("hetu").description("Käyttäjän henkilötunnus, jonka hakemukset listataan")
-      )
-  )
+
+  val getApplicationsSwagger = (apiOperation[List[Hakemus]]("getApplications")
+    summary "Hae oppijan hakemukset"
+    parameters pathParam[String]("hetu").description("Käyttäjän henkilötunnus, jonka hakemukset listataan")
+    )
+
+  val putApplicationsSwagger = (apiOperation[Unit]("putApplication")
+    summary "Tallenna hakemus"
+    )
 
   get("/applications/:hetu", operation(getApplicationsSwagger)) {
     AuthenticationInfoService.getHenkiloOID(params("hetu")) match {
@@ -27,5 +31,9 @@ class OHPServlet(implicit val swagger: Swagger) extends OmatsivutStack with Http
       case _ =>
         response.setStatus(404)
     }
+  }
+
+  put("/applications/:oid", operation(putApplicationsSwagger)) {
+    logger.info(request.body)
   }
 }
