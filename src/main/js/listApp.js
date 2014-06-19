542aa9819bed26abeff991cfa10fd769d1e6e022
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-06-19 12:25:01 +0300

    Show hakutoive list, allow reordering

--- src/main/js/listApp.js
+++ src/main/js/listApp.js
@@ -1,11 +1,28 @@
 require("angular/angular");
 require('angular-resource/angular-resource');
+require('angular-animate/angular-animate');
 
-var listApp = angular.module('listApp', ["ngResource"], function($locationProvider) {
+var listApp = angular.module('listApp', ["ngResource", "ngAnimate"], function($locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
 listApp.controller("listCtrl", ["$resource", "$scope", "$location", function ($resource, $scope, $location) {
     $scope.applications = $resource("api/applications/" + $location.search().hetu).query(function () {
     });
+}]);
+
+listApp.controller("hakemusCtrl", ["$scope", function ($scope) {
+    $scope.saved = true;
+
+    $scope.moveApplication = function(from, to) {
+        if (to >= 0 && to < this.application.hakutoiveet.length) {
+            var arr = this.application.hakutoiveet;
+            arr.splice(to, 0, arr.splice(from, 1)[0]);
+        }
+        $scope.saved = false;
+    };
+
+    $scope.saveApplication = function() {
+        $scope.saved = true;
+    };
 }]);
