bdde28693a5f93216d25d9d261e476d9e63353bd
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-06-23 12:24:22 +0300

    refactor

--- src/main/js/listApp.js
+++ src/main/js/listApp.js
@@ -30,14 +30,14 @@ listApp.controller("hakemusCtrl", ["$scope", "$element", function ($scope, $elem
             return index >= 0 && index <= self.application.hakutoiveet.length-1 && self.application.hakutoiveet[index]["Opetuspiste-id"] !== "";
         }
         return indexValid(start) && indexValid(end);
-    }
+    };
 
     $scope.moveApplication = function(from, to) {
         if (to >= 0 && to < this.application.hakutoiveet.length) {
             var arr = this.application.hakutoiveet;
             arr.splice(to, 0, arr.splice(from, 1)[0]);
+            $scope.saved = false;
         }
-        $scope.saved = false;
     };
 
     $scope.saveApplication = function() {
@@ -60,52 +60,54 @@ listApp.directive('sortable', ['$document', function($document) {
         var slide = function(el, offset) {
             el.css("transition", "all 0.5s");
             el.css("transform", "translate3d(0px, " + offset + "px, 0px)");
-        }
+        };
 
         var moveDown = function(el) {
             slide(el, el.outerHeight());
-        }
+        };
 
         var moveUp = function(el) {
             slide(el, -el.outerHeight());
-        }
+        };
 
         var resetSlide = function(el) {
             el.css({
                 "transition": "none",
                 "transform": "none"
             });
-        }
+        };
+
 
         var switchPlaces = function(element1, element2) {
-            moveDown(element1);
-            moveUp(element2)
+            if (element1.index() < element2.index()) {
+                moveDown(element1);
+                moveUp(element2);
+            } else {
+                moveUp(element1);
+                moveDown(element2);
+            }
 
             setTimeout(function() {
                 $scope.$apply(function() {
                     $scope.moveApplication(element1.index(), element2.index());
                     resetSlide(element1);
-                    resetSlide(element2)
+                    resetSlide(element2);
                 });
             }, 500);
-        }
+        };
 
-        $element.on("click", ".sort-arrow-down", function(event) {
-            var btn = $(event.target);
-            if (!btn.hasClass("disabled")) {
-                var element1 = btn.closest("li");
-                var element2 = element1.next();
-                switchPlaces(element1, element2);
-            }
-        });
+        var arrowClicked = function(elementF) {
+            return function(evt) {
+                var btn = $(evt.target);
+                if (!btn.hasClass("disabled")) {
+                    var element1 = btn.closest("li");
+                    var element2 = element1[elementF]();
+                    switchPlaces(element1, element2);
+                }
+            };
+        };
 
-        $element.on("click", ".sort-arrow-up", function(event) {
-            var btn = $(event.target);
-            if (!btn.hasClass("disabled")) {
-                var element1 = btn.closest("li");
-                var element2 = element1.prev();
-                switchPlaces(element2, element1);
-            }
-        });
+        $element.on("click", ".sort-arrow-down", arrowClicked("next"));
+        $element.on("click", ".sort-arrow-up", arrowClicked("prev"));
     };
 }]);
