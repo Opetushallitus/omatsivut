6e489b0acad391614c9bf5fb9f96cf5f15698dfa
Juha Paananen <juha.paananen@gmail.com>
2014-07-04 13:59:35 +0300

    introduce Default, Dev and IT Spring configurations

--- src/main/scala/fi/vm/sade/omatsivut/OmatSivutSpringContext.scala
+++ src/main/scala/fi/vm/sade/omatsivut/OmatSivutSpringContext.scala
@@ -3,17 +3,6 @@ package fi.vm.sade.omatsivut
 import org.springframework.context.annotation._
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 
-@Configuration
-@ComponentScan (basePackages = Array ("fi.vm.sade.haku") )
-@PropertySource (value = Array ("config/dev/haku.properties", "config/dev/ext.properties") )
-@Profile (Array ("dev") )
-@ImportResource (Array ("/META-INF/spring/logger-mock-context.xml") )
-class OmatSivutSpringConfiguration {
-  @Bean def enablePlaceholderReplacement: PropertySourcesPlaceholderConfigurer = {
-    return new PropertySourcesPlaceholderConfigurer
-  }
-}
-
 object OmatSivutSpringContext {
   val context = createApplicationContext
 
@@ -22,8 +11,42 @@ object OmatSivutSpringContext {
   private def createApplicationContext: AnnotationConfigApplicationContext = {
     val appContext: AnnotationConfigApplicationContext = new AnnotationConfigApplicationContext
     appContext.getEnvironment.setActiveProfiles("dev")
-    appContext.register(classOf[OmatSivutSpringConfiguration])
+    appContext.register(classOf[Dev], classOf[IT], classOf[Default])
     appContext.refresh
     return appContext
+  }
+
+  @Configuration
+  @ComponentScan (basePackages = Array ("fi.vm.sade.haku") )
+  @PropertySource (value = Array ("config/dev/haku.properties", "config/dev/ext.properties") )
+  @Profile (Array ("dev") )
+  @ImportResource (Array ("/META-INF/spring/logger-mock-context.xml") )
+  class Dev extends OmatSivutConfiguration {
+  }
+
+  @Configuration
+  @ComponentScan (basePackages = Array ("fi.vm.sade.haku") )
+  @PropertySource (value = Array ("config/it/haku.properties", "config/it/ext.properties") )
+  @Profile (Array ("it") )
+  @ImportResource (Array ("/META-INF/spring/logger-mock-context.xml") )
+  class IT extends OmatSivutConfiguration {
+  }
+
+  @Configuration
+  @ComponentScan (basePackages = Array ("fi.vm.sade.haku") )
+  @PropertySource (value = Array (  "file:///${user.home:''}/oph-configuration/haku.properties",
+                                    "file:///${user.home:''}/oph-configuration/common.properties",
+                                    "file:///${user.home:''}/oph-configuration/override.properties") )
+  @Profile (Array ("default") )
+  @ImportResource (Array (  "file:///${user.home:''}/oph-configuration/security-context-backend.xml",
+                            "/META-INF/spring/logger-context.xml") )
+  class Default extends OmatSivutConfiguration {
+
+  }
+
+  abstract class OmatSivutConfiguration {
+    @Bean def enablePlaceholderReplacement: PropertySourcesPlaceholderConfigurer = {
+      return new PropertySourcesPlaceholderConfigurer
+    }
   }
 }
