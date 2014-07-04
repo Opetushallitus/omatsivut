f5894b8ab5054c60a9b8570829670f8ee80a04b3
Juha Paananen <juha.paananen@gmail.com>
2014-07-04 10:03:15 +0300

    refactor

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationDaoWrapper.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationDaoWrapper.scala
@@ -1,14 +1,14 @@
 package fi.vm.sade.omatsivut.hakemus
 
-import fi.vm.sade.haku.oppija.hakemus.it.dao.impl.ApplicationDAOMongoImpl
 import fi.vm.sade.haku.oppija.hakemus.domain.Application
 import scala.collection.JavaConversions._
 import fi.vm.sade.omatsivut.OmatSivutSpringContext
 import java.util
+import fi.vm.sade.haku.oppija.hakemus.it.dao.ApplicationDAO
 
 object ApplicationDaoWrapper {
   def findByPersonOid(personOid: String): List[Hakemus] = {
-    val dao: ApplicationDAOMongoImpl = OmatSivutSpringContext.context.getBean(classOf[ApplicationDAOMongoImpl])
+    val dao = OmatSivutSpringContext.context.getBean(classOf[ApplicationDAO])
     val applicationJavaObjects: List[Application] = dao.find(new Application()).toList
     applicationJavaObjects.map { application =>
       Hakemus(application.getOid, application.getReceived.getTime, convertHakuToiveet(application), None)
