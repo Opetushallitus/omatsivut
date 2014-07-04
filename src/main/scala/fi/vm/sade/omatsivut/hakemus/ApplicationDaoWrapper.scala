cf500388e0aeb3123991e83021454b8858d40460
Juha Paananen <juha.paananen@gmail.com>
2014-07-04 10:01:44 +0300

    convert hakutoiveet in ApplicationDaoWrapper

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationDaoWrapper.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationDaoWrapper.scala
@@ -4,13 +4,59 @@ import fi.vm.sade.haku.oppija.hakemus.it.dao.impl.ApplicationDAOMongoImpl
 import fi.vm.sade.haku.oppija.hakemus.domain.Application
 import scala.collection.JavaConversions._
 import fi.vm.sade.omatsivut.OmatSivutSpringContext
+import java.util
 
 object ApplicationDaoWrapper {
   def findByPersonOid(personOid: String): List[Hakemus] = {
     val dao: ApplicationDAOMongoImpl = OmatSivutSpringContext.context.getBean(classOf[ApplicationDAOMongoImpl])
     val applicationJavaObjects: List[Application] = dao.find(new Application()).toList
     applicationJavaObjects.map { application =>
-      Hakemus(application.getOid, application.getReceived.getTime, Nil, None)
+      Hakemus(application.getOid, application.getReceived.getTime, convertHakuToiveet(application), None)
     }
+  }
+
+  def convertHakuToiveet(application: Application): List[Map[String, String]] = {
+    val answers: util.Map[String, util.Map[String, String]] = application.getAnswers
+    val hakuToiveetData: Map[String, String] = answers.get("hakutoiveet").toMap
+    HakutoiveetConverter.convert(hakuToiveetData)
+  }
+}
+
+object HakutoiveetConverter {
+  def convert(toiveet: Map[String, String]): List[Map[String, String]] = {
+    groupPreferences(toiveet)
+      .toList
+      .map(shortenNames)
+      .map(convertEmptyPreferences)
+      .sortBy(map => map.get("priority"))
+      .map((m) => m.filterKeys { Set("priority").contains(_) == false})
+  }
+
+  private def shortenKey(v: (String, String), delimiter: Char = '-') = {
+    v._1.substring(v._1.indexOf(delimiter) + 1)
+  }
+
+  private def tupleWithShortKey(v: (String, String)) = {
+    if (v._1.contains("_")) (shortenKey(v, '_'), v._2) else (shortenKey(v), v._2)
+  }
+
+  private def groupPreferences(toiveet: Map[String, String]) = {
+    val pattern = "preference(\\d+).*".r
+    toiveet.groupBy((key) => key._1 match {
+      case pattern(x: String) => x
+      case _ => ""
+    })
+  }
+
+  private def convertEmptyPreferences(toiveet: Map[String, String]) = {
+    if (toiveet.getOrElse("Koulutus-id", "").length() == 0) {
+      Map("priority" -> toiveet.getOrElse("priority", ""))
+    } else {
+      toiveet
+    }
+  }
+
+  private def shortenNames(tuple: (String, Map[String, String])) = {
+    tuple._2.map(tupleWithShortKey) ++ Map("priority" -> tuple._1)
   }
 }
--- src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusRepository.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusRepository.scala
@@ -55,50 +55,13 @@ object HakemusRepository extends Logging {
 
     val query = MongoDBObject("personOid" -> oid)
 
-    def shortenKey(v: (String, String), delimiter: Char = '-') = {
-      v._1.substring(v._1.indexOf(delimiter) + 1)
-    }
-
-    def tupleWithShortKey(v: (String, String)) = {
-      if (v._1.contains("_")) (shortenKey(v, '_'), v._2) else (shortenKey(v), v._2)
-    }
-
-    def shortenNames(tuple: (String, Map[String, String])) = {
-      tuple._2.map(tupleWithShortKey) ++ Map("priority" -> tuple._1)
-    }
-
-    def groupPreferences(toiveet: Map[String, String]) = {
-      val pattern = "preference(\\d+).*".r
-      toiveet.groupBy((key) => key._1 match {
-        case pattern(x: String) => x
-        case _ => ""
-      })
-    }
-
-    def convertEmptyPreferences(toiveet: Map[String, String]) = {
-      if (toiveet.getOrElse("Koulutus-id", "").length() == 0) {
-        Map("priority" -> toiveet.getOrElse("priority", ""))
-      } else {
-        toiveet
-      }
-    }
-
-    def flatten(toiveet: Map[String, String]): List[Map[String, String]] = {
-      groupPreferences(toiveet)
-        .toList
-        .map(shortenNames)
-        .map(convertEmptyPreferences)
-        .sortBy(map => map.get("priority"))
-        .map((m) => m.filterKeys { Set("priority").contains(_) == false})
-    }
-
     hakemukset.find(query).toList.map((hakemus: DBObject) => {
       val haku = getHaku(hakemus)
       (hakemus, haku)
     }).map((tuple: (DBObject, DBObject)) => {
       val hakem = grater[Hakemus].asObject(tuple._1)
       val toiveet = tuple._1.getAs[Map[String, String]]("answers").get.asDBObject.getAs[Map[String, String]]("hakutoiveet").get
-      hakem.hakutoiveet = flatten(toiveet)
+      hakem.hakutoiveet = HakutoiveetConverter.convert(toiveet)
       if (!tuple._2.isEmpty) {
         hakem.haku = Some(grater[Haku].asObject(tuple._2))
       }
