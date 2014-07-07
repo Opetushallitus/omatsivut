276416badc9f79b929b2d45891ba66d80be3ce84
Juha Paananen <juha.paananen@gmail.com>
2014-07-07 17:45:19 +0300

    get rid of local fixtures. use fixtures from hakemus-api

--- src/main/scala/fi/vm/sade/omatsivut/fixtures/FixtureUtils.scala
+++ src/main/scala/fi/vm/sade/omatsivut/fixtures/FixtureUtils.scala
@@ -1,41 +1,16 @@
 package fi.vm.sade.omatsivut.fixtures
 
-import com.mongodb.util.JSON
-import scala.io.Source
-import com.mongodb.casbah.Imports._
-import fi.vm.sade.omatsivut.{OmatSivutSpringContext, AppConfig}
-import fi.vm.sade.haku.testfixtures.{MongoFixtureImporter, ProjectRootFinder}
+import fi.vm.sade.haku.testfixtures.MongoFixtureImporter
+import fi.vm.sade.omatsivut.OmatSivutSpringContext
 import org.springframework.data.mongodb.core.MongoTemplate
 
 object FixtureUtils {
   def applyFixtures(): Unit = {
     importHakemusApiFixtures
-    importLocalFixtures
   }
 
   private def importHakemusApiFixtures {
     val mongoTemplate = OmatSivutSpringContext.context.getBean(classOf[MongoTemplate])
     MongoFixtureImporter.importJsonFixtures(mongoTemplate)
-  }
-
-  private def importLocalFixtures {
-    val fixtureDir = new java.io.File("testfixtures")
-
-    for (collection <- fixtureDir.listFiles if collection.isDirectory) {
-
-      val coll = AppConfig.settings.hakuAppMongoDb(collection.getName)
-
-      for (file <- collection.listFiles if file.getName endsWith ".json") {
-        val source = Source.fromFile(file)
-        val lines = source.mkString
-        source.close()
-        val doc: MongoDBObject = JSON.parse(lines).asInstanceOf[DBObject]
-        val query = doc filter {
-          case (k, v) => k == "_id"
-        }
-        coll.remove(query)
-        coll.insert(doc)
-      }
-    }
   }
 }
--- testfixtures/make-fixtures.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p application||0
-mkdir -p applicationSystem||0
-
-mongoexport "$@" -d hakulomake -c applicationSystem --query '{_id:"1.2.246.562.5.2013080813081926341927"}' --out applicationSystem/2013080813081926341927.json
-mongoexport "$@" -d hakulomake -c application --query '{oid: "1.2.246.562.11.00000876904"}' --out application/00000876904.json
