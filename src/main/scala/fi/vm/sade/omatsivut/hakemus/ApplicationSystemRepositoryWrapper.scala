182bdad80665cda0831168c91a81e85210f4019e
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-07-10 13:56:40 +0300

    Use application system service instead of repository
    has added benefit of cache

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationSystemRepositoryWrapper.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationSystemRepositoryWrapper.scala
@@ -1,14 +1,14 @@
 package fi.vm.sade.omatsivut.hakemus
 
 import fi.vm.sade.haku.oppija.lomake.domain.{ApplicationPeriod, ApplicationSystem}
-import fi.vm.sade.haku.oppija.repository.ApplicationSystemRepository
+import fi.vm.sade.haku.oppija.lomake.service.ApplicationSystemService
 import fi.vm.sade.omatsivut.OmatSivutSpringContext
 import fi.vm.sade.omatsivut.domain.{Translations, HakuAika, Haku}
 import org.joda.time.DateTime
 import scala.collection.JavaConversions._
 
 object ApplicationSystemRepositoryWrapper {
-  val repository = OmatSivutSpringContext.context.getBean(classOf[ApplicationSystemRepository])
+  val repository = OmatSivutSpringContext.context.getBean(classOf[ApplicationSystemService])
 
   def findByOid(applicationSystemOid: String): Option[Haku] = {
     tryFind(applicationSystemOid).map { applicationSystem =>
@@ -27,7 +27,7 @@ object ApplicationSystemRepositoryWrapper {
 
   private def tryFind(applicationSystemOid: String): Option[ApplicationSystem] = {
     try {
-      Some(repository.findById(applicationSystemOid))
+      Some(repository.getApplicationSystem(applicationSystemOid))
     } catch {
       case e: Exception => None
     }
