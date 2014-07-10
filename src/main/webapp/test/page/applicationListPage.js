2a57546ec7140e63d042c92da52b4f18ed3bee08
Juha Paananen <juha.paananen@gmail.com>
2014-07-10 12:48:49 +0300

    refactor page object

--- src/main/webapp/test/page/applicationListPage.js
+++ src/main/webapp/test/page/applicationListPage.js
@@ -1,77 +1,138 @@
 function ApplicationListPage() {
-    function visible() {
-        return S("#hakemus-list").attr("ng-cloak") == null && api.applications().length > 0
+  var testHetu = "010101-123N"
+  var openListPage = openPage("/omatsivut/", visible)
+
+  var api = {
+    resetDataAndOpen: function () {
+      return db.resetData().then(openListPage)
+    },
+
+    hetu: function () {
+      return testHetu
+    },
+
+    openPage: openListPage,
+
+    applications: function () {
+      return S("#hakemus-list>li")
+        .filter(function () {
+          return $(this).find("h2").length > 0
+        })
+        .map(function () {
+          return { applicationSystemName: $(this).find("h2").text().trim() }
+        }).toArray()
+    },
+
+    isSavingState: function (applicationIndex, isSaving) {
+      return function () {
+        return (getApplication(applicationIndex).find(".ajax-spinner").length > 0) === isSaving
+      }
+    },
+
+    preferencesForApplication: function (index) {
+      return preferencesForApplication(index, function (item) {
+        return item.data()["hakutoive.Koulutus"].length > 0
+      })
+    },
+
+    emptyPreferencesForApplication: function (index) {
+      return preferencesForApplication(index, function (item) {
+        return item.data()["hakutoive.Koulutus"].length == 0
+      })
+    },
+
+    saveButton: function (applicationIndex) {
+      return saveButton(getApplication(applicationIndex).find(".save-btn"))
+    },
+
+    getPreference: function (index) {
+      return PreferenceItem(function () {
+        var applicationElement = getApplication(0)
+        return applicationElement.find(".preference-list-item").eq(index)
+      })
+    }
+  }
+  return api
+
+  function PreferenceItem(el) {
+    function arrowDown() {
+      return el().find(".sort-arrow-down")
     }
 
-    function preferenceItem(el) {
-        return {
-            element: function() { return el },
-            data: function() { return uiUtil.inputValues(el) },
-            arrowDown: function() { return el.find(".sort-arrow-down") },
-            arrowUp: function() { return el.find(".sort-arrow-up") },
-            number: function() { return el.find(".row-number") },
-            deleteBtn: function() { return el.find(".delete-btn") },
-            isDisabled: function() { return this.arrowDown().hasClass("disabled") && this.arrowUp().hasClass("disabled") && this.deleteBtn().is(":hidden")},
-            selectOpetusPiste: function(query) {
-                var inputField = el.find(".opetuspiste input");
-                inputField.val(query).change()
-                return wait.until(function() { return inputField.next().find("li").eq(0).find("a").length > 0 })().then(function() {
-                  inputField.next().find("li").eq(0).find("a").click()
-                })
-            },
-            selectKoulutus: function() {}
+    function arrowUp() {
+      return el().find(".sort-arrow-up")
+    }
+
+    return {
+      data: function () {
+        return uiUtil.inputValues(el())
+      },
+      moveDown: function () {
+        arrowDown().click()
+      },
+      moveUp: function () {
+        arrowUp().click()
+      },
+
+      number: function () {
+        return el().find(".row-number").text()
+      },
+      remove: function () {
+        el().find(".delete-btn").click().click()
+      },
+      isNew: function () {
+        return el().find("input:visible").length > 0
+      },
+      isDisabled: function () {
+        return arrowDown().hasClass("disabled") && arrowUp().hasClass("disabled") && this.deleteBtn().is(":hidden")
+      },
+      opetuspiste: function () {
+        return el().find(".opetuspiste input").val()
+      },
+      selectOpetusPiste: function (query) {
+        var inputField = el().find(".opetuspiste input");
+        inputField.val(query).change()
+        return wait.until(function () {
+          return inputField.next().find("li").eq(0).find("a").length > 0
+        })().then(function () {
+          inputField.next().find("li").eq(0).find("a").click()
+        })
+      },
+      selectKoulutus: function () {
+      }
+    }
+  }
+
+  function visible() {
+    return S("#hakemus-list").attr("ng-cloak") == null && api.applications().length > 0
+  }
+
+  function saveButton(el) {
+    return {
+      isEnabled: function (isEnabled) {
+        return function () {
+          return el.prop("disabled") != isEnabled
         }
+      },
+      click: function () {
+        el.click()
+      }
     }
+  }
 
-    function saveButton(el) {
-        return {
-            element: function() { return el },
-            isEnabled: function(isEnabled) { return function() { return el.prop("disabled") != isEnabled } },
-            click: function() { el.click() }
-        }
-    }
+  function getApplication(index) {
+    return S("#hakemus-list>li").eq(index)
+  }
 
-    var testHetu = "010101-123N"
-
-    var openListPage = openPage("/omatsivut/", visible)
-
-    function getApplication(index) { return S("#hakemus-list>li").eq(index) }
-
-    function preferencesForApplication(index, filter) {
-        var application = getApplication(index)
-        return application.find(".preference-list-item")
-            .map(function() { return preferenceItem(S(this))}).toArray()
-            .filter(filter)
-    }
-
-    var api = {
-        resetDataAndOpen: function() { return db.resetData().then(openListPage) },
-
-        hetu: function() { return testHetu },
-
-        openPage: openListPage,
-
-        applications: function() {
-            return S("#hakemus-list>li")
-                .filter(function() { return $(this).find("h2").length > 0 })
-                .map(function() { return { applicationSystemName: $(this).find("h2").text().trim() } }).toArray()
-        },
-
-        isSavingState: function(applicationIndex, isSaving) {
-            return function() { return (getApplication(applicationIndex).find(".ajax-spinner").length > 0) === isSaving }
-        },
-
-        preferencesForApplication: function(index) {
-            return preferencesForApplication(index,  function(item) { return item.data()["hakutoive.Koulutus"].length > 0 })
-        },
-
-        emptyPreferencesForApplication: function(index) {
-            return preferencesForApplication(index, function(item) { return item.data()["hakutoive.Koulutus"].length == 0 })
-        },
-
-        saveButton: function(applicationIndex) {
-            return saveButton(getApplication(applicationIndex).find(".save-btn"))
-        }
-    }
-    return api
+  function preferencesForApplication(index, filter) {
+    var application = getApplication(index)
+    return application.find(".preference-list-item")
+      .map(function () {
+        var el = this
+        return PreferenceItem(function () {
+          return S(el)
+        })
+      }).toArray()
+      .filter(filter)
+  }
 }
--- src/main/webapp/test/spec/applicationListSpec.js
+++ src/main/webapp/test/spec/applicationListSpec.js
@@ -1,7 +1,9 @@
 (function () {
   describe('hakemuslistaus', function () {
+    var page = ApplicationListPage();
+
     before(function (done) {
-      session.init("010101-123N").then(ApplicationListPage().resetDataAndOpen).done(done)
+      session.init("010101-123N").then(page.resetDataAndOpen).done(done)
     })
 
     describe("näyttäminen", function() {
@@ -39,33 +41,36 @@
 
     describe("hakemuslistauksen muokkaus", function () {
       endToEndTest("järjestys", "järjestys muuttuu nuolta klikkaamalla", function () {
-        var pref2 = getPreference(1)
-        var pref3 = getPreference(2)
-        pref2.arrowDown().click()
+        var pref2 = page.getPreference(1)
+        var pref3 = page.getPreference(2)
+        var opetuspiste2 = pref2.opetuspiste()
+        var opetuspiste3 = pref3.opetuspiste()
+        pref2.moveDown()
         return wait.until(function () {
-          return pref2.element().index() === 2 && pref3.element().index() === 1 && pref2.number().text() === "3."
+          return pref2.opetuspiste() === opetuspiste3 && pref3.opetuspiste() === opetuspiste2 && pref3.number() === "3."
         })()
       }, function (dbStart, dbEnd) {
         dbStart.hakutoiveet[1].should.deep.equal(dbEnd.hakutoiveet[2])
         dbStart.hakutoiveet[2].should.deep.equal(dbEnd.hakutoiveet[1])
       })
       endToEndTest("poisto", "hakutoiveen voi poistaa", function () {
-        var pref1 = getPreference(0)
-        var pref2 = getPreference(1)
-        pref1.deleteBtn().click().click()
+        var pref1 = page.getPreference(0)
+        var pref2 = page.getPreference(1)
+        var opetuspiste2 = pref2.opetuspiste()
+        pref1.remove()
         return wait.until(function () {
-          return pref2.element().index() === 0 && pref1.element().parent().length === 0
+          return pref1.opetuspiste() === opetuspiste2
         })()
       }, function (dbStart, dbEnd) {
         dbEnd.hakutoiveet.should.deep.equal(_.flatten([_.rest(dbStart.hakutoiveet), {}]))
       })
       endToEndTest("lisäys", "hakutoiveen voi lisätä", function() {
-        var pref = getPreference(2)
+        var pref = page.getPreference(2)
+        pref.remove()
         return pref.selectOpetusPiste("Ahl").then(
             pref.selectKoulutus(0)
         )
       }, function(dbStart, dbEnd) {
-          debugger
           dbEnd.hakutoiveet.should.deep.equal(_.flatten([_.rest(dbStart.hakutoiveet), {}])) // <- todo: copy pasta
       })
     })
@@ -106,8 +111,4 @@
       return item.data()
     })
   }
-
-  function getPreference(index) {
-    return ApplicationListPage().preferencesForApplication(0)[index]
-  };
 })()
