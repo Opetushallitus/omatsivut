1b9e8b0ca27393beb3f7d77d6180e60d6b6b67f8
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-07-11 14:41:56 +0300

    Remove semicolons

--- src/test/scala/fi/vm/sade/omatsivut/TomcatRunner.scala
+++ src/test/scala/fi/vm/sade/omatsivut/TomcatRunner.scala
@@ -12,9 +12,9 @@ object TomcatRunner extends App {
   val webappDirLocation = "src/main/webapp/"
 
   tomcat.setPort(8080)
-  tomcat.addWebapp("/omatsivut", new File(webappDirLocation).getAbsolutePath());
-  System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+  tomcat.addWebapp("/omatsivut", new File(webappDirLocation).getAbsolutePath())
+  System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath())
 
-  tomcat.start();
-  tomcat.getServer().await();
+  tomcat.start()
+  tomcat.getServer().await()
 }
