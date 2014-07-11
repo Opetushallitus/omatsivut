473701998b43627d3781341db13b4b0a8e7f66c4
Juha Paananen <juha.paananen@gmail.com>
2014-07-11 12:30:28 +0300

    silence embedded mongo

--- src/main/scala/fi/vm/sade/omatsivut/mongo/EmbeddedMongo.scala
+++ src/main/scala/fi/vm/sade/omatsivut/mongo/EmbeddedMongo.scala
@@ -1,8 +1,9 @@
 package fi.vm.sade.omatsivut.mongo
 
-import de.flapdoodle.embed.mongo.{MongodProcess, MongodExecutable, MongodStarter}
-import de.flapdoodle.embed.mongo.config.{Net, MongodConfigBuilder, IMongodConfig}
+import de.flapdoodle.embed.mongo.{Command, MongodProcess, MongodExecutable, MongodStarter}
+import de.flapdoodle.embed.mongo.config.{RuntimeConfigBuilder, Net, MongodConfigBuilder, IMongodConfig}
 import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.config.io.ProcessOutput
 import de.flapdoodle.embed.process.runtime.Network
 import fi.vm.sade.omatsivut.{PortChecker, AppConfig}
 import fi.vm.sade.omatsivut.AppConfig.AppConfig
@@ -30,8 +31,15 @@ object EmbeddedMongo {
 }
 
 class MongoServer(val port: Int) {
-  private val mongodConfig: IMongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION).net(new Net(port, Network.localhostIsIPv6)).build
-  private val runtime: MongodStarter = MongodStarter.getDefaultInstance
+  private val mongodConfig: IMongodConfig = new MongodConfigBuilder()
+    .version(Version.Main.PRODUCTION)
+    .net(new Net(port, Network.localhostIsIPv6))
+    .build
+  private val runtimeConfig = new RuntimeConfigBuilder()
+    .defaults(Command.MongoD)
+    .processOutput(ProcessOutput.getDefaultInstanceSilent())
+    .build();
+  private val runtime: MongodStarter = MongodStarter.getInstance(runtimeConfig)
   private val mongodExecutable = runtime.prepare(mongodConfig)
   private val mongod = mongodExecutable.start
 
