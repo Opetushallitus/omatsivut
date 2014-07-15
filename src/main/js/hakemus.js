f1940d26a401de104d1d0ef72f4a8c18229bc25a
Juha Paananen <juha.paananen@gmail.com>
2014-07-15 14:23:58 +0300

    extract domain classes hakemus, hakutoive into js files

---
+++ src/main/js/hakemus.js
@@ -0,0 +1,61 @@
+var Hakutoive = require('./hakutoive')
+
+function Hakemus(json) {
+  _.extend(this, json)
+  this.hakutoiveet = _(this.hakutoiveet).map(function(hakutoive) { return new Hakutoive(hakutoive) })
+}
+
+Hakemus.prototype = {
+  removePreference: function(index) {
+    var row = this.hakutoiveet.splice(index, 1)[0]
+  },
+
+  addPreference: function(hakutoive) {
+    this.hakutoiveet.push(hakutoive)
+  },
+
+  hasPreference: function(index) {
+    return index >= 0 && index <= this.hakutoiveet.length-1 && this.hakutoiveet[index].hasData()
+  },
+
+  canMoveTo: function(from, to) {
+    return this.hasPreference(from) && this.hasPreference(to);
+  },
+
+  toJson: function() {
+    return _.extend({}, this, { hakutoiveet: _(this.hakutoiveet).map(function(hakutoive) { return hakutoive.toJson() })})
+  },
+
+  setAsSaved: function() {
+    _(this.hakutoiveet).each(function(hakutoive) {
+        if (hakutoive.hasData()) hakutoive.setAsSaved() }
+    )
+  },
+
+  isValid: function() {
+    return _(this.hakutoiveet).every(function(hakutoive) { return hakutoive.isValid() })
+  },
+
+  getHakutoiveWatchCollection: function() {
+    return _(this.hakutoiveet).map(function(hakutoive) { return hakutoive.data })
+  },
+
+  moveHakutoive: function(from, to) {
+    this.hakutoiveet[from].setAsModified()
+    this.hakutoiveet[to].setAsModified()
+    this.hakutoiveet.splice(to, 0, this.hakutoiveet.splice(from, 1)[0])
+  },
+
+  getChangedItems: function() {
+    return _(this.hakutoiveet).chain()
+      .map(function(hakutoive, index) { return hakutoive.isModified ? index : null })
+      .without(null)
+      .value()
+  },
+
+  isEditable: function(index) {
+    var firstEditableIndex = _(this.hakutoiveet).filter(function(hakutoive) { return hakutoive.hasData() && hakutoive.isValid() }).length
+    return index >= 0 && index < this.hakutoiveet.length && index <= firstEditableIndex
+  }
+}
+module.exports = Hakemus
--- src/main/js/listApp.js
+++ src/main/js/listApp.js
@@ -3,6 +3,9 @@ require('ng-resource')(window, angular);
 require('angular-animate');
 _ = require("underscore");
 require("../lib/ui-bootstrap-custom-tpls-0.10.0.min.js");
+var Hakemus = require('./hakemus')
+var Hakutoive = require('./hakutoive')
+
 var listApp = angular.module('listApp', ["ngResource", "ngAnimate", "ui.bootstrap.typeahead", "template/typeahead/typeahead-popup.html", "template/typeahead/typeahead-match.html"], function($locationProvider) {
   $locationProvider.html5Mode(true);
 });
@@ -25,133 +28,6 @@ listApp.factory("settings", ["$animate", function($animate) {
   };
 }]);
 
-var Hakutoive = function(json) {
-  this.data = json
-  this.isModified = false
-  this.isNew = _.isEmpty(json)
-}
-
-Hakutoive.prototype = {
-  toJson: function() {
-    return this.data
-  },
-
-  clear: function() {
-    this.data = {}
-    this.isNew = true
-    this.isModified = false
-  },
-
-  hasData: function() {
-    return !_.isEmpty(this.data)
-  },
-
-  setOpetuspiste: function(id, name) {
-    this.data["Opetuspiste"] = name
-    this.data["Opetuspiste-id"] = id
-    this.isModified = true
-  },
-
-  setKoulutus: function(koulutus) {
-    this.data["Koulutus"] = koulutus.name.toString()
-    this.data["Koulutus-id"] = koulutus.id.toString()
-    this.data["Koulutus-educationDegree"] = koulutus.educationDegree.toString()
-    this.data["Koulutus-id-sora"] = koulutus.sora.toString()
-    this.data["Koulutus-id-aoIdentifier"] = koulutus.aoIdentifier.toString()
-    this.data["Koulutus-id-kaksoistutkinto"] = koulutus.kaksoistutkinto.toString()
-    this.data["Koulutus-id-vocational"] = koulutus.vocational.toString()
-    this.data["Koulutus-id-educationcode"] = koulutus.educationCodeUri.toString()
-    this.data["Koulutus-id-athlete"] = koulutus.athleteEducation.toString()
-    this.isModified = true
-  },
-
-  hasOpetuspiste: function() {
-    return !_.isEmpty(this.data["Opetuspiste-id"])
-  },
-
-  removeOpetuspisteData: function() {
-    var self = this
-    _.each(this.data, function(value, key) {
-      if (key.indexOf("$")!==0 && key != "Opetuspiste")
-        delete self.data[key]
-    })
-  },
-
-  isValid: function() {
-    if (!_.isEmpty(this.data["Opetuspiste"]))
-      return !_.isEmpty(this.data["Koulutus-id"])
-    else
-      return true
-  },
-
-  setAsSaved: function() {
-    this.isNew = false
-    this.isModified = false
-  },
-
-  setAsModified: function() {
-    this.isModified = true
-  }
-}
-
-var Hakemus = function(json) {
-  _.extend(this, json)
-  this.hakutoiveet = _(this.hakutoiveet).map(function(hakutoive) { return new Hakutoive(hakutoive) })
-}
-
-Hakemus.prototype = {
-  removePreference: function(index) {
-    var row = this.hakutoiveet.splice(index, 1)[0]
-  },
-
-  addPreference: function(hakutoive) {
-    this.hakutoiveet.push(hakutoive)
-  },
-
-  hasPreference: function(index) {
-    return index >= 0 && index <= this.hakutoiveet.length-1 && this.hakutoiveet[index].hasData()
-  },
-
-  canMoveTo: function(from, to) {
-    return this.hasPreference(from) && this.hasPreference(to);
-  },
-
-  toJson: function() {
-    return _.extend({}, this, { hakutoiveet: _(this.hakutoiveet).map(function(hakutoive) { return hakutoive.toJson() })})
-  },
-
-  setAsSaved: function() {
-    _(this.hakutoiveet).each(function(hakutoive) {
-        if (hakutoive.hasData()) hakutoive.setAsSaved() }
-    )
-  },
-
-  isValid: function() {
-    return _(this.hakutoiveet).every(function(hakutoive) { return hakutoive.isValid() })
-  },
-
-  getHakutoiveWatchCollection: function() {
-    return _(this.hakutoiveet).map(function(hakutoive) { return hakutoive.data })
-  },
-
-  moveHakutoive: function(from, to) {
-    this.hakutoiveet[from].setAsModified()
-    this.hakutoiveet[to].setAsModified()
-    this.hakutoiveet.splice(to, 0, this.hakutoiveet.splice(from, 1)[0])
-  },
-
-  getChangedItems: function() {
-    return _(this.hakutoiveet).chain()
-      .map(function(hakutoive, index) { return hakutoive.isModified ? index : null })
-      .without(null)
-      .value()
-  },
-
-  isEditable: function(index) {
-    var firstEditableIndex = _(this.hakutoiveet).filter(function(hakutoive) { return hakutoive.hasData() && hakutoive.isValid() }).length
-    return index >= 0 && index < this.hakutoiveet.length && index <= firstEditableIndex
-  }
-}
 
 listApp.controller("listController", ["$scope", "applicationsResource", function ($scope, applicationsResource) {
   $scope.applicationStatusMessage = "Hakemuksia ladataan...";
