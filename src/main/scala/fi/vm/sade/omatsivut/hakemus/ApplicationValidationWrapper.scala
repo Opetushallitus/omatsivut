cc4f47c0736faf11725041001adda9b084f34093
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-07-15 14:14:57 +0300

    Fix message

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationValidationWrapper.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationValidationWrapper.scala
@@ -23,7 +23,7 @@ case class ApplicationValidationWrapper(implicit val appConfig: AppConfig) exten
       Some(validationErrors, questions)
     } catch {
       case e: Exception => {
-        logger.error("There was an error finding missing questions from application: " + hakemus.oid + "error was: " + e.getMessage, e)
+        logger.error("There was an error finding missing questions from application: " + hakemus.oid + " error was: " + e.getMessage, e)
         None
       }
     }
