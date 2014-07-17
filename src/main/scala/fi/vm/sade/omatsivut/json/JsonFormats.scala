aad3a99fd796d9c50e9611a758ddc6bb0d05e643
Juha Paananen <juha.paananen@gmail.com>
2014-07-17 15:53:10 +0300

    cleanup

--- src/main/scala/fi/vm/sade/omatsivut/json/JsonFormats.scala
+++ src/main/scala/fi/vm/sade/omatsivut/json/JsonFormats.scala
@@ -19,25 +19,20 @@ class QuestionNodeSerializer extends Serializer[QuestionNode] {
       json match {
       case questionGroup: JObject if questionGroup.values.contains("questions") =>
         json.extract[QuestionGroup]
-      case question: JObject if question.values.get("questionType") == Some("Text") =>
-        json.extract[Text]
-      case question: JObject if question.values.get("questionType") == Some("TextArea") =>
-        json.extract[TextArea]
-      case question: JObject if question.values.get("questionType") == Some("Radio") =>
-        json.extract[Radio]
-      case question: JObject if question.values.get("questionType") == Some("Checkbox") =>
-        json.extract[Checkbox]
-      case question: JObject if question.values.get("questionType") == Some("Dropdown") =>
-        json.extract[Dropdown]
+      case question: JObject => question.values.get("questionType") match {
+        case Some("Text") => json.extract[Text]
+        case Some("TextArea") => json.extract[TextArea]
+        case Some("Radio") => json.extract[Radio]
+        case Some("Checkbox") => json.extract[Checkbox]
+        case Some("Dropdown") => json.extract[Dropdown]
+        case unknown => throw new MappingException("Unknown question type " + unknown)
+      }
+
       case x => throw new MappingException("Can't convert " + x + " to QuestionNode")
     }
   }
 
   override def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-    case any: QuestionNode =>
-      println(any)
-      val result = Extraction.decompose(any)(JsonFormats.genericFormats)
-      println(result)
-      result
+    case any: QuestionNode => Extraction.decompose(any)(JsonFormats.genericFormats)
   }
 }
