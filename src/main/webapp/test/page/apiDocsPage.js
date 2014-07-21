b217d411b6520f71f5326c229cdcc3c57fdc7c96
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2014-07-21 14:06:02 +0300

    Add tests for api-docs

---
+++ src/main/webapp/test/page/apiDocsPage.js
@@ -0,0 +1,24 @@
+function ApiDocsPage() {
+  var apiDocsPage = openPage("/omatsivut/api-docs/#!/api", visible)
+
+  var api = {
+    openPage: apiDocsPage,
+    
+    endpoints: function () {
+      return S("#api_endpoint_list>li")
+        .map(function () {
+          return {
+            method: $(this).find(".http_method>a").text().trim(),
+            path: $(this).find(".path>a").text().trim(),
+            description: $(this).find(".options>li>a").text().trim()
+          }
+        }).toArray()
+    }
+  }
+  return api
+
+  function visible() {
+    return api.endpoints().length > 0
+  }
+  
+}
