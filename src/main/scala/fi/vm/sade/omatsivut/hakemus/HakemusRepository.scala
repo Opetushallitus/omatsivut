2210e65b6442095693d3b18a10412cab9a7851c9
Juha Paananen <juha.paananen@gmail.com>
2014-07-21 13:36:10 +0300

    remove indirection (dao/service wrappers classes)

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusRepository.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusRepository.scala
@@ -1,16 +1,30 @@
 package fi.vm.sade.omatsivut.hakemus
 
+import fi.vm.sade.haku.oppija.hakemus.domain.Application
 import fi.vm.sade.omatsivut.AppConfig.AppConfig
 import fi.vm.sade.omatsivut._
 import fi.vm.sade.omatsivut.domain.Hakemus
 import java.util.Date
 
 case class HakemusRepository(implicit val appConfig: AppConfig) extends Logging {
-  def updateHakemus(hakemus: Hakemus) = {
-    ApplicationDaoWrapper().updateApplication(hakemus.copy(updated = new Date().getTime))
+  import collection.JavaConversions._
+  private val dao = appConfig.springContext.applicationDAO
+
+  def updateHakemus(hakemus: Hakemus): Hakemus = {
+    val updatedHakemus = hakemus.copy(updated = new Date().getTime)
+    val applicationQuery: Application = new Application().setOid(updatedHakemus.oid)
+    val applicationJavaObjects: List[Application] = dao.find(applicationQuery).toList
+    applicationJavaObjects.foreach { application =>
+      ApplicationUpdater.update(application, updatedHakemus)
+      dao.update(applicationQuery, application)
+    }
+    updatedHakemus
   }
 
-  def fetchHakemukset(oid: String): List[Hakemus] = {
-    ApplicationDaoWrapper().findByPersonOid(oid)
+  def fetchHakemukset(personOid: String): List[Hakemus] = {
+    val applicationJavaObjects: List[Application] = dao.find(new Application().setPersonOid(personOid)).toList
+    applicationJavaObjects.map{ application =>
+      HakemusConverter.convertToHakemus(HakuRepository().getHakuById(application.getApplicationSystemId))(application)
+    }
   }
 }
--- src/main/scala/fi/vm/sade/omatsivut/hakemus/HakuRepository.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/HakuRepository.scala
@@ -1,11 +1,23 @@
 package fi.vm.sade.omatsivut.hakemus
 
+import fi.vm.sade.haku.oppija.lomake.domain.ApplicationSystem
 import fi.vm.sade.omatsivut.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.domain.Haku
 
 case class HakuRepository(implicit val appConfig: AppConfig) {
+  private val repository = appConfig.springContext.applicationSystemService
+
   def getHakuById(id: String): Option[Haku] = id match {
     case "" => None
-    case applicationSystemId => ApplicationSystemServiceWrapper().findByOid(applicationSystemId)
+    case applicationSystemId => tryFind(applicationSystemId).map(HakuConverter.convertToHaku)
+  }
+
+  private def tryFind(applicationSystemOid: String): Option[ApplicationSystem] = {
+    try {
+      Some(repository.getApplicationSystem(applicationSystemOid))
+    } catch {
+      case e: Exception => None
+    }
   }
 }
+
