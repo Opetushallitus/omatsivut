cf060e35f9883165576020fc1aa84e1833f0134c
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-07-24 13:42:32 +0300

    Change TestHelper to only create fake shibboleth session
    Move all authentication cookie creation to SessionServlet

--- src/main/scala/fi/vm/sade/omatsivut/servlet/TestHelperServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/TestHelperServlet.scala
@@ -5,17 +5,14 @@ import fi.vm.sade.omatsivut.fixtures.FixtureImporter
 import fi.vm.sade.omatsivut.security.{ShibbolethCookie, AuthenticationCipher, AuthenticationInfoService}
 import org.scalatra.{Cookie, CookieOptions}
 
-class TestHelperServlet(implicit val appConfig: AppConfig) extends OmatSivutServletBase with AuthCookieCreating  {
+class TestHelperServlet(implicit val appConfig: AppConfig) extends OmatSivutServletBase  {
   if(appConfig.isTest){
     get("/fakesession") {
-      val hetuOption: Option[String] = paramOption("hetu")
       val shibbolethCookie = ShibbolethCookie("_shibsession_fakeshibbolethsession", AuthenticationCipher().encrypt("FAKESESSION"))
-      createAuthCookieCredentials(hetuOption, Some(shibbolethCookie), AuthenticationInfoService.apply) match {
-        case Some(credentials) => {
-          response.addCookie(fakeShibbolethSessionCookie(shibbolethCookie))
-          createAuthCookieResponse(credentials, response, redirectUri, CookieOptions(path = "/"))
-        }
-        case _ => response.redirect(ssoContextPath + "/Shibboleth.sso/LoginFI") //TODO Localization
+      response.addCookie(fakeShibbolethSessionCookie(shibbolethCookie))
+      paramOption("hetu") match {
+        case Some(hetu) => response.redirect(request.getContextPath + "/secure/initsession?hetu=" + hetu)
+        case _ => halt(400, "Can't fake session without ssn")
       }
     }
 
@@ -27,10 +24,4 @@ class TestHelperServlet(implicit val appConfig: AppConfig) extends OmatSivutServ
   def fakeShibbolethSessionCookie(shibbolethSessionData: ShibbolethCookie): Cookie = {
     Cookie(shibbolethSessionData.name, shibbolethSessionData.value)(CookieOptions(path = "/"))
   }
-
-  def redirectUri: String = {
-    request.getContextPath + paramOption("redirect").getOrElse("/index.html")
-  }
-
-  def ssoContextPath: String = "/omatsivut"
 }
