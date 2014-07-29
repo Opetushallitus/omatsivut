1b5c16ed00ac3b8bf85c35e9f6dec58ef051bcb5
Juha Paananen <juha.paananen@gmail.com>
2014-07-29 15:10:24 +0300

    cleanup

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/FormQuestionFinder.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/FormQuestionFinder.scala
@@ -1,14 +1,9 @@
 package fi.vm.sade.omatsivut.hakemus
 
-import fi.vm.sade.haku.oppija.lomake.domain.elements.custom.SocialSecurityNumber
 import fi.vm.sade.haku.oppija.lomake.domain.elements.questions.{DropdownSelect, TextQuestion, CheckBox => HakuCheckBox, OptionQuestion => HakuOption, Radio => HakuRadio, TextArea => HakuTextArea}
-import fi.vm.sade.haku.oppija.lomake.domain.elements._
-import fi.vm.sade.haku.oppija.lomake.util.ElementTree
+import fi.vm.sade.haku.oppija.lomake.domain.elements.{Titled, HiddenValue, TitledGroup, Element}
 import fi.vm.sade.haku.oppija.lomake.validation.validators.RequiredFieldValidator
-import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.OppijaConstants
 import fi.vm.sade.omatsivut.Logging
-import fi.vm.sade.omatsivut.domain.Hakemus.Answers
-import fi.vm.sade.omatsivut.domain.Text
 import fi.vm.sade.omatsivut.domain._
 import scala.collection.JavaConversions._
 import fi.vm.sade.omatsivut.domain.Notification
@@ -38,34 +33,36 @@ protected object FormQuestionFinder extends Logging {
     val element = elementWrapper.element
     def id = QuestionId(elementWrapper.phase.getId, elementWrapper.id)
     def isRequired = element.getValidators.filter(o => o.isInstanceOf[RequiredFieldValidator]).nonEmpty
-    def maxlength = toInt(element.getAttributes.toMap.getOrElse("maxlength", "500")).getOrElse(500)
-    def rows = toInt(element.getAttributes.toMap.getOrElse("rows", "3")).getOrElse(3)
-    def cols = toInt(element.getAttributes.toMap.getOrElse("cols", "80")).getOrElse(80)
-    def toInt(s: String):Option[Int] = { try { Some(s.toInt) } catch { case e:Exception => None } }
-    def containsCheckBoxes(e: TitledGroup): Boolean = {
-      getImmediateChildElementsOfType[HakuCheckBox](e).nonEmpty
-    }
 
     element match {
-      case e: TextQuestion => List(Text(id, title(e), helpText(e), isRequired, maxlength))
-      case e: HakuTextArea => List(TextArea(id, title(e), helpText(e), isRequired, maxlength, rows, cols))
-      case e: HakuRadio => List(Radio(id, title(e), helpText(e), options(e), isRequired))
-      case e: DropdownSelect => List(Dropdown(id, title(e), helpText(e), options(e), isRequired))
-      case e: TitledGroup if containsCheckBoxes(e) => List(Checkbox(id, title(e), helpText(e), options(elementWrapper), isRequired))
-      case e: TitledGroup => Nil
-      case e: HakuCheckBox => Nil
+      case e: TextQuestion =>
+        List(Text(id, title(e), helpText(e), isRequired, maxLength(e)))
+      case e: HakuTextArea =>
+        val rows = toInt(element.getAttributes.toMap.getOrElse("rows", "3")).getOrElse(3)
+        val cols = toInt(element.getAttributes.toMap.getOrElse("cols", "80")).getOrElse(80)
+        List(TextArea(id, title(e), helpText(e), isRequired, maxLength(e), rows, cols))
+      case e: HakuRadio =>
+        List(Radio(id, title(e), helpText(e), dropDownOrRadioOptions(e), isRequired))
+      case e: DropdownSelect =>
+        List(Dropdown(id, title(e), helpText(e), dropDownOrRadioOptions(e), isRequired))
+      case e: TitledGroup if containsCheckBoxes(e) =>
+        val checkboxOptions = elementWrapper.getChildElementsOfType[HakuCheckBox].map(o => AnswerOption(title(o), o.id))
+        List(Checkbox(id, title(e), helpText(e), checkboxOptions, isRequired))
       case e: fi.vm.sade.haku.oppija.lomake.domain.elements.Notification => List(Notification(id, title(e), e.getNotificationType()))
       case e: fi.vm.sade.haku.oppija.lomake.domain.elements.Text => List(Label(id, title(e)))
       case _ => Nil
     }
   }
 
-  private def options(e: HakuOption): List[AnswerOption] = {
+  private def containsCheckBoxes(e: TitledGroup): Boolean = {
+    getImmediateChildElementsOfType[HakuCheckBox](e).nonEmpty
+  }
+
+
+  private def dropDownOrRadioOptions(e: HakuOption): List[AnswerOption] = {
     e.getOptions.map(o => AnswerOption(title(o), o.getValue, o.isDefaultOption)).toList
   }
-  private def options(e: ElementWrapper): List[AnswerOption] = {
-    e.getChildElementsOfType[HakuCheckBox].map(o => AnswerOption(title(o), o.id))
-  }
+
 
   private def getImmediateChildElementsOfType[A](rootElement: Element)(implicit mf : Manifest[A]): List[A] = {
     rootElement.getChildren.toList.flatMap { child =>
@@ -96,4 +93,11 @@ protected object FormQuestionFinder extends Logging {
     else
       help.getTranslations.get("fi") // TODO: kieliversiot
   }
+
+  private def maxLength(element: Element) = {
+    toInt(element.getAttributes.toMap.getOrElse("maxlength", "500")).getOrElse(500)
+  }
+
+  private def toInt(s: String):Option[Int] = { try { Some(s.toInt) } catch { case e:Exception => None } }
+
 }
