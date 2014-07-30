5c45220414cf2b6f472af0fa1f3269c7626080a9
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-07-30 10:59:03 +0300

    Refactor audit logging introduce auditEvent

---
+++ src/main/scala/fi/vm/sade/omatsivut/auditlog/AuditEvent.scala
@@ -0,0 +1,60 @@
+package fi.vm.sade.omatsivut.auditlog
+
+import fi.vm.sade.log.model.Tapahtuma
+import fi.vm.sade.omatsivut.domain.Hakemus.Answers
+import fi.vm.sade.omatsivut.security.CookieCredentials
+
+trait AuditEvent {
+  def target: String
+  def targetType: String
+  def systemName = "omatsivut"
+  def toTapahtuma: Tapahtuma
+  def toLogMessage: String
+}
+
+case class Login(credentials: CookieCredentials, target: String = "Session") extends AuditEvent {
+  def targetType = "Luotu eväste sisällöllä: " + credentials.toString
+  def toTapahtuma = Tapahtuma.createTRACE(systemName, target, targetType, System.currentTimeMillis())
+  def toLogMessage = "Could not write Logout to auditlog, message was: " + targetType
+}
+case class Logout(credentials: CookieCredentials, target: String = "Session") extends AuditEvent {
+  def targetType = "Käyttäjä kirjautui ulos: " + credentials.toString
+  def toTapahtuma = Tapahtuma.createTRACE(systemName, target, targetType, System.currentTimeMillis())
+  def toLogMessage = "Could not write Logout to auditlog, message was: " + targetType
+}
+case class SessionTimeout(credentials: CookieCredentials, target: String = "Session") extends AuditEvent {
+  def targetType = "Poistettu eväste sisällöllä: " + credentials.toString
+  def toTapahtuma = Tapahtuma.createTRACE(systemName, target, targetType, System.currentTimeMillis())
+  def toLogMessage = "Could not write SessionTimeout to auditlog, message was: " + targetType
+}
+case class ShowHakemus(userOid: String, hakemusOid: String, target: String = "Hakemus") extends AuditEvent {
+  def targetType = "Haettu hakemus: " + hakemusOid
+  def toTapahtuma = Tapahtuma.createREAD(systemName, userOid, target, targetType)
+  def toLogMessage = "Could not write ShowApplication to auditlog, message was: " + targetType + " with " + userOid
+}
+case class UpdateHakemus(userOid: String, hakemusOid: String, originalAnswers: Answers, updatedAnswers: Answers, target: String = "Hakemus") extends AuditEvent {
+  def targetType = "Tallennettu päivitetty hakemus: " + hakemusOid
+  def toTapahtuma = {
+    val tapahtuma = Tapahtuma.createUPDATE(systemName, userOid, target, targetType)
+    val phaseIds = originalAnswers.keySet ++ updatedAnswers.keySet
+    phaseIds foreach {
+      phaseId => addPhaseAnswersDiff(tapahtuma, phaseId, originalAnswers.getOrElse(phaseId, Map()), updatedAnswers.getOrElse(phaseId, Map()))
+    }
+    tapahtuma
+  }
+
+  private def addPhaseAnswersDiff(tapahtuma: Tapahtuma, phaseId: String, originalAnswers: Map[String,String], updatedAnswers: Map[String,String]) {
+    val allKeys = originalAnswers.keySet ++ updatedAnswers.keySet
+    allKeys foreach {
+      key => {
+        val oldValue = originalAnswers.get(key)
+        val newValue = updatedAnswers.get(key)
+        if(!oldValue.equals(newValue)) {
+          tapahtuma.addValueChange("phase_" + phaseId + "-key_" + key, oldValue.getOrElse(null), newValue.getOrElse(null));
+        }
+      }
+    }
+  }
+
+  def toLogMessage = "Could not write UpdateHakemus to auditlog, message was: " + targetType + " with " + userOid
+}
--- src/main/scala/fi/vm/sade/omatsivut/auditlog/AuditLogger.scala
+++ src/main/scala/fi/vm/sade/omatsivut/auditlog/AuditLogger.scala
@@ -1,6 +1,5 @@
 package fi.vm.sade.omatsivut.auditlog
 
-import fi.vm.sade.log.model.Tapahtuma
 import fi.vm.sade.omatsivut.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.Logging
 import fi.vm.sade.omatsivut.domain.Hakemus
@@ -9,57 +8,35 @@ import fi.vm.sade.omatsivut.security.CookieCredentials
 
 object AuditLogger extends Logging  {
   private def auditLogger(implicit appConfig: AppConfig) = appConfig.springContext.auditLogger
-  private val systemName = "omatsivut"
-  
+
   def logCreateSession(credentials: CookieCredentials)(implicit appConfig: AppConfig) {
-    withErrorLogging {
-      val tapahtuma = Tapahtuma.createTRACE(systemName, "Session", "Luotu eväste sisällöllä: " + credentials.toString, System.currentTimeMillis())
-      auditLogger.log(tapahtuma)
-    }("Could not logCreateSession for " + credentials.oid)
+    val logMessage = Login(credentials)
+    auditLog(logMessage)
   }
 
   def logUpdatedHakemus(userOid: String, applicationOid: String, originalAnswers: Answers, updatedAnswers: Answers)(implicit appConfig: AppConfig) {
-    withErrorLogging {
-      val tapahtuma = Tapahtuma.createUPDATE(systemName, userOid, "Hakemus", "Tallennettu päivitetty hakemus: " + applicationOid)
-      val phaseIds = originalAnswers.keySet ++ updatedAnswers.keySet
-      phaseIds foreach {
-        phaseId => addPhaseAnswersDiff(tapahtuma, phaseId, originalAnswers.getOrElse(phaseId, Map()), updatedAnswers.getOrElse(phaseId, Map()))
-      }
-      auditLogger.log(tapahtuma)
-    }("Could not logUpdatedHakemus for " + userOid)
-  }
-
-  private def addPhaseAnswersDiff(tapahtuma: Tapahtuma, phaseId: String, originalAnswers: Map[String,String], updatedAnswers: Map[String,String]) {
-    val allKeys = originalAnswers.keySet ++ updatedAnswers.keySet
-    allKeys foreach {
-      key => {
-        val oldValue = originalAnswers.get(key)
-        val newValue = updatedAnswers.get(key)
-        if(!oldValue.equals(newValue)) {
-          tapahtuma.addValueChange("phase_" + phaseId + "-key_" + key, oldValue.getOrElse(null), newValue.getOrElse(null));
-        }
-      }
-    }
+    val logMessage = UpdateHakemus(userOid, applicationOid, originalAnswers, updatedAnswers)
+    auditLog(logMessage)
   }
 
   def logFetchHakemus(userOid: String, hakemus: Hakemus)(implicit appConfig: AppConfig) {
-    withErrorLogging {
-      val tapahtuma = Tapahtuma.createREAD(systemName, userOid, "Hakemus", "Haettu hakemus: " + hakemus.oid )
-      auditLogger.log(tapahtuma)
-    }("Could not logFetchHakemus for " + userOid)
+    val logMessage = ShowHakemus(userOid, hakemus.oid)
+    auditLog(logMessage)
   }
 
   def logSessionTimeout(credentials: CookieCredentials)(implicit appConfig: AppConfig) {
-    withErrorLogging {
-      val tapahtuma = Tapahtuma.createTRACE(systemName, "Session", "Poistettu eväste sisällöllä: " + credentials.toString, System.currentTimeMillis())
-      auditLogger.log(tapahtuma)
-    }("Could not logSessionTimeout for " + credentials.oid)
+    val logMessage = SessionTimeout(credentials)
+    auditLog(logMessage)
   }
 
   def logLogout(credentials: CookieCredentials)(implicit appConfig: AppConfig) {
+    val logMessage = Logout(credentials)
+    auditLog(logMessage)
+  }
+
+  def auditLog(audit: AuditEvent)(implicit appConfig: AppConfig) {
     withErrorLogging {
-      val tapahtuma = Tapahtuma.createTRACE(systemName, "Session", "Käyttäjä kirjautui ulos: " + credentials.toString, System.currentTimeMillis())
-      auditLogger.log(tapahtuma)
-    }("Could not logLogout for " + credentials.oid)
+      auditLogger.log(audit.toTapahtuma)
+    }(audit.toLogMessage)
   }
 }
