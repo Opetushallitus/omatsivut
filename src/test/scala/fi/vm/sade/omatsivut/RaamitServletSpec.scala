d6829e637f2d744a051734e2beb65b2dd7dad600
Juha Paananen <juha.paananen@gmail.com>
2014-08-05 13:47:42 +0300

    fix test

--- src/test/scala/fi/vm/sade/omatsivut/RaamitServletSpec.scala
+++ src/test/scala/fi/vm/sade/omatsivut/RaamitServletSpec.scala
@@ -3,12 +3,11 @@ package fi.vm.sade.omatsivut
 import fi.vm.sade.omatsivut.servlet.RaamitServlet
 
 class RaamitServletSpec extends ScalatraTestSupport  {
-
   "GET /raamit" should {
-    "point to localhost oppija-raamit" in {
+    "point to oppija-raamit" in {
       get("/raamit/load") {
         status must_== 200
-        response.body.indexOf("http://localhost:8099/oppija-raamit/apply-raamit.js") >= 0 must beTrue
+        response.body.indexOf("/oppija-raamit/apply-raamit.js") >= 0 must beTrue
       }
     }
   }
