0fe468e7cc81601fa2a5906b20ca089146f30ced
Juha Paananen <juha.paananen@gmail.com>
2014-08-06 14:22:50 +0300

    update todos

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusPreviewGenerator.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusPreviewGenerator.scala
@@ -33,7 +33,7 @@ case class HakemusPreviewGenerator(implicit val appConfig: AppConfig, val langua
     val form = ElementWrapper.wrapFiltered(applicationSystem.getForm, answers)
 
     def questionsPreview(element: ElementWrapper): List[TypedTag[String]] = {
-      // TODO: GradeGrid, PrecerenceTable, DateQuestion, DiscretionaryAttachments
+      // TODO: PreferenceTable, DateQuestion, DiscretionaryAttachments
 
       element.element match {
         case _: GradeGrid => List(gradeGridPreview(element))
