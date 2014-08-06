ab473c0a965f00a86d87808130d5a3c98ff38f53
Juha Paananen <juha.paananen@gmail.com>
2014-08-06 10:21:47 +0300

    fetch application for preview

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusPreviewGenerator.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/HakemusPreviewGenerator.scala
@@ -1,10 +1,17 @@
 package fi.vm.sade.omatsivut.hakemus
 
+import fi.vm.sade.haku.oppija.hakemus.domain.Application
 import fi.vm.sade.omatsivut.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.Logging
 
 case class HakemusPreviewGenerator(implicit val appConfig: AppConfig) extends Logging {
+  private val dao = appConfig.springContext.applicationDAO
+
   def generatePreview(personOid: String, applicationOid: String): Option[String] = {
-    Some("<div>preview</div>")
+    import collection.JavaConversions._
+    val applicationQuery: Application = new Application().setOid(applicationOid).setPersonOid(personOid)
+    dao.find(applicationQuery).toList.headOption.map {
+      application => "<div>preview</div>"
+    }
   }
 }
