64a19c2d8d5a053df39aa83a7aa0c3d7b932482b
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-08-08 14:19:52 +0300

    add error logging

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/HakuRepository.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/HakuRepository.scala
@@ -5,8 +5,9 @@ import fi.vm.sade.omatsivut.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.domain.Haku
 import fi.vm.sade.omatsivut.domain.Language
 import fi.vm.sade.omatsivut.ohjausparametrit.OhjausparametritService
+import fi.vm.sade.omatsivut.Logging
 
-case class HakuRepository(implicit val appConfig: AppConfig) {
+case class HakuRepository(implicit val appConfig: AppConfig) extends Logging {
   private val repository = appConfig.springContext.applicationSystemService
 
   def getHakuById(id: String)(implicit lang: Language.Language): Option[Haku] = id match {
@@ -22,7 +23,9 @@ case class HakuRepository(implicit val appConfig: AppConfig) {
     try {
       Some(repository.getApplicationSystem(applicationSystemOid))
     } catch {
-      case e: Exception => None
+      case e: Exception =>
+        logger.error("applicationSystem loading failed", e)
+        None
     }
   }
 }
