1fd76496925373964874b0be607894297fdcf555
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-08-18 12:57:03 +0300

    Do audit logging in separate thread

--- src/main/scala/fi/vm/sade/omatsivut/auditlog/AuditLogger.scala
+++ src/main/scala/fi/vm/sade/omatsivut/auditlog/AuditLogger.scala
@@ -3,12 +3,16 @@ package fi.vm.sade.omatsivut.auditlog
 import fi.vm.sade.omatsivut.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.Logging
 
-object AuditLogger extends Logging  {
+object AuditLogger extends Logging {
   private def auditLogger(implicit appConfig: AppConfig) = appConfig.springContext.auditLogger
 
   def log(audit: AuditEvent)(implicit appConfig: AppConfig) {
-    withErrorLogging {
-      auditLogger.log(audit.toTapahtuma)
-    }(audit.toLogMessage)
+    new Thread(new Runnable {
+      override def run() {
+        withErrorLogging {
+          auditLogger.log(audit.toTapahtuma)
+        }(audit.toLogMessage)
+      }
+    }).start()
   }
 }
