bab82781fefb436d6824f545ead4b2d720d51b5b
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-08-26 09:01:59 +0300

    add localized login link to "not logged in" error

--- src/main/js/directives.js
+++ src/main/js/directives.js
@@ -139,6 +139,22 @@ module.exports = function(listApp) {
     }
   })
 
+  listApp.directive("localizedLink", ["$sce", "localization", function($sce, localization) {
+    return {
+      restrict: "E",
+      template: "",
+      link: function(scope, element, attrs) {
+        var linkString = localization(attrs["key"])
+        var link = $("<span>" + linkString + "</span>")
+          .find("a")
+          .attr("href", attrs["href"])
+          .attr("target", attrs["target"])
+          .end()
+        element.append(link)
+      }
+    }
+  }])
+
   listApp.directive("questionTemplate", function(RecursionHelper) {
     return {
       restrict: 'E',
--- src/main/js/listController.js
+++ src/main/js/listController.js
@@ -1,28 +1,32 @@
 var Hakemus = require('./hakemus')
 module.exports = function(listApp) {
   listApp.controller("listController", ["$scope", "restResources", "localization", function ($scope, restResources, localization) {
-    $scope.applicationStatusMessage = localization("message.loadingApplications")
-    $scope.applicationStatusMessageType = "ajax-spinner";
+    $scope.applicationStatusMessage = "message.loadingApplications"
+    $scope.applicationStatusMessageType = "ajax-spinner"
     restResources.applications.query(success, error)
 
     function success(data) {
       $scope.applications = _.map(data, function(json) { return new Hakemus(json) })
       if($scope.applications.length > 0) {
-        $scope.applicationStatusMessage = "";
-        $scope.applicationStatusMessageType = "";
+        $scope.applicationStatusMessage = ""
+        $scope.applicationStatusMessageType = ""
       }
       else {
-        $scope.applicationStatusMessage = localization("message.noApplications");
-        $scope.applicationStatusMessageType = "info";
+        $scope.applicationStatusMessage = "message.noApplications"
+        $scope.applicationStatusMessageType = "info"
       }
     }
 
     function error(err) {
       switch (err.status) {
-        case 401: $scope.applicationStatusMessage = localization("error.loadingFailed_notLoggedIn"); break;
-        default: $scope.applicationStatusMessage = localization("error.loadingFailed");
+        case 401:
+          $scope.applicationStatusMessage = "error.loadingFailed_notLoggedIn"
+          $scope.applicationStatusMessageType = "info"
+          break;
+        default:
+          $scope.applicationStatusMessage = "error.loadingFailed"
+          $scope.applicationStatusMessageType = "error"
       }
-      $scope.applicationStatusMessageType = "error"
       $scope.applications = [];
     }
   }]);
