642022493846df15252aaf6fa15816360fb95eef
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-09-15 09:37:38 +0300

    Use import

--- src/main/scala/fi/vm/sade/omatsivut/json/JsonFormats.scala
+++ src/main/scala/fi/vm/sade/omatsivut/json/JsonFormats.scala
@@ -1,12 +1,12 @@
 package fi.vm.sade.omatsivut.json
 
-import fi.vm.sade.omatsivut.hakemus.domain.HakutoiveenValintatulosTila.HakutoiveenValintatulosTila
 import fi.vm.sade.omatsivut.hakemus.domain._
 import org.json4s._
+import org.json4s.ext.EnumNameSerializer
 
 object JsonFormats {
   val genericFormats = DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all
-  val jsonFormats: Formats = JsonFormats.genericFormats ++ List(new QuestionNodeSerializer, new HakemusMuutosSerializer, new org.json4s.ext.EnumNameSerializer(HakutoiveenValintatulosTila))
+  val jsonFormats: Formats = JsonFormats.genericFormats ++ List(new QuestionNodeSerializer, new HakemusMuutosSerializer, new EnumNameSerializer(HakutoiveenValintatulosTila))
 }
 
 trait JsonFormats {
