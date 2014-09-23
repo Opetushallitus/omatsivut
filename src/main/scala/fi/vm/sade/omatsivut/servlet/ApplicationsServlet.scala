ffa3238b0bbda4a93e999af4f3999ac9dea43152
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2014-09-23 21:27:28 +0300

    Update swagger documentation

--- src/main/scala/fi/vm/sade/omatsivut/servlet/ApplicationsServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/ApplicationsServlet.scala
@@ -16,6 +16,8 @@ import org.scalatra.json._
 import org.scalatra.swagger.SwaggerSupportSyntax.OperationBuilder
 import org.scalatra.swagger._
 import org.scalatra.{BadRequest, Forbidden, NotFound, Ok}
+import org.scalatra.ActionResult
+import org.scalatra.ActionResult
 
 trait ApplicationsServletContainer {
   this: HakuRepositoryComponent with
@@ -39,30 +41,23 @@ trait ApplicationsServletContainer {
 
     protected val applicationDescription = "Oppijan henkilökohtaisen palvelun REST API, jolla voi hakea ja muokata hakemuksia ja omia tietoja"
 
-    val getApplicationsSwagger: OperationBuilder = (apiOperation[List[Hakemus]]("getApplications")
-      summary "Hae kirjautuneen oppijan hakemukset"
-      )
-
-    val putApplicationsSwagger = (apiOperation[Unit]("putApplication")
-      summary "Tallenna hakemus"
-      )
-
-    val validateApplicationsSwagger = (apiOperation[Unit]("validateApplication")
-      summary "Tarkista hakemus ja palauta virheet sekä kysymykset joihin ei ole vastattu"
-      )
-
-    val previewApplicationSwagger: OperationBuilder = (apiOperation[String]("previewApplication")
-      summary "Hakemuksen esikatselu HTML-muodossa"
-    )
-
     before() {
       contentType = formats("json")
     }
 
+    val getApplicationsSwagger: OperationBuilder = (apiOperation[List[Hakemus]]("getApplications")
+      summary "Hae kirjautuneen oppijan hakemukset"
+    )
     get("/applications", operation(getApplicationsSwagger)) {
       hakemusRepository.fetchHakemukset(personOid())
     }
 
+    val putApplicationsSwagger = (apiOperation[Hakemus]("putApplication")
+      summary "Tallenna muokattu hakemus"
+      notes "Palauttaa tallennetun hakemuksen (tyyppiä Hakemus) ok tapauksessa ja listan virheitä (tyyppiä ValidationError) virhetapauksessa"
+      parameter pathParam[String]("oid").description("Hakemuksen oid")
+      parameter bodyParam[HakemusMuutos]("updated").description("Päivitetty hakemus")
+    )
     put("/applications/:oid", operation(putApplicationsSwagger)) {
       val content: String = request.body
       val updated = Serialization.read[HakemusMuutos](content)
@@ -78,6 +73,12 @@ trait ApplicationsServletContainer {
       }
     }
 
+    val validateApplicationsSwagger = (apiOperation[ValidationResult]("validateApplication")
+      summary "Tarkista hakemus ja palauta virheet sekä pyydettyjen kohteiden kysymykset"
+      parameter queryParam[String]("questionsOf").description("Hakukohteiden oidit joiden kysymykset halultaan. Pilkulla eroteltuna")
+      parameter pathParam[String]("oid").description("Hakemuksen oid")
+      parameter bodyParam[HakemusMuutos]("muutos").description("Päivitetty hakemus")
+    )
     post("/applications/validate/:oid", operation(validateApplicationsSwagger)) {
       val muutos = Serialization.read[HakemusMuutos](request.body)
       val applicationSystem = applicationSystemService.getApplicationSystem(muutos.hakuOid)
@@ -86,7 +87,12 @@ trait ApplicationsServletContainer {
       ValidationResult(errors, questions, hakuRepository.getApplicationPeriods(updatedApplication, applicationSystem))
     }
 
-    get("/applications/preview/:oid") {
+
+    val previewApplicationSwagger: OperationBuilder = (apiOperation[String]("previewApplication")
+      summary "Hakemuksen esikatselu HTML-muodossa"
+      parameter pathParam[String]("oid").description("Hakemuksen oid")
+    )
+    get("/applications/preview/:oid", operation(previewApplicationSwagger)) {
       newHakemusPreviewGenerator(language).generatePreview(ServerContaxtPath(request), personOid(), params("oid")) match {
         case Some(previewHtml) =>
           contentType = formats("html")
@@ -96,7 +102,13 @@ trait ApplicationsServletContainer {
       }
     }
 
-    post("/applications/vastaanota/:hakuOid/:hakemusOid") {
+    val postVastaanotaSwagger: OperationBuilder = (apiOperation[String]("previewApplication")
+      summary "Tallenna opiskelupaikan vastaanottotieto"
+      parameter pathParam[String]("hakemusOid").description("Hakemuksen oid")
+      parameter pathParam[String]("hakuOid").description("Haun oid")
+      parameter bodyParam[ClientSideVastaanotto]("vastaanotto").description("Vastaanottotilan muutostieto")
+    )
+    post("/applications/vastaanota/:hakuOid/:hakemusOid", operation(postVastaanotaSwagger)) {
       val hakemusOid = params("hakemusOid")
       val hakuOid = params("hakuOid")
       if (!hakemusRepository.exists(personOid(), hakuOid, hakemusOid)) {
