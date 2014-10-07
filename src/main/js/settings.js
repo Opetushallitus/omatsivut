04e777dbe20ca746da6aeb4b6ccf35e54cee800d
Juha Paananen <juha.paananen@gmail.com>
2014-10-07 10:29:20 +0300

    drop delay to zero in tests

--- src/main/js/settings.js
+++ src/main/js/settings.js
@@ -2,7 +2,7 @@ module.exports = function(listApp, isTestMode) {
   listApp.factory("settings", ["$animate", function($animate) {
     if (isTestMode) $animate.enabled(false)
     return {
-      uiTransitionTime: isTestMode ? 10 : 500,
+      uiTransitionTime: isTestMode ? 0 : 500,
       modelDebounce: isTestMode ? 0 : 300,
       uiIndicatorDebounce: isTestMode ? 0: 500
     };
