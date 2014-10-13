0475dcbb23b1ccf0b0f716b25ec84d1826eea219
Juha Paananen <juha.paananen@gmail.com>
2014-10-13 12:37:43 +0300

    try to make mocha test more reliable

--- src/main/webapp/test/spec/applicationListSpec.js
+++ src/main/webapp/test/spec/applicationListSpec.js
@@ -48,7 +48,7 @@
     })
   })
 
-  describe.skip('Kun käyttäjän oid puuttuu', function () {
+  describe('Kun käyttäjän oid puuttuu', function () {
     function oidNotFoundPageVisible() {
       return S(".no-applications").is(":visible")
     }
