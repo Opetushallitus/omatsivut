3d40bc5a2620d81c0a77c752f8670fe90a58ec55
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-10-14 11:22:56 +0300

    Handle hakutyyppi properly and fix swagger problem with enumeration

--- src/main/scala/fi/vm/sade/omatsivut/tarjonta/Haku.scala
+++ src/main/scala/fi/vm/sade/omatsivut/tarjonta/Haku.scala
@@ -6,7 +6,7 @@ import org.joda.time.Interval
 
 object Haku {
   def apply(tarjontaHaku: TarjontaHaku) : Haku = {
-    Haku(tarjontaHaku.oid, tarjontaHaku.hakuaikas.map(h => Hakuaika(h)), HaunTyyppi(tarjontaHaku), isKorkeakouluhaku(tarjontaHaku))
+    Haku(tarjontaHaku.oid, tarjontaHaku.hakuaikas.map(h => Hakuaika(h)), HaunTyyppi(tarjontaHaku).toString, isKorkeakouluhaku(tarjontaHaku))
   }
 
   private def isKorkeakouluhaku(tarjontaHaku: TarjontaHaku) = {
@@ -24,25 +24,29 @@ object Hakuaika {
   }
 }
 
-case class Haku(oid: String, hakuajat: List[Hakuaika], tyyppi: HaunTyyppi, korkeakouluHaku: Boolean, tulosaikataulu: Option[Tulosaikataulu] = None)
+case class Haku(oid: String, hakuajat: List[Hakuaika], tyyppi: String, korkeakouluHaku: Boolean, tulosaikataulu: Option[Tulosaikataulu] = None)
 case class Hakuaika(id: String, start: Long, end: Long, active: Boolean)
 
 object HaunTyyppi extends Enumeration {
   type HaunTyyppi = Value
-  val Yhteishaku, Erillishaku, JatkuvaHaku, Lisahaku = Value
+  val Yhteishaku = Value("YHTEISHAKU")
+  val Erillishaku = Value("ERILLISHAKU")
+  val JatkuvaHaku = Value("JATKUVA_HAKU")
+  val Lisahaku = Value("LISAHAKU")
 
   def apply(tarjontaHaku: TarjontaHaku) = {
-    if(tarjontaHaku.hakutyyppiUri == "hakutyyppi_03") {
+    if(tarjontaHaku.hakutyyppiUri.contains("hakutyyppi_03")) {
       Lisahaku
     } else {
-      if(tarjontaHaku.hakutapaUri == "hakutapa_01") {
+      if(tarjontaHaku.hakutapaUri.contains("hakutapa_01")) {
         Yhteishaku
-      } else if (tarjontaHaku.hakutapaUri == "hakutapa_02") {
+      } else if (tarjontaHaku.hakutapaUri.contains("hakutapa_02")) {
         Erillishaku
-      } else if(tarjontaHaku.hakutapaUri == "hakutapa_03") {
+      } else if(tarjontaHaku.hakutapaUri.contains("hakutapa_03")) {
         JatkuvaHaku
+      } else {
+        throw new IllegalArgumentException("Unsupported type for haku: " + tarjontaHaku.oid + " - " + tarjontaHaku.hakutyyppiUri + "," + tarjontaHaku.hakutapaUri)
       }
-      throw new IllegalArgumentException("Unsupported type for haku: " + tarjontaHaku.oid + " - " + tarjontaHaku.hakutyyppiUri + "," + tarjontaHaku.hakutapaUri)
     }
   }
 }
