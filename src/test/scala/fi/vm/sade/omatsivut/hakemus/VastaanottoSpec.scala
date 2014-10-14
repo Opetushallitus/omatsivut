1bbaf7f02a646505abb20a131a32b6bf530500f5
Juha Paananen <juha.paananen@gmail.com>
2014-10-14 12:46:18 +0300

    don't use legacy fixtures from valinta-tulos-service

--- src/test/scala/fi/vm/sade/omatsivut/hakemus/VastaanottoSpec.scala
+++ src/test/scala/fi/vm/sade/omatsivut/hakemus/VastaanottoSpec.scala
@@ -14,7 +14,7 @@ class VastaanottoSpec extends HakemusApiSpecification with FixturePerson {
 
   "POST /applications/vastaanota/:hakuOid/:hakemusOid" should {
     "vastaanottaa paikan" in {
-      new RemoteValintatulosService(appConfig.settings.valintaTulosServiceUrl).applyFixture("hyvaksytty-ilmoitettu")
+      new RemoteValintatulosService(appConfig.settings.valintaTulosServiceUrl).applyFixture("hyvaksytty-kesken-julkaistavissa")
 
       authPost("/applications/vastaanota/1.2.246.562.5.2013080813081926341928/1.2.246.562.11.00000441369", Serialization.write(ClientSideVastaanotto("1.2.246.562.5.72607738902", "VASTAANOTTANUT"))) {
         status must_== 200
@@ -22,7 +22,7 @@ class VastaanottoSpec extends HakemusApiSpecification with FixturePerson {
     }
 
     "hylkää pyynnön väärältä henkilöltä" in {
-      new RemoteValintatulosService(appConfig.settings.valintaTulosServiceUrl).applyFixture("hyvaksytty-ilmoitettu")
+      new RemoteValintatulosService(appConfig.settings.valintaTulosServiceUrl).applyFixture("hyvaksytty-kesken-julkaistavissa")
 
       authPost("/applications/vastaanota/1.2.246.562.5.2013080813081926341928/1.2.246.562.11.00000441369", Serialization.write(ClientSideVastaanotto("1.2.246.562.5.72607738902", "VASTAANOTTANUT"))) {
         status must_== 404
