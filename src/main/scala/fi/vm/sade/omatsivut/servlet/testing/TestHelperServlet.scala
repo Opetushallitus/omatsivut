bd8775feb51c3baaffc9479f5859a73887497ac8
Juha Paananen <juha.paananen@gmail.com>
2014-10-16 15:16:26 +0300

    remove dead code

--- src/main/scala/fi/vm/sade/omatsivut/servlet/testing/TestHelperServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/testing/TestHelperServlet.scala
@@ -2,8 +2,8 @@ package fi.vm.sade.omatsivut.servlet.testing
 
 import fi.vm.sade.omatsivut.config.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.config.SpringContextComponent
-import fi.vm.sade.omatsivut.fixtures.FixtureImporter
-import fi.vm.sade.omatsivut.security.{FakeAuthentication, AuthenticationInfoService, AuthenticationCipher, ShibbolethCookie}
+import fi.vm.sade.omatsivut.fixtures.{FixtureImporter, TestFixture}
+import fi.vm.sade.omatsivut.security.{AuthenticationCipher, FakeAuthentication, ShibbolethCookie}
 import fi.vm.sade.omatsivut.servlet.OmatSivutServletBase
 import fi.vm.sade.omatsivut.util.Timer
 import fi.vm.sade.omatsivut.valintatulokset.ValintatulosServiceComponent
@@ -13,7 +13,6 @@ trait TestHelperServletContainer {
   this: ValintatulosServiceComponent with SpringContextComponent =>
 
   def newTestHelperServlet: TestHelperServlet
-  def authenticationInfoService: AuthenticationInfoService
 
   class TestHelperServlet(val appConfig: AppConfig) extends OmatSivutServletBase  {
     if(appConfig.usesFakeAuthentication) {
@@ -22,7 +21,7 @@ trait TestHelperServletContainer {
         response.addCookie(fakeShibbolethSessionCookie(shibbolethCookie))
         paramOption("hetu") match {
           case Some(hetu) =>
-            response.addCookie(Cookie(FakeAuthentication.oidCookie, authenticationInfoService.getHenkiloOID(hetu).getOrElse(""))(appConfig.authContext.cookieOptions))
+            response.addCookie(Cookie(FakeAuthentication.oidCookie, TestFixture.persons.get(hetu).getOrElse(""))(appConfig.authContext.cookieOptions))
             response.redirect(request.getContextPath + "/secure/initsession?hetu=" + hetu)
           case _ => halt(400, "Can't fake session without ssn")
         }
--- src/test/scala/fi/vm/sade/omatsivut/config/AppConfigSpec.scala
+++ src/test/scala/fi/vm/sade/omatsivut/config/AppConfigSpec.scala
@@ -2,7 +2,6 @@ package fi.vm.sade.omatsivut.config
 
 import fi.vm.sade.omatsivut.config.AppConfig.{AppConfig, ExampleTemplatedProps}
 import fi.vm.sade.omatsivut.mongo.EmbeddedMongo
-import fi.vm.sade.omatsivut.security.AuthenticationInfoService
 import org.specs2.mutable.Specification
 
 class AppConfigSpec extends Specification {
