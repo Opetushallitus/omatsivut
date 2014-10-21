1d74f4c33b219a1f8b1d09506a24b69ba41940aa
Juha Paananen <juha.paananen@gmail.com>
2014-10-21 13:00:31 +0300

    use random local port for embedded mongo

--- src/test/scala/fi/vm/sade/omatsivut/ValintatulosServiceRunner.scala
+++ src/test/scala/fi/vm/sade/omatsivut/ValintatulosServiceRunner.scala
@@ -2,6 +2,7 @@ package fi.vm.sade.omatsivut
 
 import java.nio.file.{Paths, Files}
 
+import fi.vm.sade.omatsivut.mongo.EmbeddedMongo
 import fi.vm.sade.omatsivut.util.{Logging, PortChecker}
 
 object ValintatulosServiceRunner extends Logging {
@@ -18,7 +19,7 @@ object ValintatulosServiceRunner extends Logging {
           val cwd = new java.io.File(path)
           val javaHome = System.getProperty("JAVA8_HOME", "")
           Process(List("./sbt", "test:compile"), cwd, "JAVA_HOME" -> javaHome).!
-          val process = Process(List("./sbt", "test:run-main fi.vm.sade.valintatulosservice.JettyLauncher", "-Dvalintatulos.profile=it-externalHakemus", "-Dfile.encoding=UTF-8"), cwd, "JAVA_HOME" -> javaHome).run(true)
+          val process = Process(List("./sbt", "test:run-main fi.vm.sade.valintatulosservice.JettyLauncher", "-Dvalintatulos.profile=it-externalHakemus", "-Dhakemus.embeddedmongo.port=" + EmbeddedMongo.port,  "-Dfile.encoding=UTF-8"), cwd, "JAVA_HOME" -> javaHome).run(true)
           for (i <- 0 to 60 if PortChecker.isFreeLocalPort(valintatulosPort)) {
             Thread.sleep(1000)
           }
