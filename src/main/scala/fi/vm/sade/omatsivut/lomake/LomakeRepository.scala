554066bc3b8df1578627a2e819c606843556f971
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-11-04 15:13:18 +0200

    Add cache reporting to lomake repository

--- src/main/scala/fi/vm/sade/omatsivut/lomake/LomakeRepository.scala
+++ src/main/scala/fi/vm/sade/omatsivut/lomake/LomakeRepository.scala
@@ -1,14 +1,16 @@
 package fi.vm.sade.omatsivut.lomake
 
 import fi.vm.sade.haku.oppija.hakemus.domain.Application
+import fi.vm.sade.haku.oppija.lomake.service.impl.ApplicationSystemServiceImpl
 import fi.vm.sade.omatsivut.config.SpringContextComponent
 import fi.vm.sade.omatsivut.domain.Language
-import fi.vm.sade.omatsivut.lomake.domain.Lomake
 import fi.vm.sade.omatsivut.koulutusinformaatio.{KoulutusInformaatioComponent, KoulutusInformaatioService}
+import fi.vm.sade.omatsivut.lomake.domain.Lomake
 import fi.vm.sade.omatsivut.ohjausparametrit.{OhjausparametritComponent, OhjausparametritService}
 import fi.vm.sade.omatsivut.tarjonta.{Haku, Hakuaika, TarjontaComponent, TarjontaService}
 import fi.vm.sade.omatsivut.util.Logging
 import fi.vm.sade.omatsivut.util.Timer.timed
+import org.joda.time.DateTime
 
 trait LomakeRepositoryComponent {
   this: OhjausparametritComponent with KoulutusInformaatioComponent with SpringContextComponent with TarjontaComponent =>
@@ -20,6 +22,7 @@ trait LomakeRepositoryComponent {
 
   class RemoteLomakeRepository extends LomakeRepository with Logging {
     private val repository = springContext.applicationSystemService
+    private[this] var lastReport = new DateTime()
 
     def lomakeByOid(oid: String): Option[Lomake] = {
       tryFind(oid)
@@ -34,6 +37,7 @@ trait LomakeRepositoryComponent {
 
     private def tryFind(applicationSystemOid: String): Option[Lomake] = {
       try {
+        reportCacheStats()
         Some(timed(1000, "Application system service"){
           Lomake(repository.getApplicationSystem(applicationSystemOid))
         })
@@ -48,6 +52,15 @@ trait LomakeRepositoryComponent {
       tarjontaService.haku(applicationSystemId, lang).map(h => h.applicationPeriods) match {
         case Some(hakuajat) => hakuajat
         case _ => List()
+      }
+    }
+
+    def reportCacheStats(): Unit = {
+      if (lastReport.plusHours(1).isBeforeNow) {
+        lastReport.synchronized {
+          lastReport = new DateTime()
+        }
+        logger.info(s"Reporting hourly cache stats on lomake repository cache: " + repository.asInstanceOf[ApplicationSystemServiceImpl].getCacheStats().toString)
       }
     }
   }
