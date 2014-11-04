9cbbf29d7082e9c5ac2be62fcae02465be104be0
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-11-04 10:10:51 +0200

    preload translations

--- src/main/scala/fi/vm/sade/omatsivut/localization/Translations.scala
+++ src/main/scala/fi/vm/sade/omatsivut/localization/Translations.scala
@@ -7,11 +7,20 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 object Translations extends JsonFormats {
-  def getTranslations(implicit lang: Language) = {
-    val fileName: String = "/translations/" + lang.toString + ".json"
+  lazy val translations = Map(
+    "fi" -> loadTranslation("fi"),
+    "en" -> loadTranslation("en"),
+    "sv" -> loadTranslation("sv")
+  )
+
+  private def loadTranslation(lang: String) = {
+    val fileName: String = "/translations/" + lang + ".json"
     val text = io.Source.fromInputStream(getClass.getResourceAsStream(fileName)).mkString
     parse(text)
   }
+
+  def getTranslations(implicit lang: Language) = translations(lang.toString)
+
   def getTranslation(path: String*)(implicit lang: Language) = {
     path.foldLeft(getTranslations){ case (json, pathElem) => json \ pathElem }.extract[String]
   }
