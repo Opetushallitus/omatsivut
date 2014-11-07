0259d81a2f1b08a8736cdda611c10198ed962405
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-11-07 15:32:49 +0200

    Change cache configuration for omat sivut

--- src/main/scala/fi/vm/sade/omatsivut/config/OmatSivutCacheConfiguration.scala
+++ src/main/scala/fi/vm/sade/omatsivut/config/OmatSivutCacheConfiguration.scala
@@ -2,26 +2,20 @@ package fi.vm.sade.omatsivut.config
 
 import java.util.concurrent.TimeUnit
 
-import com.google.common.cache.{Weigher, CacheBuilder}
+import com.google.common.cache.CacheBuilder
 import fi.vm.sade.haku.oppija.lomake.domain.ApplicationSystem
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.{Bean, Configuration}
 
 @Configuration class OmatSivutCacheConfiguration {
     @Bean(name = Array("omatSivutApplicationSystemCacheBuilder"))
-    def applicationSystemCacheBuilder(@Value("${application.system.cache.refresh:6}") cacheRefreshTimer: Long): CacheBuilder[String, ApplicationSystem] = {
-      CacheBuilder.newBuilder.recordStats.maximumWeight(20).refreshAfterWrite(cacheRefreshTimer, TimeUnit.MINUTES).weigher(new Weigher[String, ApplicationSystem] {
-        def weigh(key: String, value: ApplicationSystem): Int = {
-          if (value.isActive) {
-            1
-          }
-          else {
-            2
-          }
-        }
-      })
-    }
+    def applicationSystemCacheBuilder(@Value("${application.system.cache.refresh:6}") cacheRefreshTimer: Long): CacheBuilder[String, ApplicationSystem] =
+      CacheBuilder
+        .newBuilder
+        .recordStats
+        .maximumSize(32)
+        .refreshAfterWrite(cacheRefreshTimer, TimeUnit.MINUTES)
+        .asInstanceOf[CacheBuilder[String, ApplicationSystem]]
   }
 
 
