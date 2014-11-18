d613297f02cdcee0d8a98e5943b02edcb4d82e18
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-11-18 10:54:03 +0200

    Fix bug in haku boolean fields

--- src/main/scala/fi/vm/sade/omatsivut/tarjonta/Haku.scala
+++ src/main/scala/fi/vm/sade/omatsivut/tarjonta/Haku.scala
@@ -4,13 +4,13 @@ import fi.vm.sade.omatsivut.domain.Language.Language
 import fi.vm.sade.omatsivut.ohjausparametrit.domain.HaunAikataulu
 import org.joda.time.Interval
 
-case class Haku(oid: String, name: String, applicationPeriods: List[Hakuaika], tyyppi: String, korkeakouluhaku: Boolean,  usePriority: Boolean, aikataulu: Option[HaunAikataulu] = None) {
+case class Haku(oid: String, name: String, applicationPeriods: List[Hakuaika], tyyppi: String, korkeakouluhaku: Boolean, usePriority: Boolean, aikataulu: Option[HaunAikataulu] = None) {
   def active: Boolean = new Interval(applicationPeriods.head.start, applicationPeriods.last.end).containsNow()
 }
 
 object Haku {
   def apply(tarjontaHaku: TarjontaHaku, lang: Language) : Haku = {
-    Haku(tarjontaHaku.oid, tarjontaHaku.nimi("kieli_" + lang.toString), tarjontaHaku.hakuaikas.sortBy(_.alkuPvm).map(h => Hakuaika(h)), HakuTyyppi(tarjontaHaku).toString, tarjontaHaku.usePriority, isKorkeakouluhaku(tarjontaHaku))
+    Haku(tarjontaHaku.oid, tarjontaHaku.nimi("kieli_" + lang.toString), tarjontaHaku.hakuaikas.sortBy(_.alkuPvm).map(h => Hakuaika(h)), HakuTyyppi(tarjontaHaku).toString, isKorkeakouluhaku(tarjontaHaku), tarjontaHaku.usePriority)
   }
 
   private def isKorkeakouluhaku(tarjontaHaku: TarjontaHaku) = {
