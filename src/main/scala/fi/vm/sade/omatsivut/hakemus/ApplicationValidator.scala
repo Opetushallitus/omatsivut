5546921a279123d81b4c350bca2630621aa69d72
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2014-11-20 09:31:33 +0200

    Use inputValidation = false
    So that hakemus-api PreferenceConcreteValidator does not validate
    provider values

--- src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationValidator.scala
+++ src/main/scala/fi/vm/sade/omatsivut/hakemus/ApplicationValidator.scala
@@ -130,7 +130,7 @@ trait ApplicationValidatorComponent {
     }
 
     private def convertToValidationInput(lomake: Lomake, application: Application): ValidationInput = {
-      new ValidationInput(lomake.form, application.getVastauksetMerged, application.getOid, lomake.oid, true)
+      new ValidationInput(lomake.form, application.getVastauksetMerged, application.getOid, lomake.oid, false)
     }
   }
 }
