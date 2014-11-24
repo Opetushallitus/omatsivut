1010a4386ebc5750749c35135a45fc2fa9a0aac3
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-11-24 14:02:33 +0200

    Set larger cache on haunt aikataulu

--- src/main/scala/fi/vm/sade/omatsivut/ohjausparametrit/domain/package.scala
+++ src/main/scala/fi/vm/sade/omatsivut/ohjausparametrit/domain/package.scala
@@ -1,8 +1,6 @@
 package fi.vm.sade.omatsivut.ohjausparametrit
 
 package object domain {
-
   case class HaunAikataulu(julkistus: Option[TulostenJulkistus], hakukierrosPaattyy: Option[Long])
   case class TulostenJulkistus(start: Long, end: Long)
-
 }
