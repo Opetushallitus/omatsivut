5b9e56cfeadfeb3f352d733a52dc0fb7f4cccdd3
Mikko Apo <mikko.apo@reaktor.fi>
2015-01-22 15:44:45 +0200

    Refactored deployment test for muistilista email

--- src/it/test/muistilista.js
+++ src/it/test/muistilista.js
@@ -1,42 +1,35 @@
 var assert = require("assert")
-var Client = require('node-rest-client').Client;
-var Q = require('q');
-var Qretry = require('qretry');
+var Client = require('node-rest-client').Client
+var Q = require('q')
+var Qretry = require('qretry')
 
 describe('Muistilista QA', function () {
-    this.timeout(6000);
+    this.timeout(6000)
     function httpPost(url, args) {
-        return Q.Promise(function (resolve, reject) {
-            var client = new Client();
-            client.on('error', function (err) {
-                reject(err);
-            });
-            client.post(url, args, function (data, response) {
-                if (response.statusCode !== 200) {
-                    reject(new Error("Status: " + response.statusCode + " for " + url));
-                } else {
-                    resolve(data, response);
-                }
-            });
-        })
+        return httpOperation("post", [url, args])
     }
 
     function httpGet(url) {
-        return Q.Promise(function (resolve, reject) {
-            var client = new Client();
-            client.on('error', function (err) {
-                reject(err);
-            });
-            client.get(url, function (data, response) {
-                if (response.statusCode !== 200) {
-                    reject(new Error("Status: " + response.statusCode + " for " + url));
-                } else {
-                    resolve(data, response);
-                }
-            });
-        })
+        return httpOperation("get", [url])
     }
 
+    function httpOperation(method, args) {
+        return Q.Promise(function (resolve, reject) {
+            var client = new Client()
+            client.on('error', function (err) {
+                reject(err)
+            })
+            args.push(function (data, response) {
+                if (response.statusCode !== 200) {
+                    reject(new Error("Status: " + response.statusCode + " for " + url))
+                } else {
+                    resolve(data, response)
+                }
+            })
+            client[method].apply(this, args)
+        })
+    }
+    
     function parseEmails(data) {
         return data
             .replace(/(\r\n|\n|\r)/gm, "")
@@ -50,20 +43,18 @@ describe('Muistilista QA', function () {
     function parseLatestEmailUrl(emailDirectory) {
         return function (data) {
             var emails = parseEmails(data)
-            return emailDirectory + emails[emails.length - 1];
+            return emailDirectory + emails[emails.length - 1]
         }
     }
 
     function assertContains() {
-        console.log("POW!")
         var args = Array.prototype.slice.call(arguments)
         return function (fullText) {
             args.forEach(function (s) {
                 if (fullText.indexOf(s) === -1) {
-                    throw new Error("Assert failed: '" + s + "' was not included in: " + fullText);
+                    throw new Error("Assert failed: '" + s + "' was not included in: " + fullText)
                 }
             })
-            console.log("YES!")
         }
     }
 
@@ -74,8 +65,8 @@ describe('Muistilista QA', function () {
     }
 
     it('should send email to a file folder', function (done) {
-        var emailDirectory = 'http://wp-reppu.oph.ware.fi/ryhmasahkoposti-emails/';
-        var muistiListaEndPoint = "https://test-oppija.oph.ware.fi/omatsivut/muistilista";
+        var emailDirectory = 'http://wp-reppu.oph.ware.fi/ryhmasahkoposti-emails/'
+        var muistiListaEndPoint = "https://test-oppija.oph.ware.fi/omatsivut/muistilista"
         var muistilistaParams = {
             otsikko: "test subject " + Math.random(),
             kieli: "fi",
@@ -91,10 +82,10 @@ describe('Muistilista QA', function () {
                     .then(httpGet)
                     .then(assertContains(muistilistaParams.otsikko, muistilistaParams.vastaanottaja[0]))
             })
-        ).then(done, done);
+        ).then(done, done)
 
         // https://test-oppija.oph.ware.fi/omatsivut/muistilista -> http://wp-reppu.oph.ware.fi/ryhmasahkoposti-emails/
         // https://testi.opintopolku.fi/omatsivut/muistilista
 
     })
-});
+})
