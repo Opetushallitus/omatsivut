eebff40c775fb9bb0c83061164a3d85df42357c9
Juha Paananen <juha.paananen@gmail.com>
2015-03-05 16:28:26 +0200

    use SharedJetty in OmatSivutMochaTest

--- src/test/scala/fi/vm/sade/omatsivut/mocha/OmatSivutMochaTest.scala
+++ src/test/scala/fi/vm/sade/omatsivut/mocha/OmatSivutMochaTest.scala
@@ -1,9 +1,8 @@
 package fi.vm.sade.omatsivut.mocha
 
-import fi.vm.sade.omatsivut.JettyLauncher
-import fi.vm.sade.utils.tcp.PortChecker
-import org.specs2.mutable.Specification
+import fi.vm.sade.omatsivut.SharedJetty
 import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
@@ -11,18 +10,14 @@ class OmatSivutMochaTest extends Specification {
   import scala.sys.process._
 
   "Mocha tests" in {
-    System.setProperty("omatsivut.profile", "it")
-    val omatSivutPort: Int = PortChecker.findFreeLocalPort
-    System.setProperty("omatsivut.port", omatSivutPort.toString)
+    SharedJetty.start
 
-    new JettyLauncher(omatSivutPort).withJetty {
-      val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:"+omatSivutPort+"/omatsivut/test/runner.html")
-      val res = pb.!
-      if (res != 0) {
-        failure("Mocha tests failed")
-      } else {
-        success
-      }
+    val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:"+SharedJetty.port+"/omatsivut/test/runner.html")
+    val res = pb.!
+    if (res != 0) {
+      failure("Mocha tests failed")
+    } else {
+      success
     }
   }
 }
