f1589521eeb5e9cb9ba1e5bb7f33f154e85a8353
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-06-29 13:27:58 +0300

    use cookie auth in authentication-service calls for fake shibboleth

--- src/main/scala/fi/vm/sade/omatsivut/security/AuthenticationInfoService.scala
+++ src/main/scala/fi/vm/sade/omatsivut/security/AuthenticationInfoService.scala
@@ -1,38 +1,48 @@
 package fi.vm.sade.omatsivut.security
 
 import fi.vm.sade.omatsivut.config.{RemoteApplicationConfig, SecuritySettings}
-import fi.vm.sade.utils.http.DefaultHttpClient
-import fi.vm.sade.utils.slf4j.Logging
 import fi.vm.sade.utils.cas.{CasClient, CasTicketRequest}
+import fi.vm.sade.utils.http.{DefaultHttpClient, HttpRequest}
+import fi.vm.sade.utils.slf4j.Logging
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 class RemoteAuthenticationInfoService(val config: RemoteApplicationConfig, val securitySettings: SecuritySettings) extends Logging {
   implicit val formats = DefaultFormats
 
-  def getHenkiloOID(hetu : String) : Option[String] = {
-    val ticketRequest = CasTicketRequest(config.url + "/" + config.ticketConsumerPath, securitySettings.casUsername, securitySettings.casPassword)
-    new CasClient(securitySettings.casConfig).getServiceTicket(ticketRequest).flatMap { serviceTicket =>
+  private def getCookies(createNewSession: Boolean): List[String] =
+    new CasClient(securitySettings.casConfig).
+      getSessionCookies(CasTicketRequest(config.url, securitySettings.casUsername, securitySettings.casPassword), createNewSession)
+
+  private def addHeaders(request: HttpRequest, createNewSession: Boolean = false): Unit = {
+    request.header("Cookie", getCookies(createNewSession).mkString("; "))
+    request.header("Caller-Id", "omatsivut.omatsivut.backend")
+  }
+
+  def getHenkiloOID(hetu: String) : Option[String] = {
+    def tryGet(h: String, createNewSession: Boolean = false, retryCount: Int = 0): Option[String] = {
       val path: String = config.url + "/" + config.config.getString("get_oid.path") + "/" + hetu
-      val (responseCode, headersMap, resultString) = DefaultHttpClient.httpGet(path)
-        .param("ticket", serviceTicket)
-        .responseWithHeaders
+      val request = DefaultHttpClient.httpGet(path)
+      addHeaders(request, createNewSession)
+      val (responseCode, headersMap, resultString) = request.responseWithHeaders()
 
       responseCode match {
+        case 401 if retryCount < 2 =>
+          tryGet(h, createNewSession = true, retryCount + 1)
         case 404 => None
-        case 200 => {
+        case 200 =>
           val json = parse(resultString)
           val oids: List[String] = for {
             JObject(child) <- json
             JField("oidHenkilo", JString(oid)) <- child
           } yield oid
           oids.headOption
-        }
-        case code => {
+        case code =>
           logger.error("Error fetching personOid. Response code=" + code + ", content=" + resultString)
           None
-        }
       }
     }
+
+    tryGet(hetu)
   }
 }
