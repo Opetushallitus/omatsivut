48a7cf5c07a16a0a5c0db3cac72152e19600c5ae
Teijo Laine <teijo.laine@reaktor.fi>
2015-11-06 13:45:53 +0200

    Remove Haku getMongoUrl() dependency

--- src/main/scala/fi/vm/sade/omatsivut/mongo/OmatSivutMongoConfiguration.scala
+++ src/main/scala/fi/vm/sade/omatsivut/mongo/OmatSivutMongoConfiguration.scala
@@ -1,7 +1,6 @@
 package fi.vm.sade.omatsivut.mongo
 
 import com.mongodb.{Mongo, MongoClient, MongoClientOptions, MongoClientURI, WriteConcern}
-import fi.vm.sade.haku.oppija.configuration.MongoConfiguration
 import fi.vm.sade.utils.slf4j.Logging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.{Bean, Configuration}
@@ -13,8 +12,14 @@ import org.springframework.data.mongodb.core.MongoTemplate
     new MongoTemplate(mongo, databaseName)
   }
 
+  def sanitizeMongoUrl(mongoUri: String) = mongoUri match {
+    case uri if uri.contains("@") => uri.substring(mongoUri.indexOf("@"))
+    case uri if uri.contains("//") => uri.substring(mongoUri.indexOf("//"))
+    case _ => _
+  }
+
   @Bean def mongo(@Value("${mongodb.url}") mongoUri: String): MongoClient = {
-    logger.info("Creating MongoClient for server(s): " + MongoConfiguration.getMongoUrl(mongoUri))
+    logger.info("Creating MongoClient for server(s): " + sanitizeMongoUrl(mongoUri))
     val options = new MongoClientOptions.Builder().writeConcern(WriteConcern.FSYNCED)
     val mongoClientURI: MongoClientURI = new MongoClientURI(mongoUri, options)
     val mongoClient: MongoClient = new MongoClient(mongoClientURI)
