7d5a6ad420430a0c748323764ada363c2ca81ceb
Alexis Troberg <alexis.troberg@gofore.com>
2015-12-22 14:49:16 +0200

    Skeleton for hakutoiveidenMuokkaus

--- src/main/js/controllers/hakutoiveidenMuokkaus.js
+++ src/main/js/controllers/hakutoiveidenMuokkaus.js
@@ -1,5 +1,22 @@
 module.exports = function(app, staticResources) {
-  app.controller('HakutoiveidenMuokkausController', function($scope) {
-    $scope.lang = staticResources.translations.languageId
-  })
+    app.controller('HakutoiveidenMuokkausController', function($scope, $location, $http) {
+        var matches = $location.path().match(/token\/(.+)/)
+        var token = matches && matches[1]
+
+        $scope.lang = staticResources.translations.languageId
+
+        if (token) {
+            $location.path('')
+            $http.get('http://weerfdsfinsecure/applications/application/' + token)
+                .success(function(hakemus) {
+                    $scope.hakemus = hakemus
+                })
+                .error(function(response) {
+                    $scope.error = angular.extend({}, response)
+                })
+        }
+        else {
+            // TODO: redirect to help page?
+        }
+    })
 }
