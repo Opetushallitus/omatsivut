9f45f24422c9ccde2e4d1d3b69b3fdd77cd89cf1
Alexis Troberg <alexis.troberg@gofore.com>
2015-12-23 11:22:39 +0200

    Save hakemusOid in JWT and GET method for it

--- src/main/scala/fi/vm/sade/omatsivut/servlet/NonSensitiveApplicationServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/NonSensitiveApplicationServlet.scala
@@ -3,8 +3,10 @@ package fi.vm.sade.omatsivut.servlet
 import fi.vm.sade.hakemuseditori.hakemus.HakemusRepositoryComponent
 import fi.vm.sade.hakemuseditori.json.JsonFormats
 import fi.vm.sade.omatsivut.NonSensitiveHakemus
+import fi.vm.sade.omatsivut.NonSensitiveHakemus.Oid
 import fi.vm.sade.omatsivut.config.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.oppijantunnistus.{InvalidTokenException, OppijanTunnistusComponent}
+import fi.vm.sade.omatsivut.security.{HakemusJWT, JsonWebToken}
 import org.scalatra.json.JacksonJsonSupport
 import org.scalatra.{InternalServerError, NotFound}
 
@@ -18,22 +20,42 @@ trait NonSensitiveApplicationServletContainer {
 
     protected val applicationDescription = "Oppijan henkilökohtaisen palvelun REST API, jolla voi muokata hakemusta heikosti tunnistautuneena"
 
+    val jwt = new JsonWebToken(appConfig.settings.hmacKey)
+
+    def serverError = InternalServerError("errorType" -> "serverError")
+
+    def returnHakemus(oid: Oid) = {
+      hakemusRepository.getHakemus(oid) match {
+        case Some(hakemusInfo) =>
+          val hakemus = hakemusInfo.hakemus
+          NonSensitiveHakemus(hakemus.oid, hakemus.hakutoiveet, jwt.encode(HakemusJWT(hakemus.oid)))
+        case _ =>
+          logger.error("Hakemus not found! HakemusOid: " + oid)
+          serverError
+      }
+    }
+
     before() {
       contentType = formats("json")
     }
 
+    get("/application/session") {
+      val bearerMatch = """Bearer (.+)""".r
+      response.getHeader("Authorization") match {
+        case bearerMatch(token) =>
+          jwt.decode(token) match {
+            case Success(hakemus) =>
+              returnHakemus(hakemus.oid)
+            case Failure(e) => serverError
+          }
+        case _ => serverError
+      }
+    }
+
     get("/application/:token") {
-      def serverError = InternalServerError("errorType" -> "serverError")
       oppijanTunnistusService.validateToken(params("token")) match {
         case Success(hakemusOid) =>
-          hakemusRepository.getHakemus(hakemusOid) match {
-            case Some(hakemusInfo) =>
-              val hakemus = hakemusInfo.hakemus
-              NonSensitiveHakemus(hakemus.oid, hakemus.hakutoiveet)
-            case _ =>
-              logger.error("Token was valid but hakemus not found! Token: " + params("token") + ", hakemusOid: " + hakemusOid)
-              serverError
-          }
+          returnHakemus(hakemusOid)
         case Failure(e: InvalidTokenException) =>
           NotFound("errorType" -> "invalidToken")
         case Failure(exception) =>
