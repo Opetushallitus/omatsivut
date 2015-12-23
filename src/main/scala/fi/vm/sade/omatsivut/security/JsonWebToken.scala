3bd217ed24b4df57617178f15f5d5eb2e0a21837
Alexis Troberg <alexis.troberg@gofore.com>
2015-12-23 11:22:39 +0200

    Save hakemusOid in JWT and GET method for it

--- src/main/scala/fi/vm/sade/omatsivut/security/JsonWebToken.scala
+++ src/main/scala/fi/vm/sade/omatsivut/security/JsonWebToken.scala
@@ -1,5 +1,6 @@
 package fi.vm.sade.omatsivut.security
 
+import fi.vm.sade.omatsivut.NonSensitiveHakemus.Oid
 import org.json4s.JsonDSL.WithBigDecimal._
 import org.json4s._
 import pdi.jwt.{JwtAlgorithm, JwtJson4s}
@@ -8,21 +9,32 @@ import scala.util.{Failure, Success, Try}
 
 class InvalidJsonWebTokenException(msg: String) extends RuntimeException(msg)
 
+case class HakemusJWT(oid: Oid)
+
 class JsonWebToken(val secret: String) {
 
   if (secret.isEmpty) throw new RuntimeException("Secret cannot be empty")
 
   implicit val formats = org.json4s.DefaultFormats
   val algo = JwtAlgorithm.HS256
+  val hakemusOidKey = "hakemusOid"
 
-  def encode(claim: Map[String, String]) = {
-    JwtJson4s.encode(claim, secret, algo)
+  def encode(hakemus: HakemusJWT) = {
+    JwtJson4s.encode(Map(hakemusOidKey -> hakemus.oid), secret, algo)
   }
 
-  def decode(token: String): Try[Map[String, String]] = {
+  def decode(token: String): Try[HakemusJWT] = {
     JwtJson4s.decodeJson(token, secret, Seq(algo)) match {
       case Success(value) =>
-        Try(value.extract[Map[String, String]])
+        Try(value.extract[Map[String, String]]) match {
+          case Success(map) =>
+            map.get(hakemusOidKey) match {
+              case Some(oid) => Success(HakemusJWT(oid))
+              case _ => Failure(new InvalidJsonWebTokenException("JSON claim invalid"))
+            }
+          case Failure(e) =>
+            Failure(new InvalidJsonWebTokenException("JSON claim invalid"))
+        }
       case Failure(e) =>
         Failure(new InvalidJsonWebTokenException("Failed to decode JWT"))
     }
