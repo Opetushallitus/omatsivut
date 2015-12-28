e93f535c08f20f470b9b066da1a7ec73c1b800bb
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-12-28 15:46:27 +0200

    Fix warnings

--- src/main/scala/fi/vm/sade/omatsivut/servlet/ApplicationsServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/ApplicationsServlet.scala
@@ -124,7 +124,7 @@ trait ApplicationsServletContainer {
           }
 
         } catch {
-          case e =>
+          case e: Throwable =>
             logger.error("failure in background service call", e)
             InternalServerError("error" -> "Background service failed")
 
