dbb736b2f3df50045750d30c97ef144d9ab5b344
Alexis Troberg <alexis.troberg@gofore.com>
2015-12-28 09:56:39 +0200

    Facilitate testing by returning mocked hakemus

--- src/main/scala/fi/vm/sade/omatsivut/oppijantunnistus/OppijanTunnistus.scala
+++ src/main/scala/fi/vm/sade/omatsivut/oppijantunnistus/OppijanTunnistus.scala
@@ -44,5 +44,5 @@ class RemoteOppijanTunnistusService(verifyUrl: String, client: HttpClient = Defa
 }
 
 class StubbedOppijanTunnistusService extends OppijanTunnistusService {
-  override def validateToken(token: String): Try[Oid] = Success("successHakemusOid")
+  override def validateToken(token: String): Try[Oid] = Success("1.2.246.562.11.00004102043")
 }
