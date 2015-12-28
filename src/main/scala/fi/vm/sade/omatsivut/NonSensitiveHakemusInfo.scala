801100108ebf29816feafe9fa56a37928838a8ec
Alexis Troberg <alexis.troberg@gofore.com>
2015-12-28 13:14:54 +0200

    Return non-sensitive contact details in answers-map

--- src/main/scala/fi/vm/sade/omatsivut/NonSensitiveHakemusInfo.scala
+++ src/main/scala/fi/vm/sade/omatsivut/NonSensitiveHakemusInfo.scala
@@ -1,6 +1,7 @@
 package fi.vm.sade.omatsivut
 
 import fi.vm.sade.hakemuseditori.hakemus.HakemusInfo
+import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.OppijaConstants._
 
 object NonSensitiveHakemusInfo {
   type Oid = String
@@ -8,9 +9,20 @@ object NonSensitiveHakemusInfo {
   protected case class NonSensitiveHakemusInfo(hakemusInfo: HakemusInfo, jsonWebToken: String)
 
   def apply(sensitiveHakemusInfo: HakemusInfo, jsonWebToken: String): NonSensitiveHakemusInfo = {
-    NonSensitiveHakemusInfo(
-      sensitiveHakemusInfo.copy(
-        hakemus = sensitiveHakemusInfo.hakemus.copy(answers = Map.empty)
-      ), jsonWebToken)
+    sensitiveHakemusInfo.hakemus.answers.get(PHASE_PERSONAL) match {
+      case Some(henkilotiedot) =>
+        val nonSensitiveContactDetails = List(ELEMENT_ID_EMAIL, ELEMENT_ID_PREFIX_PHONENUMBER + "1")
+          .map(key => key -> henkilotiedot.getOrElse(key, ""))
+          .toMap
+        NonSensitiveHakemusInfo(
+          sensitiveHakemusInfo.copy(
+            hakemus = sensitiveHakemusInfo.hakemus.copy(
+              answers = Map(PHASE_PERSONAL -> nonSensitiveContactDetails)
+            ),
+            questions = List()
+          ), jsonWebToken)
+
+      case _ => throw new RuntimeException("henkilotiedot missing for hakemus " + sensitiveHakemusInfo.hakemus.oid)
+    }
   }
 }
