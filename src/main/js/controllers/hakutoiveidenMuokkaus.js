27af80f1b1812dadacd26b2dcaa91289452f8adc
Alexis Troberg <alexis.troberg@gofore.com>
2015-12-29 09:30:30 +0200

    Fix formatting: JS should apparently use 2 spaces instead of 4

--- src/main/js/controllers/hakutoiveidenMuokkaus.js
+++ src/main/js/controllers/hakutoiveidenMuokkaus.js
@@ -2,28 +2,28 @@ var Hakemus = require('./hakemuseditori').Hakemus
 var util = require('../util')
 
 module.exports = function(app) {
-    app.controller('HakutoiveidenMuokkausController', function($scope, $location, $http) {
-        var matches = $location.path().match(/token\/(.+)/)
-        var token = matches && matches[1]
-        var baseUrl = 'insecure/applications/application/'
+  app.controller('HakutoiveidenMuokkausController', function($scope, $location, $http) {
+    var matches = $location.path().match(/token\/(.+)/)
+    var token = matches && matches[1]
+    var baseUrl = 'insecure/applications/application/'
 
-        $scope.lang = 'fi' // Todo
+    $scope.lang = 'fi' // Todo
 
-        if (token || util.getBearerToken()) {
-            $scope.loading = true
-            $location.path('/').replace()
-            var suffix = token ? 'token/' + token : 'session'
-            $http.get(baseUrl + suffix).success(function(response) {
-                $scope.loading = false
-                $scope.hakemusInfo = new Hakemus(response.hakemusInfo)
-            }).error(function(response) {
-                $scope.loading = false
-                $scope.error = angular.extend({}, response)
-            })
-        } else {
-            $scope.error = {
-                errorType: 'noTokenAvailable'
-            }
-        }
-    })
+    if (token || util.getBearerToken()) {
+      $scope.loading = true
+      $location.path('/').replace()
+      var suffix = token ? 'token/' + token : 'session'
+      $http.get(baseUrl + suffix).success(function(response) {
+        $scope.loading = false
+        $scope.hakemusInfo = new Hakemus(response.hakemusInfo)
+      }).error(function(response) {
+        $scope.loading = false
+        $scope.error = angular.extend({}, response)
+      })
+    } else {
+      $scope.error = {
+        errorType: 'noTokenAvailable'
+      }
+    }
+  })
 }
