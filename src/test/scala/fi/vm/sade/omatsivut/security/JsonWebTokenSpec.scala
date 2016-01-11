5b0c4ffc3ab51dde1a3005c96621357ce4bf788e
Jani Rahkola <jani.rahkola@reaktor.com>
2016-01-11 12:07:14 +0200

    Filter visible questions based on anwsers
    Show questions only to answers given in the session.

--- src/test/scala/fi/vm/sade/omatsivut/security/JsonWebTokenSpec.scala
+++ src/test/scala/fi/vm/sade/omatsivut/security/JsonWebTokenSpec.scala
@@ -1,5 +1,6 @@
 package fi.vm.sade.omatsivut.security
 
+import fi.vm.sade.hakemuseditori.lomake.domain.AnswerId
 import org.json4s.JsonDSL.WithBigDecimal._
 import org.junit.runner.RunWith
 import org.scalatra.test.specs2.MutableScalatraSpec
@@ -12,19 +13,19 @@ class JsonWebTokenSpec extends MutableScalatraSpec with Mockito {
 
   val jwt = new JsonWebToken("secret")
   val hakemusOid = "1.2.3"
-  val preferences = List("1.2.3", "2.3.4", "4.5.6")
+  val answersFromThisSession = Set(AnswerId("foo", "bar"), AnswerId("bar", "foo"))
   val personOid = "9.7.3"
 
   "JsonWebToken" should {
 
     "return claim on valid token" in {
-      val token = jwt.encode(HakemusJWT(hakemusOid, preferences, personOid))
-      jwt.decode(token) must beSuccessfulTry.withValue(HakemusJWT(hakemusOid, preferences, personOid))
+      val token = jwt.encode(HakemusJWT(hakemusOid, answersFromThisSession, personOid))
+      jwt.decode(token) must beSuccessfulTry.withValue(HakemusJWT(hakemusOid, answersFromThisSession, personOid))
     }
 
     "return failure on invalid token signature" in {
       val expectedSignature = ".SRqRG2MnOPddgwf_obkUUpmYi-fqB2tEZcd4bPaQPds"
-      val token = jwt.encode(HakemusJWT(hakemusOid, preferences, personOid)).replace(expectedSignature, ".ArTpS2")
+      val token = jwt.encode(HakemusJWT(hakemusOid, answersFromThisSession, personOid)).replace(expectedSignature, ".ArTpS2")
       jwt.decode(token) must beFailedTry.withThrowable[InvalidJsonWebTokenException]
     }
 
