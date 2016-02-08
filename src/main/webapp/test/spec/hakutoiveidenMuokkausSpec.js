80c46446f5783796efc85a9478ecb0f92e75eef7
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-02-08 15:27:03 +0200

    Add basic mocha tests for hakutoiven muokkaus

---
+++ src/main/webapp/test/spec/hakutoiveidenMuokkausSpec.js
@@ -0,0 +1,45 @@
+(function () {
+  var page = HakutoiveidenMuokkausPage()
+
+  afterEach(function() {
+    expect(window.uiError || null).to.be.null
+  })
+
+  afterEach(function () {
+    if (this.currentTest.state == 'failed') {
+      takeScreenshot()
+    }
+  })
+
+  describe("Hakemuksen muokkaus väärällä tokenilla", function() {
+    before(
+      page.openPage("väärä")
+    )
+
+    it("näytetään virhe", function() {
+      expect(page.alertMsg()).to.equal('Hakemustasi ei voida näyttää koska linkki on virheellinen.' )
+    })
+  })
+
+  describe('Hakemuksen muokkaus, kun hakemusta ei löydy', function () {
+    before(
+      page.openPage("none")
+    )
+
+    it("näytetään ilmoitus", function() {
+      expect(page.alertMsg()).to.equal('Hakemuksesi ei ole enää aktiivinen.' )
+    })
+  })
+
+  describe('Hakemuksen muokkaus', function () {
+    before(
+      page.applyFixtureAndOpen({token: "secret"})
+    )
+
+    describe("Hakemuksen tietojen näyttäminen", function() {
+      it('näkyy oikea hakemus', function () {
+        expect(page.getApplication().name()).to.equal('Korkeakoulujen yhteishaku kevät 2015')
+      })
+    })
+  })
+})()
