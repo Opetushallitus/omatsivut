cfa00627745521c06f29edbeb586aeafa603b0ff
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-02-11 13:50:16 +0200

    Add tests for BUG-743
    failing tests are still skipped

--- src/main/webapp/test/spec/hakutoiveidenMuokkausSpec.js
+++ src/main/webapp/test/spec/hakutoiveidenMuokkausSpec.js
@@ -36,7 +36,7 @@
       page.applyFixtureAndOpen({token: hakemusKorkeakouluKevatWithJazzId})
     )
 
-    describe("Hakemuksen tietojen näyttäminen", function() {
+    describe("Linkin avaamisen jälkeen", function() {
       it('näkyy oikea hakemus', function () {
         expect(page.getApplication().name()).to.equal('Korkeakoulujen yhteishaku kevät 2015')
       })
@@ -48,9 +48,89 @@
         page.applyFixtureAndOpen({token: hakemusYhteishakuKevat2016})
     )
 
-    describe("Hakemuksen tietojen näyttäminen", function() {
+    describe("Linkin avaamisen jälkeen", function() {
       it('näkyy oikea hakemus', function () {
         expect(page.getApplication().name()).to.equal('Yhteishaku ammatilliseen ja lukioon, kevät 2016')
+      })
+    })
+
+    describe("kun lisätään hakukohde", function() {
+      before(
+          page.getApplication().getPreference(3).selectOpetusPiste("Lap"),
+          page.getApplication().getPreference(3).selectKoulutus(1)
+      )
+
+      describe("lisäämisen jälkeen", function() {
+        it("seuraava hakukohde tulee muokattavaksi", function() {
+          page.getApplication().getPreference(4).isEditable().should.be.true
+        })
+
+        it("lisätty hakutoive on edelleen muokattavissa", function() {
+          page.getApplication().getPreference(3).isEditable().should.be.true
+        })
+
+        it("näytetään uudet kysymykset", function() {
+          var questionTitles = page.getApplication().questionsForApplication().titles()
+          expect(questionTitles).to.deep.equal([
+            'Tällä alalla on terveydentilavaatimuksia, jotka voivat olla opiskelijaksi ottamisen esteenä. Onko sinulla terveydellisiä tekijöitä, jotka voivat olla opiskelijaksi ottamisen esteenä?',
+            'Tässä koulutuksessa opiskelijaksi ottamisen esteenä voi olla aiempi päätös opiskeluoikeuden peruuttamisessa. Onko opiskeluoikeutesi aiemmin peruutettu terveydentilasi tai muiden henkilöiden turvallisuuden vaarantamisen takia?',
+            'Haetko urheilijan ammatilliseen koulutukseen?'
+          ])
+        })
+
+        it("lomake ei ole vielä tallennettavissa", function() {
+          page.getApplication().isValidationErrorVisible().should.be.true
+        })
+
+        it("sovellus ei ole virhetilassa", function() {
+          page.getApplication().saveError().should.equal("Täytä kaikki tiedot")
+        })
+      })
+
+      describe("vastattaessa kysymyksiin", function() {
+        before(
+            function() { page.getApplication().questionsForApplication().enterAnswer(0, "Ei"); },
+            function() { page.getApplication().questionsForApplication().enterAnswer(1, "Ei"); },
+            function() { page.getApplication().questionsForApplication().enterAnswer(2, "Ei"); },
+            wait.forAngular
+        )
+
+        describe("vastaamisen jälkeen", function() {
+          it("lomake on tallennettavissa", function() {
+            page.getApplication().isValidationErrorVisible().should.be.false
+          })
+        })
+
+        describe("siirrettäessä lisätty hakutoive ylemmäksi", function() {
+          before(
+              page.getApplication().getPreference(3).moveUp
+          )
+
+          describe("siirtämisen jälkeen", function() {
+            it("lomake on tallennettavissa", function() {
+              page.getApplication().isValidationErrorVisible().should.be.false
+            })
+
+            it.skip("näytetään yhä uudet kysymykset", function() {
+              var questionTitles = page.getApplication().questionsForApplication().titles()
+              expect(questionTitles).to.deep.equal([
+                'Tällä alalla on terveydentilavaatimuksia, jotka voivat olla opiskelijaksi ottamisen esteenä. Onko sinulla terveydellisiä tekijöitä, jotka voivat olla opiskelijaksi ottamisen esteenä?',
+                'Tässä koulutuksessa opiskelijaksi ottamisen esteenä voi olla aiempi päätös opiskeluoikeuden peruuttamisessa. Onko opiskeluoikeutesi aiemmin peruutettu terveydentilasi tai muiden henkilöiden turvallisuuden vaarantamisen takia?',
+                'Haetko urheilijan ammatilliseen koulutukseen?'
+              ])
+            })
+          })
+
+          describe.skip("tallentaminen", function() {
+            before(
+                page.getApplication().saveWaitSuccess
+            )
+
+            it("onnistuu", function() {
+
+            })
+          })
+        })
       })
     })
   })
