e067a24b2eda743eefc03a8c8992dd35fbc0fe52
Alexis Troberg <alexis.troberg@gofore.com>
2016-03-02 10:11:41 +0200

    Wiring for suoritusrekisteriservice

--- src/main/scala/fi/vm/sade/omatsivut/config/OmatSivutSpringContext.scala
+++ src/main/scala/fi/vm/sade/omatsivut/config/OmatSivutSpringContext.scala
@@ -87,7 +87,8 @@ object OmatSivutSpringContext extends Logging {
     "fi.vm.sade.haku.virkailija.viestintapalvelu",
     "fi.vm.sade.haku.oppija.common.organisaatio",
     "fi.vm.sade.haku.virkailija.lomakkeenhallinta.ohjausparametrit",
-    "fi.vm.sade.haku.virkailija.lomakkeenhallinta.tarjonta.impl"
+    "fi.vm.sade.haku.virkailija.lomakkeenhallinta.tarjonta.impl",
+    "fi.vm.sade.haku.oppija.common.suoritusrekisteri.impl"
   ),
   excludeFilters = Array(new ComponentScan.Filter(`type` = FilterType.ASSIGNABLE_TYPE, value = Array[Class[_]](classOf[Session])))
   )
@@ -102,21 +103,6 @@ object OmatSivutSpringContext extends Logging {
 
     @Bean def userSession: Session = new SystemSession {
       override def getUser(): User = new User("HAKIJA")
-    }
-
-    @Bean def suoritusRekisteriService: SuoritusrekisteriService = new SuoritusrekisteriService {
-
-      override def getArvosanat(s: String): util.List[ArvosanaDTO] = unsupportedIntegrationException
-
-      override def getOpiskelijatiedot(s: String): util.List[OpiskelijaDTO] = unsupportedIntegrationException
-
-      override def getSuoritukset(s: String): util.Map[String, util.List[SuoritusDTO]] = unsupportedIntegrationException
-
-      override def getSuoritukset(s: String, s1: String): util.Map[String, util.List[SuoritusDTO]] = unsupportedIntegrationException
-
-      override def getSuoritukset(s: String, s1: String, date: Date): util.Map[String, util.List[SuoritusDTO]] = unsupportedIntegrationException
-
-      override def getChanges(s: String, date: Date): util.List[String] = unsupportedIntegrationException
     }
 
     @Bean def valintaService: ValintaService = new ValintaService {
