048a0274d3370fe63e62ab74ce520910044bd5c2
Jessika Penttinen <jessika.penttinen@siili.com>
2016-04-06 15:01:38 +0300

    Fix BUG-804

--- src/main/webapp/test/spec/applicationListSpec.js
+++ src/main/webapp/test/spec/applicationListSpec.js
@@ -2371,7 +2371,7 @@
         })
 
         it("Valintatulokset näytetään", function() {
-          expect(erillishaku.valintatulokset()[0].hakukohde).to.equal(' ') // <- valinta-tulos-servicellä ei ole kannassaan tätä (käyttää erillistä hakemuskantaa)
+          expect(erillishaku.valintatulokset()[0].hakukohde).to.equal('Kallion lukio Lukion ilmaisutaitolinja') // <- valinta-tulos-servicellä ei ole kannassaan tätä, mutta se haetaan koulutusInformaatio-APIsta
           expect(erillishaku.valintatulokset()[0].tila).to.equal('Hyväksytty')
         })
 
@@ -2393,7 +2393,7 @@
         before(page.applyErillishakuFixtureAndOpen(false))
 
         it("Valintatulos näytetään", function() {
-          expect(erillishaku.valintatulokset()[0].hakukohde).to.equal(' ') // <- valinta-tulos-servicellä ei ole kannassaan tätä (käyttää erillistä hakemuskantaa)
+          expect(erillishaku.valintatulokset()[0].hakukohde).to.equal('Kallion lukio Lukion ilmaisutaitolinja') // <- valinta-tulos-servicellä ei ole kannassaan tätä, mutta se haetaan koulutusInformaatio-APIsta
           expect(erillishaku.valintatulokset()[0].tila).to.equal('Et saanut opiskelupaikkaa.')
         })
       })
