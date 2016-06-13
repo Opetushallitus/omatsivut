1f7d8ec7225d34ea731ad09a492b661be1156f92
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-06-13 15:11:59 +0300

    Attempt for poor man's mocha test report

--- src/test/scala/fi/vm/sade/omatsivut/mocha/OmatSivutMochaTest.scala
+++ src/test/scala/fi/vm/sade/omatsivut/mocha/OmatSivutMochaTest.scala
@@ -1,5 +1,7 @@
 package fi.vm.sade.omatsivut.mocha
 
+import java.io.{ByteArrayOutputStream, PrintWriter}
+
 import fi.vm.sade.omatsivut.SharedJetty
 import fi.vm.sade.omatsivut.config.AppConfig
 import org.junit.runner.RunWith
@@ -15,8 +17,25 @@ class OmatSivutMochaTest extends Specification {
   }
 
   "Mocha tests" in {
-    val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:"+AppConfig.embeddedJettyPortChooser.chosenPort+"/omatsivut/test/runner.html")
-    val res = pb.!
+    val stdoutStream = new ByteArrayOutputStream
+    val stderrStream = new ByteArrayOutputStream
+    val stdoutWriter = new PrintWriter(stdoutStream)
+    val stderrWriter = new PrintWriter(stderrStream)
+
+    val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "xunit", "http://localhost:"+AppConfig.embeddedJettyPortChooser.chosenPort+"/omatsivut/test/runner.html?grep=monta%20hakuaikaa")
+    val res = pb.!(ProcessLogger(stdoutWriter.println, stderrWriter.println))
+    stdoutWriter.close()
+    stderrWriter.close()
+
+    val plainTestRunOutput: String = stdoutStream.toString
+    val testOutput = plainTestRunOutput.split("\n").filter(_.startsWith("<")).mkString("\n")
+    new PrintWriter("target/surefire-reports/mocha-tests.xml") {
+      write(testOutput)
+      close()
+    }
+    System.err.println(stderrStream.toString)
+    println(s"Test output:\n$plainTestRunOutput")
+
     if (res != 0) {
       failure("Mocha tests failed")
     } else {
