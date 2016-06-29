3e2ab0c7df0a7341ce5d8ec1dd945fb188bdbaa3
Tapio Tiili <tapio.tiili@reaktor.fi>
2016-06-29 23:09:06 +0300

    Reading stuff from stdout and stderr still dangerous 2016?

--- src/test/scala/fi/vm/sade/omatsivut/mocha/OmatSivutMochaTest.scala
+++ src/test/scala/fi/vm/sade/omatsivut/mocha/OmatSivutMochaTest.scala
@@ -1,7 +1,5 @@
 package fi.vm.sade.omatsivut.mocha
 
-import java.io.{ByteArrayOutputStream, File, PrintWriter}
-
 import fi.vm.sade.omatsivut.SharedJetty
 import fi.vm.sade.omatsivut.config.AppConfig
 import org.junit.runner.RunWith
@@ -17,28 +15,8 @@ class OmatSivutMochaTest extends Specification {
   }
 
   "Mocha tests" in {
-    val stdoutStream = new ByteArrayOutputStream
-    val stderrStream = new ByteArrayOutputStream
-    val stdoutWriter = new PrintWriter(stdoutStream)
-    val stderrWriter = new PrintWriter(stderrStream)
-
-    val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "xunit", "http://localhost:"+AppConfig.embeddedJettyPortChooser.chosenPort+"/omatsivut/test/runner.html")
-    val res = pb.!(ProcessLogger(stdoutWriter.println, stderrWriter.println))
-    stdoutWriter.close()
-    stderrWriter.close()
-
-    val plainTestRunOutput: String = stdoutStream.toString
-    val testOutput = plainTestRunOutput.split("\n").filter(_.startsWith("<")).mkString("\n")
-    val testReportsDirOnBamboo: String = "target/surefire-reports"
-    new File(testReportsDirOnBamboo).mkdirs()
-    new PrintWriter(testReportsDirOnBamboo + "/mocha-tests.xml") {
-      write(testOutput)
-      close()
-    }
-    System.err.println(stderrStream.toString)
-    println(s"Test output for mocha tests XML report:\n$testOutput")
-    println(s"Full test run est output:\n$plainTestRunOutput")
-
+    val pb = Seq("node_modules/mocha-phantomjs/bin/mocha-phantomjs", "-R", "spec", "http://localhost:"+AppConfig.embeddedJettyPortChooser.chosenPort+"/omatsivut/test/runner.html")
+    val res = pb.!
     if (res != 0) {
       failure("Mocha tests failed")
     } else {
