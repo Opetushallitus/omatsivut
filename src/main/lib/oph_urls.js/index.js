bd23a440e22df32d726080658efce15876372c7c
Mikko Apo <mikko.apo@reaktor.fi>
2016-11-04 20:58:52 +0200

    Updated oph_urls.js to fix issues with oppija-raamit

--- src/main/lib/oph_urls.js/index.js
+++ src/main/lib/oph_urls.js/index.js
@@ -26,6 +26,15 @@
  */
 
 (function(exportDest) {
+    var version="1.0"
+
+    if(exportDest.urls) {
+        if(exportDest.urls.version !== version)   {
+            console.log("'Mismatching oph_urls.js. First loaded (and in use):", exportDest.urls.version, " second loaded (not in use): ", version)
+        }
+        return
+    }
+
     exportDest.urls = function() {
         var urls_config = {}
         var omitEmptyValuesFromQuerystring = false
@@ -130,6 +139,7 @@
         return ret
     }
 
+    exportDest.urls.version = version
     exportDest.urls.properties = {}
     exportDest.urls.defaults = {}
     exportDest.urls.override = {}
