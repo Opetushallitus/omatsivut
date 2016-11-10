f4c06494efc886d6e1f94990ede68f3209156b39
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-11-10 18:40:46 +0200

    Use shared folder also for omat sivut tuloskirje download

---
+++ src/main/scala/fi/vm/sade/hakemuseditori/viestintapalvelu/TuloskirjeComponent.scala
@@ -0,0 +1,62 @@
+package fi.vm.sade.hakemuseditori.viestintapalvelu
+
+import java.io.{FileInputStream, File}
+
+import fi.vm.sade.hakemuseditori.hakemus.domain.Tuloskirje
+import fi.vm.sade.hakemuseditori.json.JsonFormats
+import fi.vm.sade.omatsivut.config.AppConfig.AppConfig
+import fi.vm.sade.utils.slf4j.Logging
+import org.apache.commons.io.IOUtils
+
+trait TuloskirjeComponent {
+
+  val tuloskirjeService: TuloskirjeService
+
+  class StubbedTuloskirjeService extends TuloskirjeService with JsonFormats with Logging {
+    override def fetchTuloskirje(hakuOid:String, hakemusOid: String) : Option[Array[Byte]] = {
+      logger.info(s"Get tuloskirje info for hakemus $hakemusOid")
+      hakemusOid match {
+        case "1.2.246.562.11.00000441369" => Some("1.2.246.562.11.00000441369_hyvaksymiskirje".getBytes)
+        case _ => None
+      }
+    }
+    override def getTuloskirjeInfo(hakuOid:String, hakemusOid: String) : Option[Tuloskirje] ={
+      fetchTuloskirje(hakuOid, hakemusOid).map(_ => Tuloskirje(hakuOid, 1479099404159L))
+    }
+  }
+
+  class SharedDirTuloskirjeService(appConfig: AppConfig) extends TuloskirjeService with Logging {
+    private val fileSystemUrl = appConfig.settings.tuloskirjeetFileSystemUrl;
+
+    private def getFileName(hakuOid: String, hakemusOid: String): File = {
+      new File(s"$fileSystemUrl/$hakuOid/$hakemusOid.pdf")
+    }
+
+    override def fetchTuloskirje(hakuOid:String, hakemusOid: String) : Option[Array[Byte]] = {
+      val file = getFileName(hakuOid, hakemusOid)
+      logger.info(s"Fetching tuloskirje from $file")
+      if (file.exists()) {
+        val fileStream = new FileInputStream(file);
+        val byteArray: Array[Byte] = IOUtils.toByteArray(fileStream)
+        IOUtils.closeQuietly(fileStream)
+        Some(byteArray)
+      } else {
+        None
+      }
+    }
+
+    override def getTuloskirjeInfo(hakuOid:String, hakemusOid: String) : Option[Tuloskirje] = {
+      val file = getFileName(hakuOid, hakemusOid)
+      if (file.exists()) {
+        Some(Tuloskirje(hakuOid, file.lastModified()))
+      } else {
+        None
+      }
+    }
+  }
+
+}
+trait TuloskirjeService {
+  def fetchTuloskirje(hakuOid:String, hakemusOid: String) : Option[Array[Byte]]
+  def getTuloskirjeInfo(hakuOid:String, hakemusOid: String) : Option[Tuloskirje]
+}
--- src/main/scala/fi/vm/sade/omatsivut/servlet/TuloskirjeetServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/TuloskirjeetServlet.scala
@@ -1,33 +1,23 @@
 package fi.vm.sade.omatsivut.servlet
 
-import java.io.{FileInputStream, File}
-
 import fi.vm.sade.hakemuseditori._
-import fi.vm.sade.hakemuseditori.hakemus.domain.HakemusMuutos
-import fi.vm.sade.hakemuseditori.hakemus.{HakemusInfo, HakemusRepositoryComponent}
-import fi.vm.sade.hakemuseditori.json.JsonFormats
-import fi.vm.sade.hakemuseditori.lomake.domain.AnswerId
-import fi.vm.sade.hakemuseditori.user.Oppija
-import fi.vm.sade.omatsivut.NonSensitiveHakemusInfo.answerIds
+import fi.vm.sade.hakemuseditori.hakemus.{HakemusRepositoryComponent}
+import fi.vm.sade.hakemuseditori.viestintapalvelu.TuloskirjeComponent
 import fi.vm.sade.omatsivut.config.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.oppijantunnistus.{ExpiredTokenException, InvalidTokenException, OppijanTunnistusComponent}
-import fi.vm.sade.omatsivut.security.{HakemusJWT, JsonWebToken}
-import fi.vm.sade.omatsivut.{NonSensitiveHakemus, NonSensitiveHakemusInfo, NonSensitiveHakemusInfoSerializer, NonSensitiveHakemusSerializer}
-import org.apache.commons.io.IOUtils
-import org.json4s.jackson.Serialization
+import fi.vm.sade.omatsivut.security.{JsonWebToken}
 import org.scalatra._
-import org.scalatra.json.JacksonJsonSupport
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 trait TuloskirjeetServletContainer {
   this: HakemusRepositoryComponent with
     HakemusEditoriComponent with
     HakemusRepositoryComponent with
+    TuloskirjeComponent with
     OppijanTunnistusComponent =>
 
   class TuloskirjeetServlet(val appConfig: AppConfig) extends OmatSivutServletBase {
-    private val fileSystemUrl = appConfig.settings.tuloskirjeetFileSystemUrl;
     protected val applicationDescription = "REST API tuloskirjeille"
     private val jwt = new JsonWebToken(appConfig.settings.hmacKey)
 
@@ -49,28 +39,14 @@ trait TuloskirjeetServletContainer {
         InternalServerError("error" -> "Internal server error")
     }
 
-    def fetchTuloskirjeFromFileSystem(hakuOid: String, hakemusOid: String): Option[Array[Byte]] = {
-      val path = s"$fileSystemUrl/$hakuOid/$hakemusOid.pdf"
-      val file = new File(path)
-      log(s"Getting tuloskirje from $path")
-      if(!file.exists()) {
-        None
-      } else {
-        val fileStream = new FileInputStream(file);
-        val byteArray: Array[Byte] = IOUtils.toByteArray(fileStream)
-        IOUtils.closeQuietly(fileStream)
-        Some(byteArray)
-      }
-    }
-
     get("/:token/tuloskirje.pdf") {
       (for {
         hakemusOid <- oppijanTunnistusService.validateToken(params("token"))
         hakemusInfo <- Try(hakemusRepository.getHakemus(hakemusOid, false).getOrElse(throw new NoSuchElementException))
-        tuloskirje <- Try(fetchTuloskirjeFromFileSystem(hakemusInfo.hakemus.haku.oid, hakemusOid))
+        tuloskirje <- Try(tuloskirjeService.fetchTuloskirje(hakemusInfo.hakemus.haku.oid, hakemusOid))
       } yield {
         tuloskirje match {
-          case Some(data) => Ok(data, Map(
+          case Some(data: Array[Byte]) => Ok(data, Map(
             "Content-Type" -> "application/octet-stream",
             "Content-Disposition" -> "attachment; filename=tuloskirje.pdf"))
           case None => throw new NoSuchElementException
