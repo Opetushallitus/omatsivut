42edb1b36a9570fd6f2d2bcf34ac7648f1ab2a71
Mikko Apo <mikko.apo@reaktor.fi>
2016-11-17 12:47:18 +0200

    Fixed localized-link url.properties by working around an angular issue

--- hakemuseditori/src/main/js/directives/localizedLink.js
+++ hakemuseditori/src/main/js/directives/localizedLink.js
@@ -10,6 +10,15 @@ module.exports = function(app) {
           .attr("href", attrs["href"])
           .attr("target", attrs["target"])
           .end()
+        // angular re-evaluates the original href expression and attrs.href needs to be watched
+        // <localized-link key="message.rememberSendAttachments"
+        //     href="{{url('omatsivut.applications.preview', application.oid) + '#liitteet'}}" target="_blank">
+        // link function is called with: attrs.href == "#liitteet"
+        // watched value for attrs.href becomes: "/omatsivut/secure/applications/preview/1.2.246.562.11.00000877699#liitteet"
+        // angular, WTF?
+        scope.$watch("attrs.href", function(value) {
+          link.find("a").attr("href", attrs.href)
+        });
         element.append(link)
       }
     }
