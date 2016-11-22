cb343c7f9f40265aeb94e23d51c982132ecf9f91
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-11-22 13:48:25 +0200

    fetchtuloskirje event fixed with new audit event API

--- src/main/scala/fi/vm/sade/hakemuseditori/auditlog/FetchTuloskirje.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/auditlog/FetchTuloskirje.scala
@@ -1,5 +1,6 @@
 package fi.vm.sade.hakemuseditori.auditlog
 
 case class FetchTuloskirje(hakuOid: String, hakemusOid: String) extends AuditEvent {
-  def toLogMessage = "Haettu haun " + hakuOid + " tuloskirje hakemukselle " + hakemusOid
+  def isUserOppija = true
+  def toLogMessage = Map("hakuOid" -> hakuOid, "hakemusOid" -> hakemusOid, "message" ->"Haettu haun tuloskirje hakemukselle")
 }
