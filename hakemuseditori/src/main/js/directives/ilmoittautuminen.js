ee8c96bf9fcb77e917f133681e01a6210c37a76a
Heikki Honkanen <heikki.honkanen@gofore.com>
2017-05-05 14:30:47 +0300

    OK-104: add token to non-hetu ilmoittautumis-url.

--- hakemuseditori/src/main/js/directives/ilmoittautuminen.js
+++ hakemuseditori/src/main/js/directives/ilmoittautuminen.js
@@ -7,8 +7,12 @@ module.exports = function(app) {
       },
       templateUrl: 'templates/ilmoittautuminen.html',
       link: function (scope, element, attrs) {
-        scope.localization = localization
+        scope.localization = localization;
+
+        scope.getIlmoittautuminenUrl = function(tulos) {
+          return tulos.ilmoittautumistila.ilmoittautumistapa.url + '?token=' + window.sessionStorage.getItem('bearerToken');
+        }
       }
     }
   }])
-}
+};
