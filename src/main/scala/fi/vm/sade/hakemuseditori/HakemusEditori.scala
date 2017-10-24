5e3456e5be6ff9727027c2a88132df6d638f8859
Jani Rahkola <jani.rahkola@reaktor.com>
2017-10-24 16:50:51 +0300

    Use fetchByHakemusOid in vastaanota

--- src/main/scala/fi/vm/sade/hakemuseditori/HakemusEditori.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/HakemusEditori.scala
@@ -62,7 +62,8 @@ trait HakemusEditoriComponent extends ApplicationValidatorComponent
     def fetchByPersonOid(personOid: String): List[HakemusInfo] =
       (ataruService.findApplications(personOid) ::: hakemusRepository.fetchHakemukset(personOid)).sortBy[Option[Long]](_.hakemus.received).reverse
 
-    def fetchByHakemusOid(hakemusOid: String): Option[HakemusInfo] = hakemusRepository.getHakemus(hakemusOid)
+    def fetchByHakemusOid(personOid: String, hakemusOid: String): Option[HakemusInfo] = hakemusRepository.getHakemus(hakemusOid)
+      .orElse(ataruService.findApplications(personOid).find(_.hakemus.oid == hakemusOid))
 
     def opetuspisteet(asId: String, query: String, lang: Option[String]): Option[List[Opetuspiste]] = koulutusInformaatioService.opetuspisteet(asId, query, parseLang(lang))
 
