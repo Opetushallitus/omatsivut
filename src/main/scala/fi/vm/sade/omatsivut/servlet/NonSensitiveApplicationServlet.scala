baaabfd01b2496fa65007cd1a731414f53daa57d
August Kilponen <august.kilponen@gmail.com>
2017-12-14 16:44:51 +0200

    Paikka vastaanotettu sähköpostista pois sitovuusteksti 2. asteen hauissa

--- src/main/scala/fi/vm/sade/omatsivut/servlet/NonSensitiveApplicationServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/NonSensitiveApplicationServlet.scala
@@ -16,6 +16,7 @@ import fi.vm.sade.hakemuseditori.lomake.domain.AnswerId
 import fi.vm.sade.hakemuseditori.tarjonta.TarjontaComponent
 import fi.vm.sade.hakemuseditori.tarjonta.domain.Haku
 import fi.vm.sade.hakemuseditori.user.Oppija
+import fi.vm.sade.hakemuseditori.valintatulokset.domain.{VastaanotaSitovasti, VastaanottoAction}
 import fi.vm.sade.omatsivut.NonSensitiveHakemusInfo.answerIds
 import fi.vm.sade.omatsivut.config.AppConfig.AppConfig
 import fi.vm.sade.omatsivut.oppijantunnistus.{ExpiredTokenException, InvalidTokenException, OppijanTunnistusComponent}
@@ -39,6 +40,7 @@ case class InsecureHakemusInfo(jsonWebToken: String, response: NonSensitiveHakem
 trait VastaanottoEmailContainer {
   this: HakemusRepositoryComponent with
     HakemusEditoriComponent with
+    TarjontaComponent with
     GroupEmailComponent =>
 
   def vastaanota(hakemusOid: String, hakukohdeOid: String, hakuOid: String, henkiloOid: String, requestBody: String, emailOpt: Option[String],
@@ -46,11 +48,18 @@ trait VastaanottoEmailContainer {
     def sendEmail(clientVastaanotto: ClientSideVastaanotto) = {
       emailOpt match {
         case Some(email: String) =>
+          val haku = tarjontaService.haku(hakuOid, language).get
           val subject = translations.getTranslation("message", "acceptEducation", "email", "subject")
 
           val dateFormat = new SimpleDateFormat(translations.getTranslation("message", "acceptEducation", "email", "dateFormat"))
           val dateAndTime = dateFormat.format(Calendar.getInstance().getTime)
-          val answer = translations.getTranslation("message", "acceptEducation", "email", "tila", clientVastaanotto.vastaanottoAction.toString)
+          val vastaanottoActionString =
+            if (haku.toisenasteenhaku && VastaanotaSitovasti.equals(clientVastaanotto.vastaanottoAction)) {
+              clientVastaanotto.vastaanottoAction.toString + "ToinenAste"
+            } else {
+              clientVastaanotto.vastaanottoAction.toString
+            }
+          val answer = translations.getTranslation("message", "acceptEducation", "email", "tila", vastaanottoActionString)
           val aoInfoRow = List(answer, clientVastaanotto.tarjoajaNimi, clientVastaanotto.hakukohdeNimi).mkString(" - ")
           val body = translations.getTranslation("message", "acceptEducation", "email", "body")
             .format(dateAndTime, aoInfoRow)
