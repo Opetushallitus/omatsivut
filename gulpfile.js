6884f93db7558f9ad3319d6d0eecc168b5258c78
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-01-17 16:23:54 +0200

    Combine hakemuseditori with omatsivut

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,10 @@ var gulp = require('gulp'),
     templates = require('gulp-angular-templatecache'),
     uglify = require('gulp-uglify'),
     gulpif = require('gulp-if'),
-    ngAnnotate = require('gulp-ng-annotate')
+    ngAnnotate = require('gulp-ng-annotate');
 
 var jsFiles = 'src/main/js/**/*.js';
-var isWatch
-var localEditor = false
-var editorLocation = "hakemuseditori"
+var isWatch = false;
 
 function handleError(err) {
     console.log(err.toString());
@@ -37,22 +35,9 @@ gulp.task("templates", function() {
   return gulp.src("src/main/templates/**/*.html")
     .pipe(templates("templates.js", { root:"templates/"}))
     .pipe(gulp.dest("src/main/templates"))
-})
+});
 
-gulp.task('copy-editor', function() {
-    gulp.src(editorLocation + "/src/main/img/**")
-      .pipe(gulp.dest('src/main/webapp/img/hakemuseditori'))
-
-    if(!localEditor) {
-      gulp.src(editorLocation + "/dist/*.js")
-        .pipe(gulp.dest('src/main/webapp'))
-    }
-
-    return gulp.src(editorLocation + "/src/main/less/**")
-      .pipe(gulp.dest('src/main/less/hakemuseditori'))
-})
-
-gulp.task('less', ['copy-editor'], function () {
+gulp.task('less', function () {
     gulp.src('src/main/less/main.less')
         .pipe(less().on('error', handleError))
         .pipe(concat('main.css'))
@@ -67,20 +52,15 @@ gulp.task('less', ['copy-editor'], function () {
       .pipe(less().on('error', handleError))
       .pipe(concat('preview.css'))
       .pipe(gulp.dest('src/main/webapp/css'));
-
-    gulp.src('src/main/less/hakutoiveidenMuokkaus.less')
-      .pipe(less().on('error', handleError))
-      .pipe(concat('hakutoiveidenMuokkaus.css'))
-      .pipe(gulp.dest('src/main/webapp/css'));
 });
 
 gulp.task('browserify', ["templates"], function() {
   compileJs(false)
-})
+});
 
 gulp.task('browserify-min', ["templates"], function() {
   compileJs(true)
-})
+});
 
 function compileJs(compress) {
   gulp.src(['src/main/js/app.js'])
@@ -96,22 +76,13 @@ function compileJs(compress) {
 }
 
 gulp.task('watch', function() {
-    isWatch = true
+    isWatch = true;
     livereload.listen();
     gulp.watch(['src/main/webapp/**/*.js', 'src/main/webapp/**/*.css', 'src/main/webapp/**/*.html'], livereload.changed);
-    gulp.watch([editorLocation + "/src/main/less/**", 'src/main/templates/**/*.html'], ['compile-dev'])
-    gulp.watch([jsFiles, 'src/main/webapp/hakemuseditori.js', 'src/main/webapp/hakemuseditori-templates.js'],['lint', 'browserify']);
     gulp.watch(['src/main/less/**/*.less'],['less']);
 });
 
-gulp.task('local-editor', function() {
-    localEditor = true
-    editorLocation = "hakemuseditori"
-});
-
-gulp.task('dev-local-editor', ['local-editor', 'dev']);
-
-gulp.task('compile', ['copy-editor', 'templates', 'browserify-min', 'less']);
-gulp.task('compile-dev', ['copy-editor', 'templates', 'browserify', 'less']);
+gulp.task('compile', ['templates', 'browserify-min', 'less']);
+gulp.task('compile-dev', ['templates', 'browserify', 'less']);
 gulp.task('dev', ['lint', 'compile-dev', 'watch']);
 gulp.task('default', ['compile']);
