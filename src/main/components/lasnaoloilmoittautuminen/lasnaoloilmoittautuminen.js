1918addda2abfd828a0af9f7b9a40278e5f6411b
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-02-05 14:34:50 +0200

    OK-176: Add a new ui component

--- src/main/components/lasnaoloilmoittautuminen/lasnaoloilmoittautuminen.js
+++ src/main/components/lasnaoloilmoittautuminen/lasnaoloilmoittautuminen.js
@@ -1,13 +1,85 @@
 module.exports = function(app) {
-    app.directive("lasnaoloilmoittautuminen", ["localization", function (localization) {
+    app.directive("lasnaoloilmoittautuminen", ["localization", "restResources", function (localization, restResources) {
         return {
             restrict: 'E',
-            scope: {},
+            scope: {
+                application: '=application',
+                tulos: '=tulos'
+            },
             templateUrl: 'lasnaoloilmoittautuminen.html',
 
             link: function ($scope, element, attrs) {
+                $scope.done = false;
                 $scope.localization = localization;
+
+                $scope.states = {
+                    // Spring hakus
+                    semester: 'LasnaKokoLukuvuosi',
+                    autumn: 'LasnaSyksy',
+
+                    // Autumn hakus
+                    spring: 'Lasna'
+                };
+
+                $scope.postLasnaoloilmoittautuminen = function() {
+                    $scope.error = false;
+                    var body = {
+                        hakukohdeOid: $scope.tulos.hakukohdeOid,
+                        muokkaaja: '',
+                        tila: $scope.states[$scope.ilmoittautuminen],
+                        selite: 'Omien sivujen läsnäoloilmoittautuminen'
+                    };
+                    restResources.lasnaoloilmoittautuminen.save({ hakuOid: $scope.application.haku.oid , hakemusOid: $scope.application.oid }, JSON.stringify(body), onSuccess, onError);
+                };
+
+                $scope.getStateTranslation = function() {
+                    return localization('lasnaoloilmoittautuminen.' + $scope.ilmoittautuminen);
+                };
+
+                $scope.getErrorTranslation = function() {
+                    return localization('lasnaoloilmoittautuminen.error.' + $scope.error);
+                };
+
+                function onSuccess(data) {
+                    $scope.tulos.ilmoittautumistila.ilmoittautumisaika.tehty = new Date();
+                    $scope.done = true;
+                }
+
+                function onError(err) {
+                    console.log(err);
+                    switch (err.status) {
+                        case 401:
+                            document.location.replace(window.url("omatsivut.login"));
+                            break;
+                        case 404:
+                            $scope.error = 'notFound';
+                            break;
+                        default:
+                            $scope.error = 'other';
+                    }
+                }
+
+                $scope.getEnrolmentMessageKeys = function() {
+                    var date = $scope.tulos.ilmoittautumistila.ilmoittautumisaika.tehty;
+                    return {date: getEnrolmentDate(date), time: getEnrolmentTime(date)};
+                };
+
+                function getEnrolmentDate(date) {
+                    var day = date.getDate();
+                    var month = date.getMonth() + 1;
+                    var year = date.getFullYear();
+
+                    return day + '.' + month + '.' + year;
+                }
+
+                function getEnrolmentTime(date) {
+                    var hours = date.getHours();
+                    var minutes = date.getMinutes();
+
+                    return hours + ':' + ("0" + minutes).slice(-2);
+                }
             }
+
         }
     }])
 };
--- src/main/js/hakemuseditori/hakemuseditori.js
+++ src/main/js/hakemuseditori/hakemuseditori.js
@@ -1,23 +1,24 @@
 module.exports = function(app) {
-  require('./applicationValidator')(app)
-  require('./angularBacon')(app)
-  require('./constants')(app)
-  require('./directives/confirm')(app)
-  require('./directives/question')(app)
-  require('./directives/localizedLink')(app)
-  require('./directives/formattedTime')(app)
-  require('./directives/sortable')(app)
-  require('./directives/disableClickFocus')(app)
-  require('./directives/application')(app)
-  require('./directives/hakutoiveenVastaanotto')(app)
-  require('./directives/ilmoittautuminen')(app)
-  require('./directives/kela')(app)
-  require('./directives/hakutoiveet')(app)
-  require('./directives/valintatulos')(app)
-  require('./directives/henkilotiedot')(app)
-  require('./directives/applicationPeriods')(app)
-  require('./directives/clearableInput')(app)
-  require('./directives/callout')(app)
-}
+  require('./applicationValidator')(app);
+  require('./angularBacon')(app);
+  require('./constants')(app);
+  require('./directives/confirm')(app);
+  require('./directives/question')(app);
+  require('./directives/localizedLink')(app);
+  require('./directives/formattedTime')(app);
+  require('./directives/sortable')(app);
+  require('./directives/disableClickFocus')(app);
+  require('./directives/application')(app);
+  require('./directives/hakutoiveenVastaanotto')(app);
+  require('./directives/ilmoittautuminen')(app);
+  require('./directives/kela')(app);
+  require('./directives/hakutoiveet')(app);
+  require('./directives/valintatulos')(app);
+  require('./directives/henkilotiedot')(app);
+  require('./directives/applicationPeriods')(app);
+  require('./directives/clearableInput')(app);
+  require('./directives/callout')(app);
+  require('../../components/lasnaoloilmoittautuminen/lasnaoloilmoittautuminen')(app);
+};
 
-module.exports.Hakemus = require("./hakemus")
+module.exports.Hakemus = require("./hakemus");
