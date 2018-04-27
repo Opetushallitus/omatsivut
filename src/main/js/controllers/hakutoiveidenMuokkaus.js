bd0af52f13e959605bcb1ddcaef2b54809daaa60
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-04-27 09:50:32 +0300

    OK-176: Fix undefined in securelink name

--- src/main/js/controllers/hakutoiveidenMuokkaus.js
+++ src/main/js/controllers/hakutoiveidenMuokkaus.js
@@ -1,30 +1,30 @@
-var Hakemus = require('../hakemuseditori/hakemuseditori').Hakemus
-var util = require('../util')
+var Hakemus = require('../hakemuseditori/hakemuseditori').Hakemus;
+var util = require('../util');
 
 module.exports = function(app, staticResources) {
   app.controller('HakutoiveidenMuokkausController', function($scope, $location, $http) {
-    var matches = $location.path().match(/token\/(.+)/)
-    var token = matches && matches[1]
-    var baseUrl = 'insecure/applications/application/'
+    var matches = $location.path().match(/token\/(.+)/);
+    var token = matches && matches[1];
+    var baseUrl = 'insecure/applications/application/';
 
-    $scope.lang = staticResources.translations.languageId
+    $scope.lang = staticResources.translations.languageId;
 
     $scope.logout = function() {
-      util.removeBearerToken()
+      util.removeBearerToken();
       $scope.application = null;
       $scope.loggedOut = true;
-    }
+    };
 
     if (token || util.getBearerToken()) {
-      $scope.loading = true
-      $location.path('/').replace()
-      var suffix = token ? 'token/' + token : 'session'
+      $scope.loading = true;
+      $location.path('/').replace();
+      var suffix = token ? 'token/' + token : 'session';
       $http.get(baseUrl + suffix).then(
           function (response) {
-            $scope.loading = false
-            $scope.application = new Hakemus(response.data)
-            $scope.application.oiliJwt = response.oiliJwt
-            var henkilotiedot = response.data.hakemus.answers.henkilotiedot
+            $scope.loading = false;
+            $scope.application = new Hakemus(response.data);
+            $scope.application.oiliJwt = response.oiliJwt;
+            var henkilotiedot = response.data.hakemus.answers.henkilotiedot;
             if(henkilotiedot.Henkilotunnus) {
               $scope.allowVastaanotto = false
             } else {
@@ -35,7 +35,7 @@ module.exports = function(app, staticResources) {
             }
           },
           function (response) {
-            $scope.loading = false
+            $scope.loading = false;
             if (404 === response.status) {
               $scope.errorMessage = 'error.noActiveApplication'
             } else if (response.data && response.data.error === 'expiredToken') {
@@ -50,4 +50,4 @@ module.exports = function(app, staticResources) {
       $scope.errorMessage = 'error.noTokenAvailable'
     }
   })
-}
+};
