fecfe431c8faba6d0f538e2b880a07c70a4ae8be
teo.mertanen <teo.mertanen@gofore.com>
2018-06-20 13:09:32 +0300

    BUG-1788 logging in LanguageFilter

--- src/main/scala/fi/vm/sade/omatsivut/servlet/LanguageFilter.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/LanguageFilter.scala
@@ -3,10 +3,11 @@ package fi.vm.sade.omatsivut.servlet
 import javax.servlet.http.{Cookie, HttpServletRequest, HttpServletResponse}
 
 import fi.vm.sade.hakemuseditori.domain.Language
+import fi.vm.sade.hakemuseditori.domain.Language.Language
 import fi.vm.sade.utils.slf4j.Logging
 import org.scalatra.ScalatraFilter
 
-class LanguageFilter extends ScalatraFilter with Logging{
+class LanguageFilter extends ScalatraFilter with Logging {
   val cookieName = "i18next"
   val cookieMaxAge = 60 * 60 * 24 * 1800
 
@@ -20,26 +21,32 @@ class LanguageFilter extends ScalatraFilter with Logging{
 
   private def checkLanguage(request: HttpServletRequest, response: HttpServletResponse) {
     val (lang: Language.Language, setCookie: Boolean) = chooseLanguage(Option(request.getParameter("lang")), Option(request.getCookies()), request.getRequestURL.toString)
-    if(setCookie) {
+    if (setCookie) {
       addCookie(response, lang)
     }
     request.setAttribute("lang", lang)
   }
 
   def chooseLanguage(param: Option[String], cookies: Option[Array[Cookie]], url: String): (Language.Language, Boolean) = {
-    (chooseLanguageFromParam(param).orElse(chooseLanguageFromCookie(cookies)).orElse(chooseLanguageFromHost(url)).getOrElse(Language.fi), true)
+    val language = chooseLanguageFromParam(param)
+      .orElse(chooseLanguageFromCookie(cookies))
+      .orElse(chooseLanguageFromHost(url))
+      .getOrElse{
+        logger.warn("Could not decide language from params, cookies, or URL. Using default language.")
+        Language.fi
+      }
+    (language, true)
   }
 
-  private def chooseLanguageFromCookie(cookies: Option[Array[Cookie]]) = {
-    reqCookie(cookies, {_.getName.equals(cookieName)})
-  }
-
-  private def reqCookie(optCookies: Option[Array[Cookie]], matcher: (Cookie) => Boolean) = {
-    for {
+  private def chooseLanguageFromCookie(optCookies: Option[Array[Cookie]]): Option[Language] = {
+    (for {
       cookies <- optCookies
-      cookie <- cookies.find(matcher)
-      lang <- Language.parse(cookie.getValue())
-    } yield lang
+      cookie <- cookies.find(_.getName.equals(cookieName))
+      lang <- Language.parse(cookie.getValue)
+    } yield lang).map { lang =>
+      logger.info(s"Got language $lang from cookies.")
+      lang
+    }
   }
 
   private def addCookie(response: HttpServletResponse, lang: Language.Language) {
@@ -51,12 +58,21 @@ class LanguageFilter extends ScalatraFilter with Logging{
 
   private def chooseLanguageFromParam(param: Option[String]): Option[Language.Language] = {
     param.flatMap(Language.parse(_))
+      .map { lang =>
+        logger.info(s"Got language $lang from parameter ${param.get}")
+        lang
+      }
   }
 
-  private def chooseLanguageFromHost(url: String): Option[Language.Language] = url match {
-    case x if x.contains(domainFi) => Some(Language.fi)
-    case x if x.contains(domainSv) => Some(Language.sv)
-    case x if x.contains(domainEn) => Some(Language.en)
-    case default => None
+  private def chooseLanguageFromHost(url: String): Option[Language.Language] = {
+    (url match {
+      case x if x.contains(domainFi) => Some(Language.fi)
+      case x if x.contains(domainSv) => Some(Language.sv)
+      case x if x.contains(domainEn) => Some(Language.en)
+      case default => None
+    }).map { lang =>
+      logger.info(s"Got language $lang from url $url")
+      lang
+    }
   }
 }
