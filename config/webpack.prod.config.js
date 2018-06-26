dcc5693917c281f4893c66b761263fa07b3b3d8c
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-06-26 10:23:45 +0300

    Add webpack build files

---
+++ config/webpack.prod.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main/js/app.js',
+  output: {
+    path: path.resolve(__dirname, '../src/main/webapp/'),
+    filename: 'bundle.js',
+    publicPath: '/omatsivut/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: "raw-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              sourceMap: true
+            }
+          }
+        ]
+      },
+    ]
+  }
+  /*
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [ 'babel-loader' ],
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader },
+          {
+            loader: 'css-loader',
+            options: {
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
+  */
+}
