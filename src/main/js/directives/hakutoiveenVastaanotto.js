c01c468dcaca67a7c251df043bef86adae970743
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-06-28 17:22:28 +0300

    OK-202: Add additional checkbox for rejecting study place

--- src/main/js/directives/hakutoiveenVastaanotto.js
+++ src/main/js/directives/hakutoiveenVastaanotto.js
@@ -1,80 +1,103 @@
-import {VASTAANOTTO_ACTION} from '../constants';
 import localize from '../localization';
+import moment from '../moment';
 
-export default ["restResources", "$timeout", function (restResources, $timeout) {
-  return {
-    restrict: 'E',
-    scope: {
-      applicationOid: '&applicationOid',
-      haku: '&haku',
-      hakutoiveet: '&hakutoiveet',
-      callback: '=callback'
-    },
-    template: require('./hakutoiveenVastaanotto.html'),
-    link: function (scope, element, attrs) {
-      scope.vastaanottoAction = ""
-      scope.localization = localize;
-      scope.ajaxPending = false
-      scope.error = ""
-      scope.VASTAANOTTO_ACTION = VASTAANOTTO_ACTION;
-      try {
-        scope.email = scope.$parent.$parent.application.henkilotiedot['Sähköposti'].answer
-      }
-      catch (e) {
-        scope.email = '';
-      }
+export default class HakutoiveenVastaanotto {
+  constructor() {
+    this.restrict = 'E';
+    this.bindToController = {
+      applicationOid: '&',
+      haku: '&',
+      hakutoiveet: '&',
+      callback: '='
+    };
+    this.template = require('./hakutoiveenVastaanotto.html');
+    this.controller = HakutoiveenVastaanottoController;
+    this.controllerAs = 'ctrl';
+  }
 
-      scope.formatTimestamp = function(dt) {
-          return window.moment(dt).format('LLL').replace(/,/g, "")
-      }
+  link(scope, element, attrs) {
+    scope.localization = localize;
+  }
+}
 
-      scope.inputIsDisabled = function() {
-        return this.ajaxPending || this.vastaanottoSentSuccessfully
-      }
+class HakutoiveenVastaanottoController {
+  constructor($timeout, restResources, $scope) {
+    this.$timeout = $timeout;
+    this.restResources = restResources;
 
-      scope.flashSiirtohakuNotification = function() {
-        scope.siirtohakuClass = 'siirtohaku-fade-out'
-        $timeout(function() {
-          scope.siirtohakuClass = 'siirtohaku-fade-in'
-        }, 50)
-      }
-
-      scope.vastaanotaHakutoive = function(hakutoive) {
-        var scope = this
-        scope.error = ""
-        scope.ajaxPending = true
-
-        restResources.vastaanota.post({hakemusOid: scope.applicationOid(), hakukohdeOid: hakutoive.hakukohdeOid}, {
-          vastaanottoAction: { action:scope.vastaanottoAction },
-          email: scope.email,
-          hakukohdeNimi: hakutoive.hakukohdeNimi,
-          tarjoajaNimi: hakutoive.tarjoajaNimi
-        }, onSuccess, onError)
-
-        function onSuccess(updatedApplication) {
-          scope.ajaxPending = false
-          scope.error = ""
-          scope.vastaanottoSentSuccessfully = true
-          $timeout(function() {
-            scope.callback(hakutoive, updatedApplication)
-          }, 3500);
-        }
-
-        function onError(err) {
-          var saveError = (function() {
-           if (err.status == 401)
-              return "error.saveFailed_sessionExpired"
-            else if (err.status == 500)
-              return "error.serverError"
-            else if (err.status == 403)
-              return "error.priorAcceptance"
-            else
-              return "error.saveFailed"
-          })()
-          scope.error = localize(saveError)
-          scope.ajaxPending = false
-        }
-      }
+    try {
+      this.email = $scope.$parent.$parent.application.henkilotiedot['Sähköposti'].answer
+    } catch (e) {
+      this.email = '';
     }
   }
-}]
+
+  formatTimestamp(dt) {
+    return moment(dt).format('LLL').replace(/,/g, "");
+  };
+
+  isVastaanottoKesken() {
+    return this.ajaxPending || this.vastaanottoSentSuccessfully;
+  };
+
+  isNotVastaanotettavissa() {
+    return !(this.vastaanottoAction && this.vastaanottoAction.length !== 0)
+      || this.isVastaanottoKesken()
+      || (this.isRejectSelected() && !this.confirmCancelAction);
+  }
+
+  isRejectSelected() {
+    return this.vastaanottoAction === 'Peru';
+  }
+
+
+  flashSiirtohakuNotification() {
+    this.siirtohakuClass = 'siirtohaku-fade-out';
+    this.$timeout(() => this.siirtohakuClass = 'siirtohaku-fade-in', 50)
+  };
+
+  vastaanotaHakutoive(hakutoive) {
+    this.ajaxPending = true;
+
+    const pathParams = {
+      hakemusOid: this.applicationOid(),
+      hakukohdeOid: hakutoive.hakukohdeOid
+    };
+
+    const data = {
+      vastaanottoAction: {action: this.vastaanottoAction},
+      email: this.email,
+      hakukohdeNimi: hakutoive.hakukohdeNimi,
+      tarjoajaNimi: hakutoive.tarjoajaNimi
+    };
+
+    this.restResources.vastaanota.post(pathParams, data).$promise
+      .then(this.onSuccess(updatedApplication, hakutoive), this.onError(err));
+  }
+
+  onSuccess(updatedApplication, hakutoive) {
+    this.ajaxPending = false;
+    this.error = "";
+    this.vastaanottoSentSuccessfully = true;
+    this.$timeout(() => this.callback(hakutoive, updatedApplication), 3500);
+  }
+
+  onError(err) {
+    const errorKey = this.constructor.getErrorKey(err);
+    this.error = localize(errorKey);
+    this.ajaxPending = false;
+  }
+
+  static getErrorKey(err) {
+    if (err.status === 401)
+      return "error.saveFailed_sessionExpired";
+    else if (err.status === 500)
+      return "error.serverError";
+    else if (err.status === 403)
+      return "error.priorAcceptance";
+    else
+      return "error.saveFailed"
+  }
+}
+
+HakutoiveenVastaanottoController.$inject = ['$timeout', 'restResources', '$scope'];
