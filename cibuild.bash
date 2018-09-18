c8e94d27e6655af8286c629992c502e3a6759aed
August Kilponen <august.kilponen@gmail.com>
2018-09-18 20:56:58 +0300

    Update cibuild.bash

--- cibuild.bash
+++ cibuild.bash
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 time ./webbuild.sh
-time mvn clean install -Dbranch=${TRAVIS_BRANCH} -Drevision=${TRAVIS_COMMIT} -DbuildNumber=${TRAVIS_BUILD_NUMBER} -Dvalintatulos.it.postgres.port=5432
+time mvn clean install -B -Dbranch=${TRAVIS_BRANCH} -Drevision=${TRAVIS_COMMIT} -DbuildNumber=${TRAVIS_BUILD_NUMBER} -Dvalintatulos.it.postgres.port=5432
