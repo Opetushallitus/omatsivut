72761a40504a52b4e7f94d9ee699398543e2494c
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-09-19 07:34:53 -0300

    Start adding Docker build + upload

--- cibuild.bash
+++ cibuild.bash
@@ -3,4 +3,7 @@
 set -eo pipefail
 
 time ./webbuild.sh
-time mvn clean install -B -Dbranch=${TRAVIS_BRANCH} -Drevision=${TRAVIS_COMMIT} -DbuildNumber=${TRAVIS_BUILD_NUMBER} -Dvalintatulos.it.postgres.port=5432
+#EXTRA_MAVEN_ARGS="-DskipTests"
+time mvn clean install $EXTRA_MAVEN_ARGS -B -Dbranch=${TRAVIS_BRANCH} -Drevision=${TRAVIS_COMMIT} -DbuildNumber=${TRAVIS_BUILD_NUMBER} -Dvalintatulos.it.postgres.port=5432
+mv -i target/omatsivut*SNAPSHOT.war $DOCKER_BUILD_DIR/artifact/${ARTIFACT_NAME}.war
+cp -vair src/main/resources/oph-configuration $DOCKER_BUILD_DIR/config/
