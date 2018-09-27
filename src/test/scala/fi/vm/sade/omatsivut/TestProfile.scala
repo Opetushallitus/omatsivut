ebae5ca801258ad4439f3aa9c90c2acec734d415
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-09-27 10:06:57 +0300

    Siivoa testiajojen jälkiä
    Omalla koneellani oli kymmeniä gigatavua
    purettuja vts-warreja tmp-hakemistossa.

--- src/test/scala/fi/vm/sade/omatsivut/TestProfile.scala
+++ src/test/scala/fi/vm/sade/omatsivut/TestProfile.scala
@@ -1,6 +1,10 @@
 package fi.vm.sade.omatsivut
 
-import fi.vm.sade.omatsivut.config.{ComponentRegistry, AppConfig}
+import java.io.{File, FilenameFilter}
+
+import fi.vm.sade.omatsivut.config.{AppConfig, ComponentRegistry}
+import fi.vm.sade.utils.slf4j.Logging
+import org.apache.commons.io.FileUtils
 
 object SharedAppConfig {
   lazy final val appConfig = new AppConfig.IT
@@ -15,8 +19,25 @@ object SharedJetty {
   private lazy val jettyLauncher = new JettyLauncher(Some("it"))
 
   def start {
+    Runtime.getRuntime.addShutdownHook(new TempDirCleanUpThread)
     jettyLauncher.start
   }
 
   def main(args: Array[String]) = start
+}
+
+class TempDirCleanUpThread extends Thread with Logging {
+  override def run(): Unit = {
+    logger.info("Running shutdown hook for cleanup.")
+    val tmpDir = System.getProperty("java.io.tmpdir")
+    logger.info(s"Deleting jetty-* and scalate-* from $tmpDir")
+    new File(tmpDir).listFiles(new FilenameFilter {
+      override def accept(dir: File, name: String): Boolean = {
+        name.startsWith("jetty-") || name.startsWith("scalate-")
+      }
+    }).foreach(f => {
+      logger.info(s"\tremoving $f")
+      FileUtils.forceDelete(f)
+    })
+  }
 }
