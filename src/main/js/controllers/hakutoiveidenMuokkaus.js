20317b99a0ef4773d21c7d04491cf1d82f663321
teo.mertanen <teo.mertanen@gofore.com>
2018-10-31 18:14:21 +0200

    henkilötietojen piilotus css:stä ohjelmalliseksi

--- src/main/js/controllers/hakutoiveidenMuokkaus.js
+++ src/main/js/controllers/hakutoiveidenMuokkaus.js
@@ -30,6 +30,7 @@ export default ['$scope', '$location', '$http', function($scope, $location, $htt
         $scope.loading = false;
         $scope.application = new Hakemus(response.data);
         $scope.application.oiliJwt = response.oiliJwt;
+        $scope.application.isHakutoiveidenMuokkaus = true;
         const henkilotiedot = response.data.hakemus.answers.henkilotiedot;
         $scope.allowVastaanotto = !henkilotiedot.Henkilotunnus;
         $scope.user = {
