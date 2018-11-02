cd6030a99dd753e6eead6cb365775e29ffe64e46
teo.mertanen <teo.mertanen@gofore.com>
2018-11-02 10:27:33 +0200

    fix rebase conflict between 47ab42134c911dab014c16dcfa92b14b0e25c938 and ca9045bd8e97f85fcdad280b3678cefc743ab31b in moved file ilmoittautuminen.js

--- src/main/js/directives/ilmoittautuminen.js
+++ src/main/js/directives/ilmoittautuminen.js
@@ -4,18 +4,43 @@ export default function () {
   return {
     restrict: 'E',
     scope: {
-      hakukohteet: '&',
-      oili: '&',
-      application: '='
+      hakukohteet: '&hakukohteet',
+      oili: '&oili',
+      application: '=application'
     },
-    template: require('./ilmoittautuminen.html'),
+    templateUrl: require('./ilmoittautuminen.html'),
     link: function (scope, element, attrs) {
       scope.localization = localize;
 
-      scope.linkkiOK = function(tulos) {
+      $scope.statesToReport = {
+        LASNA_KOKO_LUKUVUOSI: 'semester',
+        POISSA_KOKO_LUKUVUOSI: 'away',
+        LASNA_SYKSY: 'autumn',
+        POISSA_SYKSY: 'spring',
+        LASNA: 'spring',
+        POISSA: 'away_spring'
+      };
+
+      $scope.linkkiOK = function(tulos) {
         return tulos.ilmoittautumistila.ilmoittautumistapa != null &&
-            tulos.ilmoittautumistila.ilmoittautumistapa.url
-      }
+          tulos.ilmoittautumistila.ilmoittautumistapa.url
+      };
+
+      $scope.ilmoittautumistietoNaytetaan = function(tulos) {
+        if (tulos && tulos.ilmoittautumistila && tulos.ilmoittautumistila.ilmoittautumistila) {
+          return $scope.statesToReport[tulos.ilmoittautumistila.ilmoittautumistila];
+        } else return false;
+      };
+
+      $scope.getEnrolmentMessageKeys = function(tulos) {
+        var date = tulos.ilmoittautumisenAikaleima ? new Date(tulos.ilmoittautumisenAikaleima) : new Date();
+        return {date: date.toLocaleDateString('fi-FI'), time: date.toLocaleTimeString('fi-FI')}
+      };
+
+      $scope.getStateTranslation = function(tulos) {
+        return localization('lasnaoloilmoittautuminen.' + $scope.statesToReport[tulos.ilmoittautumistila.ilmoittautumistila] );
+      };
+
     }
   }
 }
