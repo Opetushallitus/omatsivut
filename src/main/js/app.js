36a95b3435a246f91b3b2cdd823b361697b5c655
Mikko Siukola <mikko.siukola@cybercom.com>
2019-01-31 13:28:39 +0200

    Don't log thrown errors in tests

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -153,6 +153,10 @@ angular.module("exceptionOverride", []).factory("$exceptionHandler", ["$injector
   var loggedErrors = [];
   var skippedErrors = 0;
   return function (exception, cause) {
+    if(isTestMode()) {
+      console.log("Won't log errors to backend in test mode");
+      return;
+    }
     var $http = $injector.get("$http");
     var $window = $injector.get("$window");
     function get_browser() {
