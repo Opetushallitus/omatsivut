25fb44a4e8f386076a2c385a813252a2404f410e
Mikko Siukola <mikko.siukola@cybercom.com>
2019-02-01 16:12:15 +0200

    Don't require authentication for now.

--- src/main/scala/fi/vm/sade/omatsivut/servlet/ClientErrorLoggingServlet.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/ClientErrorLoggingServlet.scala
@@ -11,25 +11,27 @@ import org.scalatra.{InternalServerError, Ok, ScalatraServlet}
 import org.scalatra.json.JacksonJsonSupport
 import org.slf4j.LoggerFactory
 
-class ClientErrorLoggingServlet(val appConfig: AppConfig) extends ScalatraServlet with OmatSivutServletBase with AuthenticationRequiringServlet
-                                                                  with JacksonJsonSupport with JsonFormats with HakemusEditoriUserContext {
+class ClientErrorLoggingServlet(val appConfig: AppConfig) extends ScalatraServlet with OmatSivutServletBase
+                                                                  with JacksonJsonSupport with JsonFormats {
 
   private val frontLogger = LoggerFactory.getLogger("frontend")
 
-  override def user() = Oppija(personOid())
+  //override def user() = Oppija(personOid())
 
   post("/") {
     before() {
       contentType = formats("json")
     }
-    logger.info("A frontend error from user ({}) is being logged to oph-omatsivut-frontend.log", {user().oid})
+    //logger.info("A frontend error from user ({}) is being logged to oph-omatsivut-frontend.log", {user().oid})
     try {
       val stringToLog = parsedBody.extract[Map[String, Any]].map {case (k, v) => k + ": " + v}.mkString(" | ")
-      frontLogger.error("Error from frontend - user " + user().oid + ", " + stringToLog)
+      //frontLogger.error("Error from frontend - user " + user().oid + ", " + stringToLog)
+      frontLogger.error("Error from frontend - " + stringToLog)
       Ok(body = {"Error successfully logged to backend"})
     } catch {
       case t: Throwable =>
-        logger.error("Error when trying to log frontend error for user (" + user().oid + "). Error: " + t + " ,Request: " + request)
+        //logger.error("Error when trying to log frontend error for user (" + user().oid + "). Error: " + t + " ,Request: " + request)
+        logger.error("Error when trying to log frontend error:: " + t + ", Request: " + request)
         InternalServerError(body = {"Error when logging to backend"}, reason = "Parsing failed")
     }
   }
