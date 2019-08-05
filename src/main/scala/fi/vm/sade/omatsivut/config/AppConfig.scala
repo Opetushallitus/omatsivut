c23973a903b9882615224744e60530f67761c18c
oph-petar <petar@phz.fi>
2019-08-05 18:11:31 +0300

    Adjusting tests for new db initialization

--- src/main/scala/fi/vm/sade/omatsivut/config/AppConfig.scala
+++ src/main/scala/fi/vm/sade/omatsivut/config/AppConfig.scala
@@ -53,14 +53,27 @@ object AppConfig extends Logging {
   }
 
   class Dev extends AppConfig with ExampleTemplatedProps with MockAuthentication with StubbedExternalDeps {
+    val localPostgresService = new LocalPostgresService
+
     def springConfiguration = new OmatSivutSpringContext.Dev()
 
     override lazy val settings = ConfigTemplateProcessor.createSettings("common", templateAttributesFile)
       .withOverride("mongodb.oppija.uri", "mongodb://localhost:27017")
+      .withOverride("omatsivut.db.port", itPostgresPortChooser.chosenPort.toString)
+      .withOverride("omatsivut.db.host", "localhost")
+      .withOverride("omatsivut.db.url", "jdbc:postgresql://localhost:" + itPostgresPortChooser.chosenPort + "/omatsivutdb")
+
+    override def onStart: Unit = {
+      localPostgresService.start()
+    }
+
+    override def onStop: Unit = {
+      localPostgresService.stop()
+    }
   }
 
   class IT extends AppConfig with ExampleTemplatedProps with MockAuthentication with StubbedExternalDeps {
-    def springConfiguration = new OmatSivutSpringContext.Dev()
+    def springConfiguration: OmatSivutConfiguration = new OmatSivutSpringContext.Dev()
 
     // Testien vaatimat overridet
     OphUrlProperties.addOverride("url-oppija", "http://localhost:" + AppConfig.embeddedJettyPortChooser.chosenPort.toString)
