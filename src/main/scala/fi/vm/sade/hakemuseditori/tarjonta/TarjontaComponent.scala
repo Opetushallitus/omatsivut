302e8371c487e7092b830d14ff03625ef24b5e49
Mikko Suonio <mikko.suonio@reaktor.com>
2020-01-15 10:46:05 +0200

    HLE-1065 : Refaktorointi: siirrä trait omaan tiedostoon

--- src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/TarjontaComponent.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/TarjontaComponent.scala
@@ -151,27 +151,4 @@ trait TarjontaComponent {
   }
 }
 
-trait TarjontaService {
-  def haku(oid: String, lang: Language.Language) : Option[Haku]
-  def hakukohde(oid: String) : Option[Hakukohde]
 
-  def filterHakutoiveOidsByActivity(activity: Boolean, hakutoiveet: List[Hakemus.HakutoiveData], haku: Haku): List[String] = {
-    val hakukohteet = hakutoiveet.flatMap(entry => entry.get("Koulutus-id").map(oid => {
-      hakukohde(oid).getOrElse(Hakukohde(oid, None, None, Some(KohteenHakuaika(0L, 0L)), None))
-    }))
-    hakukohteet.filter(hakukohde => hakukohde.kohteenHakuaika match {
-      case Some(aika) => aika.active == activity
-      case _ => hakukohde.hakuaikaId.map((hakuaikaId: String) => haku.applicationPeriods.find(_.id == hakuaikaId).exists(_.active == activity)).getOrElse(haku.active == activity)
-    }).map(_.oid)
-  }
-
-  def getOhjeetUudelleOpiskelijalle(hakukohdeOid: Option[String]): Option[String] = {
-    for {
-      oid <- hakukohdeOid
-      tarjonnanHakukohde <- hakukohde(oid)
-      linkki <- tarjonnanHakukohde.ohjeetUudelleOpiskelijalle
-    } yield {
-      linkki
-    }
-  }
-}
