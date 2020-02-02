44934b18fd0a634d9c3255003a5220a57788effb
Mikko Suonio <mikko.suonio@reaktor.com>
2020-02-02 13:06:23 +0200

    HLE-1065 : Lisää Kouta-hakuun kenttiä
    Selvitettäväksi jäävät aikataulu, jarjestelmanHakulomake, showSingleStudyPlaceEnforcement ja
    usePriority.

--- src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/kouta/KoutaHaku.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/kouta/KoutaHaku.scala
@@ -2,26 +2,62 @@ package fi.vm.sade.hakemuseditori.tarjonta.kouta
 
 import fi.vm.sade.hakemuseditori.domain.Language.Language
 import fi.vm.sade.hakemuseditori.tarjonta.domain.Haku
+import fi.vm.sade.hakemuseditori.tarjonta.domain.HakuTyyppi.{Erillishaku, JatkuvaHaku, Yhteishaku}
 
-sealed case class KoutaHaku(hakuajat: List[KoutaHakuaika], nimi: Map[String, String], oid: String, tila: String) {
+sealed case class KoutaHaku(hakuajat: List[KoutaHakuaika],
+                            hakutapaKoodiUri: Option[String],
+                            kohdejoukkoKoodiUri: String,
+                            kohdejoukonTarkenneKoodiUri: Option[String],
+                            nimi: Map[String, String],
+                            oid: String,
+                            tila: String) {
   def getLocalizedName(lang: Language): String = {
     nimi.get(lang.toString).orElse(nimi.get("fi")).getOrElse("?")
+  }
+
+  def getHakutyyppi() = {
+    if (hakutapaKoodiUri.exists(_.contains("hakutapa_01"))) {
+      Yhteishaku
+    } else if (hakutapaKoodiUri.exists(_.contains("hakutapa_02"))) {
+      Erillishaku
+    } else if (hakutapaKoodiUri.exists(_.contains("hakutapa_03"))) {
+      JatkuvaHaku
+    } else {
+      throw new IllegalArgumentException("Unsupported type for haku: " + oid + " - " + hakutapaKoodiUri)
+    }
   }
 }
 
 object KoutaHaku {
   def toHaku(koutaHaku: KoutaHaku, lang: Language): Haku = {
     Haku(applicationPeriods = koutaHaku.hakuajat map { _.toHakuaika },
-      checkBaseEducationConflict = false,
+      checkBaseEducationConflict = checkBaseEducationConflict(koutaHaku),
       jarjestelmanHakulomake = false,
-      korkeakouluhaku = false,
+      korkeakouluhaku = isKorkeakouluhaku(koutaHaku),
       name = koutaHaku.getLocalizedName(lang),
       oid = koutaHaku.oid,
-      showSingleStudyPlaceEnforcement = false,
-      siirtohaku = false,
+      showSingleStudyPlaceEnforcement = false, // FIXME
+      siirtohaku = isSiirtohaku(koutaHaku),
       tila = koutaHaku.tila,
-      toisenasteenhaku = false,
-      tyyppi = "",
-      usePriority = false)
+      toisenasteenhaku = isToisenasteenhaku(koutaHaku),
+      tyyppi = koutaHaku.getHakutyyppi().toString,
+      usePriority = false) // FIXME
+  }
+
+  private def isKorkeakouluhaku(koutaHaku: KoutaHaku) = {
+    koutaHaku.kohdejoukkoKoodiUri.contains("haunkohdejoukko_12")
+  }
+
+  private def isToisenasteenhaku(koutaHaku: KoutaHaku) = {
+    val kohdejoukot = List("haunkohdejoukko_11","haunkohdejoukko_17","haunkohdejoukko_20")
+    kohdejoukot.exists(koutaHaku.kohdejoukkoKoodiUri.contains(_))
+  }
+
+  private def checkBaseEducationConflict(koutaHaku: KoutaHaku): Boolean = {
+    isKorkeakouluhaku(koutaHaku) && koutaHaku.kohdejoukonTarkenneKoodiUri.getOrElse("").trim.isEmpty
+  }
+
+  private def isSiirtohaku(koutaHaku: KoutaHaku) = {
+    koutaHaku.kohdejoukonTarkenneKoodiUri.exists(_.contains("haunkohdejoukontarkenne_1#"))
   }
 }
