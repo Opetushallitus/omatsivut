5897e2ac7b0a6c2f674ad1db5d54b2035e8883d9
Mikko Suonio <mikko.suonio@reaktor.com>
2020-02-12 13:31:00 +0200

    HLE-1065 : Lisää koulutuksen alkamisen tiedot hakukohteeseen

--- src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/kouta/KoutaHakukohde.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/kouta/KoutaHakukohde.scala
@@ -1,23 +1,27 @@
 package fi.vm.sade.hakemuseditori.tarjonta.kouta
 
-import fi.vm.sade.hakemuseditori.tarjonta.domain.{Hakukohde, KohteenHakuaika}
+import fi.vm.sade.hakemuseditori.tarjonta.domain.{Hakukohde, KohteenHakuaika, KoulutuksenAlkaminen}
 
-import scala.util.Try
+import scala.util.{Success, Try}
 
-sealed case class KoutaHakukohde(kaytetaanHaunAikataulua: Option[Boolean],
+sealed case class KoutaHakukohde(alkamiskausiKoodiUri: Option[String],
+                                 alkamisvuosi: Option[String],
+                                 kaytetaanHaunAikataulua: Option[Boolean],
+                                 kaytetaanHaunAlkamiskautta: Option[Boolean],
                                  hakuajat: List[KoutaHakuaika],
                                  oid: String) {
 }
 
 object KoutaHakukohde {
   def toHakukohde(koutaHakukohde: KoutaHakukohde): Try[Hakukohde] = {
-    extractKohteenHakuaika(koutaHakukohde) map { kohteenHakuaika =>
-      Hakukohde(hakuaikaId = Some("kouta-hakuaika-id"), // FIXME
-        koulutuksenAlkaminen = None,
-        kohteenHakuaika = kohteenHakuaika, // FIXME: tuki useammalle hakuajalle
-        ohjeetUudelleOpiskelijalle = None,
-        oid = koutaHakukohde.oid)
-    }
+    for {
+      kohteenHakuaika <- extractKohteenHakuaika(koutaHakukohde)
+      koulutuksenAlkaminen <- extractKoulutuksenAlkaminen(koutaHakukohde)
+    } yield Hakukohde(hakuaikaId = Some("kouta-hakuaika-id"), // FIXME
+      koulutuksenAlkaminen = koulutuksenAlkaminen,
+      kohteenHakuaika = kohteenHakuaika, // FIXME: tuki useammalle hakuajalle
+      ohjeetUudelleOpiskelijalle = None,
+      oid = koutaHakukohde.oid)
   }
 
   private def extractKohteenHakuaika(koutaHakukohde: KoutaHakukohde) : Try[Option[KohteenHakuaika]] = Try {
@@ -25,5 +29,20 @@ object KoutaHakukohde {
       None
     else
       koutaHakukohde.hakuajat.headOption map ( _.toKohteenHakuaika )
+  }
+
+  private def extractKoulutuksenAlkaminen(koutaHakukohde: KoutaHakukohde): Try[Option[KoulutuksenAlkaminen]] = {
+    if (koutaHakukohde.kaytetaanHaunAlkamiskautta.getOrElse(false))
+      Success(None)
+    else
+      createKoulutuksenAlkaminen(koutaHakukohde.alkamiskausiKoodiUri, koutaHakukohde.alkamisvuosi)
+  }
+
+  private def createKoulutuksenAlkaminen(alkamiskausiKoodiUri: Option[String],
+                                         alkamisvuosi: Option[String]) = Try {
+    for {
+      alkamiskausiKoodiUri <- alkamiskausiKoodiUri
+      alkamisvuosi <- alkamisvuosi map ( _.toInt )
+    } yield KoulutuksenAlkaminen(alkamisvuosi, alkamiskausiKoodiUri)
   }
 }
