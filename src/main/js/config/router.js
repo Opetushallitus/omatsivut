5ab0f7cabf768d4772b960cf27ce6218c6b0ef07
Jaakko Heikelä <jaakko.heikela@gofore.com>
2020-03-03 16:04:30 +0200

    SEC-95: Add CSRF as default header for post and put.

--- src/main/js/config/router.js
+++ src/main/js/config/router.js
@@ -1,8 +1,14 @@
 import HakemusInterceptor from '../interceptors/nonSensitiveHakemus';
 import RestErrorInterceptor from '../interceptors/restError';
 
-export default ['$httpProvider', '$locationProvider', function($httpProvider, $locationProvider) {
+export default ['$httpProvider', '$locationProvider', '$cookies', function($httpProvider, $locationProvider, $cookies) {
   $locationProvider.html5Mode(true);
+  $httpProvider.defaults.headers.post = {
+    'CSRF': $cookies['CSRF']
+  };
+  $httpProvider.defaults.headers.put = {
+    'CSRF': $cookies['CSRF']
+  };
   $httpProvider.interceptors.push(HakemusInterceptor);
   $httpProvider.interceptors.push(RestErrorInterceptor);
 }]
