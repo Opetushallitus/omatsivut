17be4abb5740380c1fc5420d642598fcfb503df9
Jaakko Heikelä <jaakko.heikela@gofore.com>
2020-03-05 11:48:02 +0200

    SEC-95: Remove console logs and redundant code changes.

--- src/main/js/interceptors/nonSensitiveHakemus.js
+++ src/main/js/interceptors/nonSensitiveHakemus.js
@@ -13,7 +13,6 @@ function shouldAuthenticate(config) {
 export default ['$cookies', function HakemusInterceptor($cookies) {
   return {
     request: function(config) {
-      console.log('$injector.get($cookies).get(CSRF): ' + $cookies.get('CSRF'));
       if (shouldRerouteRequest(config)) {
         config.url = config.url.replace(/\/secure\//, '/insecure/')
       }
