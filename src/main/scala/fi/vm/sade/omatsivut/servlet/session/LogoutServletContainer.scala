a7adca266364ba9397417c5eb6e267669c5250f8
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-28 09:07:03 +0200

    OY-378 : Handle logoutrequest

--- src/main/scala/fi/vm/sade/omatsivut/servlet/session/LogoutServletContainer.scala
+++ src/main/scala/fi/vm/sade/omatsivut/servlet/session/LogoutServletContainer.scala
@@ -3,7 +3,6 @@ package fi.vm.sade.omatsivut.servlet.session
 import java.util.UUID
 
 import fi.vm.sade.hakemuseditori.auditlog.Audit
-import fi.vm.sade.omatsivut.OphUrlProperties
 import fi.vm.sade.omatsivut.auditlog.Logout
 import fi.vm.sade.omatsivut.security.{AttributeNames, CookieHelper, SessionId, SessionService}
 import fi.vm.sade.omatsivut.servlet.OmatSivutServletBase
@@ -17,6 +16,23 @@ trait LogoutServletContainer extends OmatsivutPaths {
       sessionService.deleteSession(cookies.get(sessionCookieName).map(UUID.fromString).map(SessionId))
       clearCookie(sessionCookieName)
       redirectToCASOppijaLogout(request, response)
+    }
+
+    post("/") {
+      params.get("logoutRequest")
+        .map(_ => {
+          sessionService.deleteSession(cookies.get(sessionCookieName).map(UUID.fromString).map(SessionId))
+          clearCookie(sessionCookieName)
+          redirectToCASOppijaLogout(request, response)
+        })
+        .orElse(throw new IllegalArgumentException("Not 'logoutRequest' parameter given"))//) toRight(new IllegalArgumentException("Not 'logoutRequest' parameter given"))
+        //.right.flatMap(request => CasLogout.parseTicketFromLogoutRequest(request).toRight(new RuntimeException(s"Failed to parse CAS logout request $request")))
+        //.right.flatMap(ticket => sessionService.deleteSession(ServiceTicket(ticket))) match {
+       // .flatMap(_ => sessionService.deleteSession(cookies.get(sessionCookieName).map(UUID.fromString).map(SessionId)))
+//      ) match {
+//        case Right(_) => NoContent()
+//        case Left(t) => throw t
+//      }
     }
 
     def sendLogOut(): Unit = {
