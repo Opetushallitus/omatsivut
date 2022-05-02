93051b63b9b3a72b8c64294f2797153c9bde5cff
August Kilponen <august.kilponen@gmail.com>
2022-05-02 15:28:03 +0300

    Poistettu Travis ja lisätty Github Actions

--- cibuild.bash
+++ cibuild.bash
@@ -5,9 +5,7 @@ set -eo pipefail
 time ./webbuild.sh
 #EXTRA_MAVEN_ARGS="-DskipTests"
 #EXTRA_MAVEN_ARGS="-Dtest=HakemusPreviewSpec"
-time mvn clean install -U $EXTRA_MAVEN_ARGS -B -Dbranch=${TRAVIS_BRANCH} -Drevision=${TRAVIS_COMMIT} -DbuildNumber=${TRAVIS_BUILD_NUMBER} -Dvalintatulos.it.postgres.port=5432 -Domatsivut.it.postgres.alreadyrunning=true -Domatsivut.it.postgres.port=5432
+time mvn clean install -U $EXTRA_MAVEN_ARGS -B -Dbranch=${GITHUB_REF_NAME} -Drevision=${GITHUB_SHA} -DbuildNumber=${GITHUB_RUN_NUMBER}
 echo "Checking what the local valinta-tulos-service war looks like"
 ls -l ./target/valinta-tulos-service.war
 #unzip -v ./target/valinta-tulos-service.war
-mv -i target/omatsivut*SNAPSHOT-allinone.jar $DOCKER_BUILD_DIR/artifact/${ARTIFACT_NAME}.jar
-cp -vair src/main/resources/oph-configuration $DOCKER_BUILD_DIR/config/
