4dc746e7b3cd7c4b326a9164939883bb88e002f5
August Kilponen <august.kilponen@gmail.com>
2022-06-23 09:18:17 +0300

    Asetetaan kouta-hauille ohjausparametreista tieto hakutoiveiden piorisoinnista

--- src/main/scala/fi/vm/sade/hakemuseditori/ohjausparametrit/OhjausparametritService.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/ohjausparametrit/OhjausparametritService.scala
@@ -4,7 +4,7 @@ import fi.vm.sade.hakemuseditori.fixtures.JsonFixtureMaps
 import fi.vm.sade.utils.http.DefaultHttpClient
 import fi.vm.sade.hakemuseditori.json.JsonFormats
 import fi.vm.sade.hakemuseditori.memoize.TTLOptionalMemoize
-import fi.vm.sade.hakemuseditori.ohjausparametrit.domain.{HaunAikataulu, TulostenJulkistus}
+import fi.vm.sade.hakemuseditori.ohjausparametrit.domain.{HaunAikataulu, HaunParametrit, TulostenJulkistus}
 import fi.vm.sade.omatsivut.config.AppConfig
 import fi.vm.sade.utils.slf4j.Logging
 import org.json4s.JsonAST.JValue
@@ -15,18 +15,23 @@ trait OhjausparametritComponent {
 
   class StubbedOhjausparametritService extends OhjausparametritService with JsonFormats {
     def haunAikataulu(asId: String) = {
-      JsonFixtureMaps.findByKey[JValue]("/hakemuseditorimockdata/ohjausparametrit.json", asId).flatMap(OhjausparametritParser.parseHaunAikataulu(_))
+      JsonFixtureMaps.findByKey[JValue]("/hakemuseditorimockdata/ohjausparametrit.json", asId).flatMap(OhjausparametritParser.parseHaunParametrit(_).flatMap(_.haunAikataulu))
+    }
+    def haunParametrit(asId: String) = {
+      JsonFixtureMaps.findByKey[JValue]("/hakemuseditorimockdata/ohjausparametrit.json", asId).flatMap(OhjausparametritParser.parseHaunParametrit(_))
     }
   }
 
   object CachedRemoteOhjausparametritService {
     def apply(ohjausparametritUrl: String): OhjausparametritService = {
       val service = new RemoteOhjausparametritService(ohjausparametritUrl)
-      val haunAikatauluMemo = TTLOptionalMemoize.memoize(service.haunAikataulu _, "ohjausparametrit haun aikataulu", 60 * 60, 256)
+      val haunParametritMemo = TTLOptionalMemoize.memoize(service.haunParametrit _, "ohjausparametrit haun aikataulu", 60 * 60, 256)
 
       new OhjausparametritService() {
-        override def haunAikataulu(asId: String) = haunAikatauluMemo(asId)
+        override def haunAikataulu(asId: String) = haunParametritMemo(asId).flatMap(_.haunAikataulu)
+        override def haunParametrit(asId: String) = haunParametritMemo(asId)
       }
+
     }
   }
 
@@ -34,13 +39,16 @@ trait OhjausparametritComponent {
     import org.json4s.jackson.JsonMethods._
 
     def haunAikataulu(asId: String) = {
+      haunParametrit(asId).flatMap(_.haunAikataulu)
+    }
+    def haunParametrit(asId: String) = {
       val url = ohjausparametritUrl + "/" + asId
       val (responseCode, _, resultString) = DefaultHttpClient.httpGet(url)(AppConfig.callerId)
         .responseWithHeaders
 
       responseCode match {
         case 200 =>
-          parse(resultString, useBigDecimalForDouble = false).extractOpt[JValue].flatMap(OhjausparametritParser.parseHaunAikataulu(_))
+          parse(resultString, useBigDecimalForDouble = false).extractOpt[JValue].flatMap(OhjausparametritParser.parseHaunParametrit(_))
         case errorCode =>
           logger.error(s"Response code ${errorCode} from ohjausparametrit-service at ${url}, expected: 200. Content: ${resultString}")
           None
@@ -49,7 +57,7 @@ trait OhjausparametritComponent {
   }
 
   private object OhjausparametritParser extends JsonFormats {
-    def parseHaunAikataulu(json: JValue) = {
+    def parseHaunParametrit(json: JValue) = {
       val julkistus = for {
         obj <- (json \ "PH_VTJH").toOption
         start <- (obj \ "dateStart").extractOpt[Long]
@@ -59,12 +67,16 @@ trait OhjausparametritComponent {
         obj <- (json \ "PH_HKP").toOption
         end <- (obj \ "date").extractOpt[Long]
       } yield end
-      Some(HaunAikataulu(julkistus, hakukierrosPaattyy))
+      val jarjestetytHakutoiveet = for {
+        param <- (json \ "jarjestetytHakutoiveet").extractOpt[Boolean]
+      } yield param
+      Some(HaunParametrit(haunAikataulu = Some(HaunAikataulu(julkistus, hakukierrosPaattyy)), jarjestetytHakutoiveet = jarjestetytHakutoiveet))
     }
   }
 }
 
 trait OhjausparametritService {
   def haunAikataulu(asId: String): Option[HaunAikataulu]
+  def haunParametrit(asId: String): Option[HaunParametrit]
 }
 
