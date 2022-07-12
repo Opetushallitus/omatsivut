ab1242471d2473c623a75e4edbe6cc1f1a21685a
August Kilponen <august.kilponen@gmail.com>
2022-07-12 16:02:01 +0300

    Päätellään oidin pituudesta pitääkö tiedot hakea koutasta vai tarjonnasta

--- src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/UnionTarjontaService.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/tarjonta/UnionTarjontaService.scala
@@ -2,15 +2,20 @@ package fi.vm.sade.hakemuseditori.tarjonta
 import fi.vm.sade.hakemuseditori.domain.Language.Language
 import fi.vm.sade.hakemuseditori.tarjonta.domain.{Haku, Hakukohde}
 
-class UnionTarjontaService(highPriorityService : TarjontaService,
-                           lowPriorityService : TarjontaService) extends TarjontaService {
+class UnionTarjontaService(tarjontaService: TarjontaService,
+                           koutaService : TarjontaService) extends TarjontaService {
+  def isKoutaOid(oid: String): Boolean = {
+    if(oid.length == 35) true
+    else false
+  }
+
   override def haku(oid: String, lang: Language): Option[Haku] = {
-    highPriorityService.haku(oid, lang)
-      .orElse(lowPriorityService.haku(oid, lang))
+    if(isKoutaOid(oid)) koutaService.haku(oid, lang)
+    else tarjontaService.haku(oid, lang)
   }
 
   override def hakukohde(oid: String, lang: Language): Option[Hakukohde] = {
-    highPriorityService.hakukohde(oid, lang)
-      .orElse(lowPriorityService.hakukohde(oid, lang))
+    if(isKoutaOid(oid)) koutaService.hakukohde(oid, lang)
+    else tarjontaService.hakukohde(oid, lang)
   }
 }
