e7ab05589bb8e75ea2cd96e6d2304f9ab96be414
August Kilponen <august.kilponen@gmail.com>
2022-07-13 13:35:53 +0300

    Enabloitu testit

--- cibuild.bash
+++ cibuild.bash
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-#time ./webbuild.sh
+time ./webbuild.sh
 #EXTRA_MAVEN_ARGS="-DskipTests"
 #EXTRA_MAVEN_ARGS="-Dtest=HakemusPreviewSpec"
 time mvn clean install -U $EXTRA_MAVEN_ARGS -B -Dbranch=${GITHUB_REF_NAME} -Drevision=${GITHUB_SHA} -DbuildNumber=${GITHUB_RUN_NUMBER}
