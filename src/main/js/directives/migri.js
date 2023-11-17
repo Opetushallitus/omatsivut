8ebc84e078b6de4d7eb4adcc0659b3cb40b6931a
Mikko Siukola <mikko.siukola@cybercom.com>
2023-11-17 14:39:35 +0200

    OK-326 Clean up UI

--- src/main/js/directives/migri.js
+++ src/main/js/directives/migri.js
@@ -9,18 +9,9 @@ export default function () {
     },
     template: require('./migri.html'),
     link: function (scope, element, attrs) {
-      console.log('linking', scope);
       scope.localization = localize;
-      scope.justSomeToken = 'c d e f g'
-      scope.migriTokenParsed = scope.token || 'no token found';
-      scope.constructUrl = function() {
-        const base = "https://opintopolkumigri.fi?token=";
-        const localized = scope.localization('migri.linkki-pohja');
-        console.log("Getting migri url for base " + (localized || base) + ". Token: " + scope.migriTokenParsed);
-        scope.parsedUrl = (localized || base) + scope.migriTokenParsed;
-        return (localized || base) + scope.migriTokenParsed;
-      }
-      scope.constructUrl();
+      scope.tokenParam = scope.token ? "?token=" + scope.token : ""
+      scope.parsedUrl = scope.localization('migri.linkki-pohja') + scope.tokenParam;
     }
   }
 }
