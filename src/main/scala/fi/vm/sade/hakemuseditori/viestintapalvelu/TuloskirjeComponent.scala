fcc6ebb3b482086bd587680dc72a11df86207853
August Kilponen <august.kilponen@gmail.com>
2025-10-30 15:49:33 +0200

    Korjattu tuloskirjeen luonti hetken hakemista S3-metadatasta

--- src/main/scala/fi/vm/sade/hakemuseditori/viestintapalvelu/TuloskirjeComponent.scala
+++ src/main/scala/fi/vm/sade/hakemuseditori/viestintapalvelu/TuloskirjeComponent.scala
@@ -18,8 +18,6 @@ import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.{GetBucketAclRequest, GetObjectRequest, GetObjectResponse, HeadObjectRequest, HeadObjectResponse, S3Exception, S3Object}
 import software.amazon.awssdk.utils.Validate
 
-import java.time.temporal.ChronoField
-import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
 
 trait TuloskirjeComponent {
@@ -126,7 +124,7 @@ trait TuloskirjeComponent {
 
     override def getTuloskirjeInfo(request: HttpServletRequest, hakuOid: String, hakemusOid: String, tuloskirjeKind: TuloskirjeKind) : Option[Tuloskirje] = {
       getHeadObjectResponse(hakuOid, hakemusOid, tuloskirjeKind) match {
-        case Some(headObjectResponse) => Some(Tuloskirje(hakuOid, headObjectResponse.lastModified().getLong(ChronoField.MILLI_OF_SECOND)))
+        case Some(headObjectResponse) => Some(Tuloskirje(hakuOid, headObjectResponse.lastModified().toEpochMilli))
         case None => None
       }
     }
